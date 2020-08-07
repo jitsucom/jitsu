1639fa056d26393a0212e227400f8c46d71cb22f
Sergey Burykin <xtreding@gmail.com>
2020-08-07 12:06:30 -0400

    add redirect / -> /p/welcome.html

---
+++ handlers/redirect.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type RedirectHandler struct {
+	toUrl string
+}
+
+func NewRedirectHandler(redirectToUrl string) *RedirectHandler {
+	return &RedirectHandler{toUrl: redirectToUrl}
+}
+
+func (rh *RedirectHandler) Handler(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Redirect(http.StatusPermanentRedirect, rh.toUrl)
+}
