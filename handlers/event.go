7e3eb462ed109b80cea5415e00fcb3006da98814
Sergey Burykin <xtreding@gmail.com>
2020-08-07 11:16:12 -0400

    added resolving user agent

--- handlers/event.go
+++ handlers/event.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ksensehq/eventnative/geo"
 	"github.com/ksensehq/eventnative/middleware"
 	"github.com/ksensehq/eventnative/timestamp"
+	"github.com/ksensehq/eventnative/useragent"
 	"log"
 	"net/http"
 	"strings"
@@ -14,16 +15,19 @@ import (
 )
 
 const eventnKey = "eventn_ctx"
+const uaKey = "user_agent"
 
 type EventHandler struct {
 	eventConsumer events.Consumer
 	geoResolver   geo.Resolver
+	uaResolver    *useragent.Resolver
 }
 
 func NewEventHandler() (eventHandler *EventHandler) {
 	eventHandler = &EventHandler{}
 	eventHandler.eventConsumer = appconfig.Instance.EventsConsumer
 	eventHandler.geoResolver = appconfig.Instance.GeoResolver
+	eventHandler.uaResolver = appconfig.Instance.UaResolver
 	return
 }
 
@@ -51,13 +55,22 @@ func (eh *EventHandler) Handler(c *gin.Context) {
 		log.Println(err)
 	}
 
-	kpixObject, ok := payload[eventnKey]
+	eventnObject, ok := payload[eventnKey]
 	if ok {
-		koMap, ok := kpixObject.(map[string]interface{})
+		evntMap, ok := eventnObject.(map[string]interface{})
 		if ok {
-			koMap[geo.GeoDataKey] = geoData
+			//geo
+			evntMap[geo.GeoDataKey] = geoData
+
+			//user agent
+			ua, ok := evntMap[uaKey]
+			if ok {
+				if uaStr, ok := ua.(string); ok {
+					evntMap[useragent.ParsedUaKey] = eh.uaResolver.Resolve(uaStr)
+				}
+			}
 		} else {
-			log.Printf("%s isn't an object %v", eventnKey, kpixObject)
+			log.Printf("%s isn't an object %v", eventnKey, eventnObject)
 		}
 	} else {
 		log.Printf("Unable to get %s from %v", eventnKey, payload)
