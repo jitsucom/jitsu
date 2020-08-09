419308a914c6f683dc81c56f07b144ec93423796
Sergey Burykin <xtreding@gmail.com>
2020-08-09 11:48:08 -0400

    added log.show_in_server for debugging (event json logs will be shown in server log)

--- events/multiple_async_logger.go
+++ events/multiple_async_logger.go
@@ -16,8 +16,9 @@ type tokenizedFact struct {
 
 //Logger that's is handling multiple log files: one per token(api_key)
 type MultipleAsyncLogger struct {
-	writerPerToken map[string]io.WriteCloser
-	logCh          chan tokenizedFact
+	writerPerToken     map[string]io.WriteCloser
+	logCh              chan tokenizedFact
+	showInGlobalLogger bool
 }
 
 //Put event fact to channel
@@ -37,16 +38,21 @@ func (m *MultipleAsyncLogger) Close() (resultErr error) {
 
 //Create MultipleAsyncLogger and run goroutine that's read from channel
 //and write to file according to token
-func NewMultipleAsyncLogger(writerPerToken map[string]io.WriteCloser) Consumer {
-	logger := MultipleAsyncLogger{writerPerToken: writerPerToken, logCh: make(chan tokenizedFact, 20000)}
+func NewMultipleAsyncLogger(writerPerToken map[string]io.WriteCloser, showInGlobalLogger bool) Consumer {
+	logger := MultipleAsyncLogger{writerPerToken: writerPerToken, logCh: make(chan tokenizedFact, 20000), showInGlobalLogger: showInGlobalLogger}
 
 	go func() {
 		for {
 			tokenizedFact := <-logger.logCh
-			bts, err := json.Marshal(tokenizedFact.fact)
+			bts, err := json.Marshal(&tokenizedFact.fact)
 			if err != nil {
-				log.Printf("Error writing event to log file: %v", err)
+				log.Printf("Error marshaling json event for token %s: %v", tokenizedFact.token, err)
 				continue
+			}
+
+			if logger.showInGlobalLogger {
+				prettyJsonBytes, _ := json.MarshalIndent(&tokenizedFact.fact, "", "  ")
+				log.Println(string(prettyJsonBytes))
 			}
 
 			buf := bytes.NewBuffer(bts)
@@ -56,6 +62,8 @@ func NewMultipleAsyncLogger(writerPerToken map[string]io.WriteCloser) Consumer {
 				log.Printf("Error: unable to find writer for token %s", tokenizedFact.token)
 				continue
 			}
+
+			log.Fatalln()
 
 			if _, err := writer.Write(buf.Bytes()); err != nil {
 				log.Println(err)
