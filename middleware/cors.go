6c82852c4c00eb29b4b0e7ab56943006151ed266
Sergey Burykin <xtreding@gmail.com>
2020-08-10 12:19:06 -0400

    transferred all cors header writes to cors middleware

--- middleware/cors.go
+++ middleware/cors.go
@@ -6,11 +6,11 @@ import (
 
 func Cors(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r.Header.Add("Access-Control-Allow-Origin", "*")
-		r.Header.Add("Access-Control-Max-Age", "86400")
-		r.Header.Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		r.Header.Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Host")
-		r.Header.Add("Access-Control-Allow-Credentials", "true")
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Max-Age", "86400")
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Host")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
