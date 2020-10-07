54b09f1f34b47128002cf0210656940385cb4795
akhtyamovrr <ruslan@ksense.ai>
2020-10-07 13:32:18 +0300

    PMF-23 Fixed review changes

--- middleware/admin_auth.go
+++ middleware/admin_auth.go
@@ -11,6 +11,10 @@ type AdminToken struct {
 
 func (a *AdminToken) AdminAuth(main gin.HandlerFunc, errMsg string) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		if a.Token == "" {
+			c.JSON(http.StatusUnauthorized, ErrorResponse{Message: "admin_token must be configured"})
+			return
+		}
 		token := c.GetHeader("X-Admin-Token")
 		if token != a.Token {
 			c.JSON(http.StatusUnauthorized, ErrorResponse{Message: errMsg})
