871c472fef4a4ceacbea0d4ddbabb6a6f06ac589
Sergey Burykin <xtreding@gmail.com>
2020-10-20 13:21:13 +0300

    added telemetry events

--- telemetry/collector.go
+++ telemetry/collector.go
@@ -8,10 +8,12 @@ type Collector struct {
 	events uint64
 }
 
+//Event increment events counter
 func (c *Collector) Event() {
 	atomic.AddUint64(&c.events, 1)
 }
 
+//Cut return current value and set it to 0
 func (c *Collector) Cut() uint64 {
 	return atomic.SwapUint64(&c.events, 0)
 }
