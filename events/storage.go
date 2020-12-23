094b90bde634e8dcf2c6881f963934e0570889e2
akhtyamovrr <ruslan@ksense.ai>
2020-12-23 13:11:13 +0300

    PR changes - renaming, code cleaning, constants extraction

--- events/storage.go
+++ events/storage.go
@@ -8,7 +8,7 @@ type Storage interface {
 	io.Closer
 	Store(fileName string, payload []byte, alreadyUploadedTables map[string]bool) (map[string]*StoreResult, int, error)
 	StoreWithParseFunc(fileName string, payload []byte, skipTables map[string]bool, parseFunc func([]byte) (map[string]interface{}, error)) (map[string]*StoreResult, int, error)
-	SyncStore(tableName string, objects []map[string]interface{}, deleteValue string) (int, error)
+	SyncStore(tableName string, objects []map[string]interface{}, timeIntervalValue string) (int, error)
 	Fallback(events ...*FailedEvent)
 	Name() string
 	Type() string
