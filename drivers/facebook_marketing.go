e7efaaaf76bf73e637e830a702b24d8ff9aa9212
akhtyamovrr <ruslan@ksense.ai>
2021-01-28 16:58:43 +0300

    Implemented data loading from Facebook insights API

--- drivers/facebook_marketing.go
+++ drivers/facebook_marketing.go
@@ -3,15 +3,27 @@ package drivers
 import (
 	"context"
 	"errors"
+	"fmt"
+	fb "github.com/huandu/facebook/v2"
 	"github.com/jitsucom/eventnative/logging"
+	"strings"
 	"time"
 )
 
-const fbMarketingType = "facebook_marketing"
+const (
+	fbMarketingType = "facebook_marketing"
+)
+
+var keyFields = []string{"account_currency", "account_id", "account_name", "ad_id", "ad_name",
+	"adset_id", "adset_name", "campaign_id", "campaign_name", "objective", "buying_type",
+}
+
+var metrics = []string{"cpc", "cpm", "cpp", "ctr", "estimated_ad_recall_rate", "estimated_ad_recallers", "reach",
+	"unique_clicks", "unique_ctr", "frequency", "actions", "conversions"}
 
 type FacebookMarketingConfig struct {
-	adAccountId string
-	token       string
+	AccountId string `mapstructure:"account_id" json:"account_id,omitempty" yaml:"account_id,omitempty"`
+	Token     string `mapstructure:"token" json:"token,omitempty" yaml:"token,omitempty"`
 }
 
 type FacebookMarketing struct {
@@ -20,10 +32,10 @@ type FacebookMarketing struct {
 }
 
 func (fmc *FacebookMarketingConfig) Validate() error {
-	if fmc.adAccountId == "" {
+	if fmc.AccountId == "" {
 		return errors.New("[ad_account_id] is not configured")
 	}
-	if fmc.token == "" {
+	if fmc.Token == "" {
 		return errors.New("[token] is not configured")
 	}
 	return nil
@@ -57,8 +69,36 @@ func (fm *FacebookMarketing) GetAllAvailableIntervals() ([]*TimeInterval, error)
 	return intervals, nil
 }
 
+type FacebookInsightsResponse struct {
+	Data []map[string]interface{} `facebook:"data"`
+}
+
 func (fm *FacebookMarketing) GetObjectsFor(interval *TimeInterval) ([]map[string]interface{}, error) {
-	panic("implement me")
+	var fields []string
+	fields = append(fields, keyFields...)
+	fields = append(fields, metrics...)
+	res, err := fb.Get("/v9.0/act_"+fm.config.AccountId+"/insights", fb.Params{
+		"level":        "ad",
+		"fields":       strings.Join(fields, ","),
+		"access_token": fm.config.Token,
+		"time_range":   fm.buildTimeInterval(interval),
+	})
+	if err != nil {
+		return nil, err
+	}
+	var response FacebookInsightsResponse
+	if err = res.Decode(&response); err != nil {
+		return nil, err
+	}
+	logging.Debugf("[%s] Rows to sync: %d", interval.String(), len(response.Data))
+	return response.Data, nil
+}
+
+func (fm *FacebookMarketing) buildTimeInterval(interval *TimeInterval) string {
+	dayStart := interval.LowerEndpoint()
+	since := DAY.Format(dayStart)
+	until := DAY.Format(dayStart.AddDate(0, 0, 1))
+	return fmt.Sprintf("{'since': '%s', 'until': '%s'}", since, until)
 }
 
 func (fm *FacebookMarketing) Type() string {
