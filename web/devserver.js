441f14fc42b8c53b25447f11dda4f082b3478d11
vklimontovich <vladimir@jitsu.com>
2021-02-12 21:16:40 +0300

    #195: id() data persistence and few improvements along the way:
     - Rollup upgraded 1.0.0 --> 3.39
     - DevServer now runs rollup builds and watching for changes
     - Better typing in tracker interface

--- web/devserver.js
+++ web/devserver.js
@@ -1,21 +1,23 @@
 require('console-stamp')(console, 'HH:MM:ss.l');
 
 const fs = require('fs');
-const { spawn } = require('child_process');
+const {spawn} = require('child_process');
 const express = require('express');
 const path = require('path');
 const bodyParser = require("body-parser");
 
 const openBrowser = require('react-dev-utils/openBrowser');
-const { choosePort } = require('react-dev-utils/WebpackDevServerUtils');
+const {choosePort} = require('react-dev-utils/WebpackDevServerUtils');
+
+const rollup = require('rollup')
+const loadConfigFile = require('rollup/dist/loadConfigFile')
 
 
-
-
-const DEFAULT_PORT 				= 4000;
-const PORT 						= process.env.TRACK_DEMO_PORT || DEFAULT_PORT;
-const HOST 						= 'localhost';
-const DEFAULT_USE_PROD_FILES 	= true;
+const DEFAULT_PORT = 4000;
+const PORT = process.env.TRACK_DEMO_PORT || DEFAULT_PORT;
+const HOST = 'localhost';
+const DEFAULT_USE_PROD_FILES = true;
+const WATCH_AND_BUILD_TRACKER = process.env.WATCH_AND_BUILD_TRACKER || true;
 
 
 let globalPort = PORT;
@@ -24,6 +26,28 @@ let globalPort = PORT;
 const useProdFiles = process.env.USE_PROD_FILE === undefined ? DEFAULT_USE_PROD_FILES : process.env.USE_PROD_FILE;
 const testHtmlPath = path.resolve("./test.html");
 
+async function buildTracker() {
+    if (WATCH_AND_BUILD_TRACKER) {
+        console.log("Building JS SDK with Rollup...")
+        const {options, warnings} = await loadConfigFile(path.resolve(__dirname, 'rollup.config.js'), {format: 'es'});
+        console.log(`Rollup build currently have ${warnings.count} warnings`);
+        warnings.flush();
+        options.map(async option => {
+            const bundle = await rollup.rollup(option)
+            await Promise.all(option.output.map(bundle.write))
+            rollup.watch(option).on('event', event => {
+                if (event.code === 'BUNDLE_START') {
+                    console.log(`Building ${event.input}`);
+                } else if (event.code === 'BUNDLE_END') {
+                    console.log(`${event.input} has been built successfully!`)
+                } else if (event.code === 'ERROR') {
+                    console.error("Error", event.error)
+                }
+            })
+        })
+        console.log("Rollup build completed!")
+    }
+}
 
 function prepareJsFile(file) {
     let fullFilePath = path.join(__dirname, useProdFiles ? "./dist/web" : "./src", file);
@@ -61,13 +85,14 @@ devserver.use(bodyParser.text())
 devserver.use(bodyParser.json())
 
 devserver.get('/', (req, res) => {
-	res.sendFile(testHtmlPath);
+    res.sendFile(testHtmlPath);
 });
 
 devserver.use(express.static('demo'))
 //devserver.use(express.json());
 
-devserver.use(function(req, res, next) {
+
+devserver.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST');
     res.header('Access-Control-Expose-Headers', 'Content-Length');
@@ -96,7 +121,7 @@ devserver.get("/s/track.js", (req, res) => {
 devserver.get('/t/inline.js', (req, res) => {
     const toBool = str => str === "true";
     const asIs = str => str;
-    const accepted_params = {'key': asIs, 'segment_hook': toBool, 'tracking_host': asIs , 'cookie_domain': asIs, 'ga_hook': toBool, 'debug': toBool()};
+    const accepted_params = {'key': asIs, 'segment_hook': toBool, 'tracking_host': asIs, 'cookie_domain': asIs, 'ga_hook': toBool, 'debug': toBool()};
     if (!req.query['key']) {
         throw new Error("Mandatory key parameter is missing");
     }
@@ -124,23 +149,24 @@ devserver.get('/t/inline.js', (req, res) => {
     res.send(js);
 });
 
-choosePort(HOST, PORT).then(selectedPort => {
-	if (selectedPort == null) {
-		// We have not found a selectedPort.
-		return;
-	}
+choosePort(HOST, PORT).then(async selectedPort => {
+    if (selectedPort == null) {
+        // We have not found a selectedPort.
+        return;
+    }
 
-	globalPort = selectedPort;
+    globalPort = selectedPort;
 
-	devserver.listen(globalPort, () => {
-		console.log(`Started at ${globalPort}!`);
+    devserver.listen(globalPort, async () => {
+        await buildTracker();
+        console.log(`Started at ${globalPort}!`);
 
-		console.log('Opening browser...');
-		openBrowser('http://' + HOST + ':' + globalPort + '/');
+        // console.log('Opening browser...');
+        // openBrowser('http://' + HOST + ':' + globalPort + '/');
 
-		if (process.argv.indexOf('--watch') > -1) {
-			console.log('Watching changes...');
-			spawn('npm', ['run', 'watch'], { stdio: 'inherit' });
-		}
-	});
+        if (process.argv.indexOf('--watch') > -1) {
+            console.log('Watching changes...');
+            spawn('npm', ['run', 'watch'], {stdio: 'inherit'});
+        }
+    });
 });
