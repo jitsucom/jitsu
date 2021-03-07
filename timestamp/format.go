8689c256d4cc462620067b5da6ed7d7fa1c4403c
Maksim Lebedev <alessarik@gmail.com>
2021-03-07 14:13:29 +0300

    Allocate Layout For Logs

--- timestamp/format.go
+++ timestamp/format.go
@@ -8,6 +8,7 @@ const Layout = "2006-01-02T15:04:05.000000Z"
 const DayLayout = "20060102"
 const MonthLayout = "200601"
 const DashDayLayout = "2006-01-02"
+const LogsLayout = "2006-01-02 15:04:05"
 
 //TODO delete
 const DeprecatedLayout = "2006-01-02T15:04:05.000Z"
