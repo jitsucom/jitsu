a7276cd504fdffac039db835c5274ce172e4fff9
Maksim Lebedev <alessarik@gmail.com>
2021-03-14 17:48:53 +0300

    Validate S3 in batch mode at handler

--- handlers/destinations.go
+++ handlers/destinations.go
@@ -3,14 +3,15 @@ package handlers
 import (
 	"context"
 	"errors"
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 	"github.com/hashicorp/go-multierror"
 	"github.com/jitsucom/eventnative/adapters"
 	"github.com/jitsucom/eventnative/logging"
 	"github.com/jitsucom/eventnative/middleware"
 	"github.com/jitsucom/eventnative/storages"
-	"net/http"
-	"strings"
 )
 
 func DestinationsHandler(c *gin.Context) {
@@ -72,7 +73,11 @@ func testConnection(config *storages.DestinationConfig) error {
 			if err != nil {
 				return err
 			}
-			s3.Close()
+			defer s3.Close()
+			err = s3.ValidateWritePermission()
+			if err != nil {
+				return err
+			}
 		}
 
 		redshift, err := adapters.NewAwsRedshift(context.Background(), config.DataSource, config.S3, nil, map[string]string{})
@@ -119,6 +124,10 @@ func testConnection(config *storages.DestinationConfig) error {
 					return err
 				}
 				defer s3.Close()
+				err = s3.ValidateWritePermission()
+				if err != nil {
+					return err
+				}
 			} else if config.Google != nil && config.Google.Bucket != "" {
 				if err := config.Google.Validate(false); err != nil {
 					return err
