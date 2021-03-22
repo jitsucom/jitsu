027d6fa6c98730615f0c476880186061ee9c1a32
Vladimir Klimontovich <vladimir@jitsu.com>
2021-03-22 12:17:07 -0400

    Added build info to UI

--- configurator/frontend/craco.config.js
+++ configurator/frontend/craco.config.js
@@ -50,6 +50,7 @@ module.exports = {
           GIT_COMMIT_REF: JSON.stringify(process.env.COMMIT_REF || null),
           BUILD_TIMESTAMP: JSON.stringify(new Date().toISOString()),
           BUILD_ID: JSON.stringify(process.env.BUILD_ID || null),
+          COMMIT_REF: JSON.stringify(process.env.COMMIT_REF || null),
           BACKEND_API_BASE: JSON.stringify(process.env.BACKEND_API_BASE),
           NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production'),
           ANALYTICS_KEYS: JSON.stringify(process.env.ANALYTICS_KEYS || null),
