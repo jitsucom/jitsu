bffb2e02a4dbe447a76f968b0c0b98c35cdee5b0
Hovansky Alexander <alex.hovansky@gmail.com>
2021-03-26 00:41:51 +0300

    #213 SourcesList icon fix

--- configurator/frontend/src/ui/pages/SourcesPage/partials/SourcesList/SourcesListItem.tsx
+++ configurator/frontend/src/ui/pages/SourcesPage/partials/SourcesList/SourcesListItem.tsx
@@ -3,13 +3,14 @@ import React, { memo, useCallback, useMemo } from 'react';
 import { generatePath } from 'react-router-dom';
 import { Button, List } from 'antd';
 // @Icons
-import ColumnWidthOutlined from '@ant-design/icons/lib/icons/ColumnWidthOutlined';
 import EditOutlined from '@ant-design/icons/lib/icons/EditOutlined';
 import DeleteOutlined from '@ant-design/icons/lib/icons/DeleteOutlined';
 // @Types
 import { SourcesListItemProps as Props } from './SourcesList.types';
 // @Routes
 import { routes } from '@page/SourcesPage/routes';
+// @Styles
+import styles from './SourcesListItem.module.less';
 
 const SourcesListItemComponent = ({ sourceId, sourceProto, handleDeleteSource }: Props) => {
   const itemDescription = useMemo(() => <div>Source ID: {sourceId}</div>, [sourceId]);
@@ -19,11 +20,8 @@ const SourcesListItemComponent = ({ sourceId, sourceProto, handleDeleteSource }:
   return (
     <List.Item
       key={sourceId}
-      className="sources-list-item"
+      className={styles.sourcesListItem}
       actions={[
-        <Button icon={<ColumnWidthOutlined />} key="edit" shape="round" onClick={() => alert('Mappings')}>
-          Mappings
-        </Button>,
         <Button icon={<EditOutlined />} key="edit" shape="round" href={generatePath(routes.editExact, { sourceId })}>
           Edit
         </Button>,
