95c1b033673951dddf05a54253aa6100e932091f
Hovansky Alexander <alex.hovansky@gmail.com>
2021-03-30 17:58:38 +0300

    #213 Switch instead of checkbox

--- configurator/frontend/src/ui/pages/SourcesPage/partials/_common/SourceForm/SourceFormDestinations.tsx
+++ configurator/frontend/src/ui/pages/SourcesPage/partials/_common/SourceForm/SourceFormDestinations.tsx
@@ -1,7 +1,7 @@
 // @Libs
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Avatar, Checkbox, Form, List } from 'antd';
+import { Avatar, Form, List, Switch } from 'antd';
 // @Services
 import ApplicationServices from '@service/ApplicationServices';
 // @Components
@@ -9,14 +9,36 @@ import { getIconSrc } from '@page/DestinationsPage/partials/DestinationsList/Des
 import { loadDestinations } from '@page/DestinationsPage/commons';
 import { CenteredError, CenteredSpin } from '@./lib/components/components';
 // @Types
-import { SourceFormDestinationsProps } from '@page/SourcesPage/partials/_common/SourceForm/SourceForm.types';
+import { DestinationConfig } from '@service/destinations';
+import { SourceFormDestinationsProps } from './SourceForm.types';
 // @Hooks
 import useLoader from '@./lib/commons/useLoader';
 // @Routes
 import { destinationPageRoutes } from '@page/DestinationsPage/DestinationsPage.routes';
 
-const SourceFormDestinations = ({ initialValues }: SourceFormDestinationsProps) => {
+const SourceFormDestinations = ({ initialValues, form }: SourceFormDestinationsProps) => {
   const [error, destinations] = useLoader(async() => await loadDestinations(ApplicationServices.get()));
+
+  const [checkedDestinations, setCheckedDestinations] = useState<string[]>(initialValues.destinations ?? []);
+
+  const handleChange = useCallback((config: DestinationConfig) => (checked: boolean) => {
+    let newDestinations;
+
+    if (checked && !checkedDestinations.includes(config.id)) {
+      newDestinations = [...checkedDestinations, config.id];
+    } else if (!checked) {
+      newDestinations = checkedDestinations.filter((destination: string) => destination !== config.id);
+    }
+
+    setCheckedDestinations(newDestinations);
+
+    const formValues = form.getFieldsValue();
+
+    form.setFieldsValue({
+      ...formValues,
+      destinations: newDestinations
+    });
+  }, [checkedDestinations, form]);
 
   if (error) {
     return <CenteredError error={error} />
@@ -43,30 +65,28 @@ const SourceFormDestinations = ({ initialValues }: SourceFormDestinationsProps) 
         initialValue={initialValues.destinations}
         rules={destinations.length > 0 && [{ required: true, message: 'You have to choose at least one destination.' }]}
       >
-        <Checkbox.Group>
-          <List key="list" className="destinations-list" itemLayout="horizontal">
-            {destinations.map((config) => {
-              const description = config.describe();
+        <List key="list" className="destinations-list" itemLayout="horizontal">
+          {destinations.map((config) => {
+            const description = config.describe();
 
-              return <List.Item key={config.id}>
-                <label htmlFor={config.id} className="ant-checkbox-group-label">
-                  <List.Item.Meta
-                    avatar={<div className="ant-checkbox-group-label__avatar">
-                      <Checkbox id={config.id} value={config.id} />
-                      <Avatar shape="square" src={getIconSrc(config.type)}/>
-                    </div>}
-                    description={<span className="destinations-list-show-connect-command">{description.displayURL}</span>}
-                    title={config.connectionTestOk
-                      ? config.id
-                      : <span className="destinations-list-failed-connection">
-                        <b>!</b> {config.id}
-                      </span>}
-                  />
-                </label>
-              </List.Item>
-            })}
-          </List>
-        </Checkbox.Group>
+            return <List.Item key={config.id}>
+              <label htmlFor={config.id} className="ant-switch-group-label">
+                <List.Item.Meta
+                  avatar={<div className="ant-switch-group-label__avatar">
+                    <Switch onChange={handleChange(config)} checked={checkedDestinations.includes(config.id)} />
+                    <Avatar shape="square" src={getIconSrc(config.type)}/>
+                  </div>}
+                  description={<span className="destinations-list-show-connect-command">{description.displayURL}</span>}
+                  title={config.connectionTestOk
+                    ? config.id
+                    : <span className="destinations-list-failed-connection">
+                      <b>!</b> {config.id}
+                    </span>}
+                />
+              </label>
+            </List.Item>
+          })}
+        </List>
       </Form.Item>
     </>
   );
