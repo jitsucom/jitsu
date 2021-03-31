23cbfe837b400b621d3da1f611347af48fc82f60
Hovansky Alexander <alex.hovansky@gmail.com>
2021-03-31 10:14:47 +0300

    #213 Validate Config tab by Test connection click

--- configurator/frontend/src/ui/pages/SourcesPage/partials/_common/SourceForm/SourceForm.tsx
+++ configurator/frontend/src/ui/pages/SourcesPage/partials/_common/SourceForm/SourceForm.tsx
@@ -39,16 +39,14 @@ const sourceFormCleanFunctions = {
     result += tabs[key].errorsCount;
     return result;
   }, 0),
-  getErrors: (tabs: TabsMap) => (<ul>
-    {Object
-      .keys(tabs)
-      .reduce((result: React.ReactNode[], key: string) => {
-        if (tabs[key].errorsCount > 0) {
-          result.push(<li key={key}>{tabs[key].errorsCount} error(s) at `{tabs[key].name}` tab;</li>)
-        }
+  getErrors: (tabs: TabsMap, tabsKeys: string[]) => (<ul>
+    {tabsKeys.reduce((result: React.ReactNode[], key: string) => {
+      if (tabs[key].errorsCount > 0) {
+        result.push(<li key={key}>{tabs[key].errorsCount} error(s) at `{tabs[key].name}` tab;</li>)
+      }
 
-        return result;
-      }, [])}
+      return result;
+    }, [])}
   </ul>),
   getTabName: (currentTab: Tab) => currentTab.errorsCount === 0
     ? currentTab.name
@@ -67,10 +65,11 @@ const SourceForm = ({
 
   const forceUpdate = useForceUpdate();
 
-  const [isVisiblePopover, switchIsVisiblePopover] = useState<boolean>();
-  const [connectionTestPending, setConnectionTestPending] = useState<boolean>();
+  const [isVisiblePopover, switchIsVisiblePopover] = useState<boolean>(false);
+  const [isVisibleTestConnectionPopover, switchIsVisibleTestConnectionPopover] = useState<boolean>(false);
+  const [connectionTestPending, setConnectionTestPending] = useState<boolean>(false);
 
-  const mutableRefObject = useRef<{ tabs: TabsMap; submitOnce: boolean; }>({
+  const mutableRefObject = useRef<{ tabs: TabsMap; submitOnce: boolean; connectedOnce: boolean; }>({
     tabs: {
       config: {
         name: 'Config',
@@ -91,24 +90,11 @@ const SourceForm = ({
         errorsCount: 0
       }
     },
-    submitOnce: false
+    submitOnce: false,
+    connectedOnce: false
   });
 
   const services = useMemo(() => ApplicationServices.get(), []);
-
-  const handleTestConnectionClick = useCallback(async() => {
-    setConnectionTestPending(true);
-
-    try {
-      await services.backendApiClient.post('sources/test', {});
-
-      message.success('Successfully connected!');
-    } catch (error) {
-      handleError(error, 'Unable to test connection with filled data');
-    } finally {
-      setConnectionTestPending(false);
-    }
-  }, [services]);
 
   const handleTabSubmit = useCallback(async(key: string) => {
     const currentTab = mutableRefObject.current.tabs[key];
@@ -122,10 +108,12 @@ const SourceForm = ({
         ...mutableRefObject.current.tabs,
         [key]: tabToUpdate
       };
+
+      throw errors;
     }
   }, []);
 
-  const handleSubmit = useCallback(() => {
+  const handleFormSubmit = useCallback(() => {
     switchIsVisiblePopover(true);
 
     mutableRefObject.current.submitOnce = true;
@@ -144,12 +132,37 @@ const SourceForm = ({
   const handleFormValuesChange = useCallback(() => {
     if (mutableRefObject.current.submitOnce) {
       Promise.all(Object.keys(mutableRefObject.current.tabs).map((key: string) => handleTabSubmit(key))).then(() => forceUpdate());
+    } else if (mutableRefObject.current.connectedOnce) {
+      handleTabSubmit('config').then(() => forceUpdate());
     }
   }, [forceUpdate, handleTabSubmit]);
 
   const handleCancel = useCallback(() => history.push(routes.root), [history]);
 
   const handlePopoverClose = useCallback(() => switchIsVisiblePopover(false), []);
+  const handleTestConnectionPopoverClose = useCallback(() => switchIsVisibleTestConnectionPopover(false), []);
+
+  const handleTestConnectionClick = useCallback(async() => {
+    setConnectionTestPending(true);
+
+    mutableRefObject.current.connectedOnce = true;
+
+    try {
+      await handleTabSubmit('config');
+
+      try {
+        await services.backendApiClient.post('sources/test', {});
+
+        message.success('Successfully connected!');
+      } catch(e) {
+        handleError(e, 'Service is temporary unavailable');
+      }
+    } catch (error) {
+      handleError(error, 'Unable to test connection with filled data');
+    } finally {
+      setConnectionTestPending(false);
+    }
+  }, [handleTabSubmit, services]);
 
   return (
     <>
@@ -173,7 +186,7 @@ const SourceForm = ({
 
       <div className="flex-shrink border-t pt-2">
         <Popover
-          content={sourceFormCleanFunctions.getErrors(mutableRefObject.current.tabs)}
+          content={sourceFormCleanFunctions.getErrors(mutableRefObject.current.tabs, Object.keys(mutableRefObject.current.tabs))}
           title={<p className={styles.popoverTitle}><span>{capitalize(formMode)} source form errors:</span> <CloseOutlined onClick={handlePopoverClose} /></p>}
           trigger="click"
           visible={isVisiblePopover && sourceFormCleanFunctions.getErrorsCount(mutableRefObject.current.tabs) > 0}
@@ -185,20 +198,27 @@ const SourceForm = ({
             size="large"
             className="mr-3"
             loading={isRequestPending}
-            onClick={handleSubmit}
+            onClick={handleFormSubmit}
           >
             <span style={{ textTransform: 'capitalize' }}>{formMode}</span>&nbsp;source
           </Button>
         </Popover>
 
-        <Button
-          size="large"
-          className="mr-3"
-          type="dashed"
-          loading={connectionTestPending}
-          onClick={handleTestConnectionClick}
-          icon={<ApiOutlined/>}
-        >Test connection</Button>
+        <Popover
+          content={sourceFormCleanFunctions.getErrors(mutableRefObject.current.tabs, ['config'])}
+          title={<p className={styles.popoverTitle}><span>Config form errors:</span> <CloseOutlined onClick={handleTestConnectionPopoverClose} /></p>}
+          trigger="click"
+          visible={isVisibleTestConnectionPopover && mutableRefObject.current.tabs.config.errorsCount > 0}
+        >
+          <Button
+            size="large"
+            className="mr-3"
+            type="dashed"
+            loading={connectionTestPending}
+            onClick={handleTestConnectionClick}
+            icon={<ApiOutlined/>}
+          >Test connection</Button>
+        </Popover>
 
         <Button
           type="default"
