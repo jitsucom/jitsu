7ce91b99c02924a470bd21bd76562486816d9c4e
vklimontovich <vladimir@jitsu.com>
2021-04-01 13:11:35 +0300

    - Destination definition types - Defined postgres with a new method

---
+++ configurator/frontend/src/catalog/destinations/lib/common.tsx
@@ -0,0 +1,28 @@
+import {Parameter, selectionType, stringType} from "../../sources/types";
+
+export const modeParameter = (constValue?: string): Parameter => {
+    return {
+        id: "_formData.mode",
+        displayName: "",
+        documentation: <>
+            In steam mode the data will be send to destination instantly.
+        </>,
+        required: true,
+        defaultValue: constValue ?? "stream",
+        constant: constValue ?? undefined,
+        type: constValue ? stringType : selectionType(["stream", "batch"], 1)
+    }
+}
+
+export const tableName = ((): Parameter => {
+    return {
+        id: "_formData.tableName",
+        displayName: "Table Name",
+        documentation: <>
+            Table name (or table name template)
+        </>,
+        required: true,
+        defaultValue: "events",
+        type: stringType
+    }
+})();
