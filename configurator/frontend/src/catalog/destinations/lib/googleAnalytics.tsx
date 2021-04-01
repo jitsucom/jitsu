b091290ffe6434ddf5f48b6ac06447c61fd3bafd
vklimontovich <vladimir@jitsu.com>
2021-04-01 17:31:20 +0300

    Google analytics and facebook destinations

--- configurator/frontend/src/catalog/destinations/lib/googleAnalytics.tsx
+++ configurator/frontend/src/catalog/destinations/lib/googleAnalytics.tsx
@@ -1,3 +1,22 @@
-const googleAnalytics = {}
+import { Destination } from '../types';
+import { filteringExpressionDocumentation, modeParameter, tableName } from './common';
+import { stringType } from '../../sources/types';
+
+const googleAnalytics: Destination = {
+  displayName: "GoogleAnalytics",
+  id: "google_analytics",
+  parameters: [
+    modeParameter("stream"),
+    tableName(filteringExpressionDocumentation),
+    {
+      id: "_formData.gaTrackingId",
+      displayName: "Tracking ID",
+      required: true,
+      type: stringType
+    }
+
+  ],
+  ui: undefined
+}
 
 export default googleAnalytics;
