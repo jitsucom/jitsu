17e1187dda3de1d94cec8622bfc18cc2d35855ae
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-06 16:43:23 +0300

    Confirmation modal by click on Delete button

--- configurator/frontend/src/ui/pages/SourcesPage/partials/SourcesList/SourcesListItem.tsx
+++ configurator/frontend/src/ui/pages/SourcesPage/partials/SourcesList/SourcesListItem.tsx
@@ -1,11 +1,12 @@
 // @Libs
 import React, { memo, useCallback, useMemo } from 'react';
 import { generatePath, NavLink } from 'react-router-dom';
-import { Button, List } from 'antd';
+import { Button, List, Modal } from 'antd';
 import cn from 'classnames';
 // @Icons
 import EditOutlined from '@ant-design/icons/lib/icons/EditOutlined';
 import DeleteOutlined from '@ant-design/icons/lib/icons/DeleteOutlined';
+import ExclamationCircleOutlined from '@ant-design/icons/lib/icons/ExclamationCircleOutlined';
 // @Types
 import { SourcesListItemProps as Props } from './SourcesList.types';
 // @Routes
@@ -16,7 +17,21 @@ import styles from './SourcesListItem.module.less';
 const SourcesListItemComponent = ({ sourceId, sourceProto, handleDeleteSource }: Props) => {
   const itemDescription = useMemo(() => <div>Source ID: {sourceId}</div>, [sourceId]);
 
-  const handleDelete = useCallback(() => handleDeleteSource(sourceId), [sourceId, handleDeleteSource]);
+  const handleDelete = useCallback(() => {
+    Modal.confirm({
+      title: 'Please confirm deletion of source',
+      icon: <ExclamationCircleOutlined/>,
+      content: <span>Are you sure you want to delete <strong>{sourceProto?.displayName}</strong> source?</span>,
+      okText: 'Delete',
+      cancelText: 'Cancel',
+      onOk: () => {
+        handleDeleteSource(sourceId)
+      },
+      onCancel: () => {
+      }
+    });
+  }
+  , [sourceId, handleDeleteSource, sourceProto?.displayName]);
 
   return (
     <List.Item
