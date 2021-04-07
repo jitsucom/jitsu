b6c88ce3e62aaea75cb533de2cb185583196cbe8
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-07 17:01:22 +0300

    #300 form configurator blank component

---
+++ configurator/frontend/src/ui/components/molecule/ConfigurableFieldsForm/ConfigurableFieldsForm.tsx
@@ -0,0 +1,14 @@
+// @Libs
+import React from 'react';
+// @Types
+import { Props } from './ConfigurableFieldsForm.types';
+
+const ConfigurableFieldsForm = ({}: Props) => {
+  return (
+    <div>ConfigurableFieldsForm</div>
+  );
+};
+
+ConfigurableFieldsForm.displayName = 'ConfigurableFieldsForm';
+
+export { ConfigurableFieldsForm };
