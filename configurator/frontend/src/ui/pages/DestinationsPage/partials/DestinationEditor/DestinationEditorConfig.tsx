47027b16bcaf7023b6fafdeb8063b13dc9c2de07
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-12 08:16:29 +0300

    #300 temo commit to make rebase

--- configurator/frontend/src/ui/pages/DestinationsPage/partials/DestinationEditor/DestinationEditorConfig.tsx
+++ configurator/frontend/src/ui/pages/DestinationsPage/partials/DestinationEditor/DestinationEditorConfig.tsx
@@ -6,12 +6,10 @@ import { ConfigurableFieldsForm } from '@molecule/ConfigurableFieldsForm';
 // @Types
 import { Props } from './DestinationEditor.types';
 
-const DestinationEditorConfig = ({ destination }: Props) => {
-  const [form] = Form.useForm();
-
+const DestinationEditorConfig = ({ destination, form }: Props) => {
   return(
-    <Form name="destination-config" form={form}>
-      <ConfigurableFieldsForm fieldsParamsList={destination.parameters} />
+    <Form name="destination-config" form={form} autoComplete="off">
+      <ConfigurableFieldsForm fieldsParamsList={destination.parameters} form={form} />
     </Form>
   )
 };
