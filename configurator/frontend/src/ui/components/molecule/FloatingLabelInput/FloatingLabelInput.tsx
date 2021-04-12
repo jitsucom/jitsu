592533f8eccfea80cf2557240044be8337d7d433
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-12 10:44:52 +0300

    Autocomplete input styles fixes

--- configurator/frontend/src/ui/components/molecule/FloatingLabelInput/FloatingLabelInput.tsx
+++ configurator/frontend/src/ui/components/molecule/FloatingLabelInput/FloatingLabelInput.tsx
@@ -1,19 +1,20 @@
 // @Libs
 import * as React from 'react';
 import { Form, Input } from 'antd';
+import cn from 'classnames';
 // @Components
-import { FloatingLabel } from '../../atom/FloatingLabel';
+import { FloatingLabel } from '@atom/FloatingLabel';
 // @Types
 import { Props } from './FloatingLabelInput.types';
 
-const FloatingLabelInputComponent = ({ formName, name, rules, floatingLabelText, prefix, inputType = 'text', size }: Props) => {
+const FloatingLabelInputComponent = ({ formName, name, rules, floatingLabelText, prefix, inputType = 'text', size, className }: Props) => {
   return (
     <Form.Item noStyle shouldUpdate={(prevValues, currentValues) => prevValues[name] !== currentValues[name]}>
       {/* ToDo: getInternalHooks what is it??? */}
       {({ getFieldValue }) => (
         <Form.Item name={name} rules={rules}>
           <Input
-            className="with-floating-label"
+            className={cn('with-floating-label', className)}
             prefix={prefix}
             suffix={<FloatingLabel className={prefix && 'with-prefix'} size={size} hasValue={getFieldValue(name)} htmlFor={`${formName}_${name}`} render={floatingLabelText} />}
             type={inputType}
