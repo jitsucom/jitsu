c58ad18da0d14b0846512da8af0a50939f756d0d
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-19 21:20:25 +0300

    Destinations list fixes

--- configurator/frontend/src/ui/pages/DestinationsPage/partials/DestinationsList/DestinationsList.tsx
+++ configurator/frontend/src/ui/pages/DestinationsPage/partials/DestinationsList/DestinationsList.tsx
@@ -1,34 +1,46 @@
-/* eslint-disable */
-import * as React from 'react';
-import { ReactNode, useState } from 'react';
-import { DestinationConfig, destinationConfigTypes, destinationsByTypeId } from '@./lib/services/destinations';
-import { Avatar, Button, Dropdown, Form, List, Menu, message, Modal, Popover, Tooltip } from 'antd';
-import DatabaseOutlined from '@ant-design/icons/lib/icons/DatabaseOutlined';
-import DeleteOutlined from '@ant-design/icons/lib/icons/DeleteOutlined';
-import EditOutlined from '@ant-design/icons/lib/icons/EditOutlined';
-import ExclamationCircleOutlined from '@ant-design/icons/lib/icons/ExclamationCircleOutlined';
-import PlusOutlined from '@ant-design/icons/lib/icons/PlusOutlined';
-import '../../DestinationsPage.less';
-
+// @Libs
+import React, { useCallback } from 'react';
+import { generatePath, useHistory } from 'react-router-dom';
+import { Button, Dropdown, List, message, Modal, Popover, Tooltip } from 'antd';
+// @Hooks
+import useLoader from '@hooks/useLoader';
+// @Services
+import ApplicationServices from '@service/ApplicationServices';
+import {
+  destinationsReferenceList,
+  destinationsReferenceMap,
+  getGeneratedPath,
+  loadDestinations
+} from '@page/DestinationsPage/commons';
+import { ConnectionDescription, DestinationConfig } from '@service/destinations';
+// @Components
 import {
   ActionLink,
-  Align, CenteredError, CenteredSpin,
+  Align,
+  CenteredError,
+  CenteredSpin,
   CodeInline,
   CodeSnippet,
-  handleError,
-  LoadableComponent
+  handleError
 } from '@./lib/components/components';
-import ApplicationServices from '../../../../../lib/services/ApplicationServices';
-import { copyToClipboard } from '@./lib/commons/utils';
-import Icon from '@ant-design/icons';
-import { loadDestinations } from "@page/DestinationsPage/commons";
-import useLoader from "@./lib/commons/useLoader";
-import { useHistory } from "react-router-dom";
+import { EmptyList } from '@molecule/EmptyList';
+import { DropDownList } from '@molecule/DropDownList';
+import { ListItem } from '@molecule/ListItem';
 import { LabelWithTooltip } from '@atom/LabelWithTooltip';
+// @Icons
+import DatabaseOutlined from '@ant-design/icons/lib/icons/DatabaseOutlined';
+import PlusOutlined from '@ant-design/icons/lib/icons/PlusOutlined';
+import ExclamationCircleOutlined from '@ant-design/icons/lib/icons/ExclamationCircleOutlined';
+// @Styles
+import styles from './DestinationsList.module.less';
+// @Utils
+import { copyToClipboard } from '@./lib/commons/utils';
+// @Routes
+import { destinationPageRoutes } from '@page/DestinationsPage/DestinationsPage.routes';
 
 export function getIconSrc(destinationType: string): string {
   try {
-    const icon = require('../../../../../icons/destinations/' + destinationType + '.svg');
+    const icon = require('/src/icons/destinations/' + destinationType + '.svg');
     return icon.default;
   } catch (e) {
     console.log('Icon for ' + destinationType + ' is not found');
@@ -38,46 +50,46 @@ export function getIconSrc(destinationType: string): string {
 
 export function getIcon(destinationType: string): any {
   let src = getIconSrc(destinationType);
-  return src ? <img src={src} className="destination-type-icon" alt="[destination]"/> : <DatabaseOutlined/>;
+  return src
+    ? <img src={src} className="destination-type-icon" alt="[destination]"/>
+    : <DatabaseOutlined/>;
 }
 
-type DestinationComponentProps = {
-  config: DestinationConfig
-  destinations?: DestinationConfig[]
-  onChange?: (destinations: DestinationConfig[]) => void
-};
-
-function DestinationTitle({ config }: DestinationComponentProps) {
-  let configTitle = config.connectionTestOk ? <>{config.id}</> :
-    <Tooltip
-      trigger={['click', 'hover']}
-      title={
-        <>
-          Last connection test failed with <b><i>'{config.connectionErrorMessage}'</i></b>. Destination might be not
-          accepting data. Please, go to editor and fix the connection settings
-        </>
-      }>
-        <span className="destinations-list-failed-connection">
-          <b>!</b> {config.id}
-        </span>
-    </Tooltip>;
-  if (config.comment) {
-    return <LabelWithTooltip documentation={config.comment} render={configTitle} />;
-  } else {
-    return configTitle;
-  }
-}
-
-function DestinationRow({ config, destinations, onChange }: DestinationComponentProps) {
+const DestinationsList = () => {
   const history = useHistory();
-  let description = config.describe();
-  let descriptionComponent;
-  if (!description.commandLineConnect) {
-    descriptionComponent = description.displayURL;
-  } else {
-    let codeSnippet;
-    if (description.commandLineConnect.indexOf('\n') < 0) {
-      codeSnippet = <>
+
+  const [error, destinations, updateDestinations] = useLoader(async() => await loadDestinations(ApplicationServices.get()));
+
+  const getTitle = useCallback((dst: DestinationConfig) => {
+    const configTitle = dst.connectionTestOk
+      ? <>{dst.id}</> :
+      <Tooltip
+        trigger={['click', 'hover']}
+        title={
+          <>
+            Last connection test failed with <b><i>'{dst.connectionErrorMessage}'</i></b>. Destination might be not
+            accepting data. Please, go to editor and fix the connection settings
+          </>
+        }>
+        <strong className={styles.errorName}>
+          <b>!</b> {dst.id}
+        </strong>
+      </Tooltip>;
+
+    return dst.comment
+      ? <LabelWithTooltip documentation={dst.comment} render={configTitle} />
+      : configTitle;
+  }, []);
+
+  const getDescription = useCallback((dst: DestinationConfig) => {
+    const description = dst.description ?? {} as ConnectionDescription;
+
+    if (!description.commandLineConnect) {
+      return description.displayURL;
+    }
+
+    const codeSnippet = description.commandLineConnect.indexOf('\n') < 0
+      ? <>
         <div>
           <CodeInline>{description.commandLineConnect}</CodeInline>
         </div>
@@ -90,13 +102,12 @@ function DestinationRow({ config, destinations, onChange }: DestinationComponent
             Copy command to clipboard
           </ActionLink>
         </Align>
-      </>;
-    } else {
-      codeSnippet = <CodeSnippet className="destinations-list-multiline-code" language="bash">
+      </>
+      : <CodeSnippet className="destinations-list-multiline-code" language="bash">
         {description.commandLineConnect}
-      </CodeSnippet>
-    }
-    descriptionComponent = <Popover
+      </CodeSnippet>;
+
+    return <Popover
       placement="topLeft"
       content={
         <>
@@ -105,97 +116,98 @@ function DestinationRow({ config, destinations, onChange }: DestinationComponent
         </>
       }
       trigger="click">
-      <span className="destinations-list-show-connect-command">{description.displayURL}</span>
+      <span className={styles.description}>{description.displayURL}</span>
     </Popover>;
-  }
+  }, []);
 
-  return <List.Item
-    key={config.id}
-    actions={[
-      <Button icon={<EditOutlined/>} key="edit" shape="round" onClick={() => {
-        history.push(`/destinations/edit/${config.id}`)
-      }}>Edit</Button>,
-      <Button icon={<DeleteOutlined/>} key="delete" shape="round" onClick={() => {
-        Modal.confirm({
-          title: 'Please confirm deletion of destination',
-          icon: <ExclamationCircleOutlined/>,
-          content: 'Are you sure you want to delete ' + config.id + ' destination?',
-          okText: 'Delete',
-          cancelText: 'Cancel',
-          onOk: () => {
-            destinations = destinations.filter(dest => dest.id !== config.id);
-            onChange(destinations);
-          },
-          onCancel: () => {
-          }
-        });
-      }}>
-        Delete
-      </Button>
-    ]}
-    className="destination-list-item">
-    <List.Item.Meta
-      avatar={<Avatar shape="square" src={getIconSrc(config.type)}/>}
-      title={<DestinationTitle config={config} />}
-      description={
-        <>
-          {descriptionComponent}
-          <br/>
-          mode: {config.mode}
-        </>
+  const getMode = useCallback((mode: string) => mode
+    ? <span className={styles.mode}>mode: {mode}</span>
+    : undefined, []);
+
+  const saveDestinations = useCallback(async(id: string) => {
+    const appServices = ApplicationServices.get();
+
+    const newDestinations = destinations.filter(dest => dest.id !== id);
+
+    try {
+      await appServices.storageService.save('destinations', { destinations: newDestinations }, appServices.activeProject.id);
+
+      updateDestinations(newDestinations);
+    } catch (errors) {
+      handleError(errors, 'Unable to delete destination at this moment, please try later.')
+    }
+  }, [updateDestinations, destinations]);
+
+  const handleDeleteAction = useCallback((id: string) => () => {
+    Modal.confirm({
+      title: 'Please confirm deletion of destination',
+      icon: <ExclamationCircleOutlined/>,
+      content: 'Are you sure you want to delete ' + id + ' destination?',
+      okText: 'Delete',
+      cancelText: 'Cancel',
+      onOk: () => {
+        saveDestinations(id);
       }
-    />
-  </List.Item>;
+    });
+  }, [saveDestinations]);
 
-}
+  const handleEditAction = useCallback((id: string) => () => history.push(generatePath(destinationPageRoutes.editDestination, { id })), [history]);
 
-export default function DestinationsList() {
-  const [error, destinations, updateDestinations] = useLoader(async() => await loadDestinations(ApplicationServices.get()))
-  const history = useHistory();
   if (error) {
-    return <CenteredError error={error} />
+    return <CenteredError error={error} />;
   } else if (!destinations) {
-    return <CenteredSpin />
+    return <CenteredSpin />;
+  } else if (destinations.length === 0) {
+    return <EmptyList
+      list={
+        <DropDownList
+          getPath={getGeneratedPath}
+          list={destinationsReferenceList}
+          filterPlaceholder="Filter by destination name"
+        />
+      }
+      title="Destinations list is still empty"
+      unit="destination"
+    />;
   }
-  let componentList = [
-    <List key="list" className="destinations-list" itemLayout="horizontal" header={(
-      <Dropdown trigger={['click']} overlay={(
-        <Menu className="destinations-list-add-menu">
-          {destinationConfigTypes.map((type) => (
-            <Menu.Item
-              key={type.name}
-              icon={
-                <Icon
-                  component={() => (
-                    <img
-                      height={16}
-                      width={16}
-                      src={getIconSrc(type.type)}
-                      className="destination-type-icon"
-                      alt="[destination]"
-                    />
-                  )}
-                />
-              }
-              onClick={() => history.push(`/destinations/new/${(type.type)}`)}>
-              Add {type.name}
-            </Menu.Item>
-          ))}
-        </Menu>
-      )}>
-        <Button type="primary" icon={<PlusOutlined/>}>
-          Add destination
-        </Button>
-      </Dropdown>
-    )} split={true}>
-      {destinations.map((config) => <DestinationRow
-          key={config.id}
-                                                    config={config}
-                                                    destinations={destinations}
-                                                    onChange={updateDestinations}
-      />)}
-    </List>
-  ];
 
-  return <>{componentList}</>;
-}
+  return <>
+    <div className="mb-5">
+      <Dropdown
+        trigger={['click']}
+        overlay={
+          <DropDownList
+            getPath={getGeneratedPath}
+            list={destinationsReferenceList}
+            filterPlaceholder="Filter by destination name"
+          />
+        }
+      >
+        <Button type="primary" icon={<PlusOutlined />}>Add source</Button>
+      </Dropdown>
+    </div>
+
+    <List key="list" className="destinations-list" itemLayout="horizontal" split={true}>
+      {
+        destinations.map((dst: DestinationConfig) => {
+          const reference = destinationsReferenceMap[dst.type];
+
+          return <ListItem
+            additional={getMode(dst.mode)}
+            description={getDescription(dst)}
+            icon={reference?.ui?.icon}
+            title={getTitle(dst)}
+            id={dst.id}
+            key={dst.id}
+            actions={[
+              { key: 'edit', method: handleEditAction, title: 'Edit' },
+              { key: 'delete', method: handleDeleteAction, title: 'Delete' }
+            ]}
+          />
+        })
+      }
+    </List>
+  </>
+};
+
+export { DestinationsList };
