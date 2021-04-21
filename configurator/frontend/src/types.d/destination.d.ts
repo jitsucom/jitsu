3f163ad1d8d467ca5b9c83f1d1f56f347c626c2c
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-21 12:59:19 +0300

    #300 Destinations list fixes after removing class-based code

--- configurator/frontend/src/types.d/destination.d.ts
+++ configurator/frontend/src/types.d/destination.d.ts
@@ -1,3 +1,5 @@
+declare type DestinationType = 'postgres' | 'bigquery' | 'redshift' | 'clickhouse' | 'snowflake' | 'facebook' | 'google_analytics';
+
 declare interface DestinationData {
   // private _mappings: FieldMappings = new FieldMappings([], true);
   // protected readonly _uid = randomId();
@@ -5,12 +7,14 @@ declare interface DestinationData {
   // protected readonly _type: string;
   // protected readonly _onlyKeys = [];
 
+  _type: DestinationType;
   _id: string;
+  _uid: string;
+  _comment: string;
   _connectionTestOk: boolean;
   _connectionErrorMessage: React.ReactNode;
+  _mode: 'batch' | 'stream';
   _formData: {
     [key: string]: any;
   };
-
-  [key: string]: any;
 }
--- configurator/frontend/src/ui/pages/DestinationsPage/DestinationsPage.tsx
+++ configurator/frontend/src/ui/pages/DestinationsPage/DestinationsPage.tsx
@@ -1,18 +1,81 @@
 // @Libs
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Route, Switch } from 'react-router-dom';
 // @Pages
 import { DestinationsList } from './partials/DestinationsList';
 import { DestinationEditor } from '@page/DestinationsPage/partials/DestinationEditor/DestinationEditor';
 // @Routes
 import { destinationPageRoutes } from './DestinationsPage.routes';
+// @Hooks
+import useLoader from '@hooks/useLoader';
+// @Services
+import ApplicationServices from '@service/ApplicationServices';
+// @Hocs
+import { getComponent } from '@hocs/getComponent';
+// @Components
+import { CenteredError, CenteredSpin } from '@./lib/components/components';
+import { EmptyList } from '@molecule/EmptyList';
+import { DropDownList } from '@molecule/DropDownList';
+// @Utils
+import { destinationsReferenceList, getGeneratedPath } from '@page/DestinationsPage/commons';
+
+export interface CollectionDestinationData {
+  destinations: DestinationData[];
+  _lastUpdated?: string;
+}
+
+export interface CommonDestinationPageProps {
+  destinations: DestinationData[];
+}
 
 export const DestinationsPage = () => {
+  const services = useMemo(() => ApplicationServices.get(), []);
+
+  const [error, destinations, updateDestinations] = useLoader(
+    async() => await services.storageService.get('destinations', services.activeProject.id)
+  );
+
+  const additionalProps = useMemo(() => ({
+    destinations: destinations?.destinations
+  }), [destinations]);
+
+  if (error) {
+    return <CenteredError error={error} />;
+  } else if (!destinations) {
+    return <CenteredSpin />;
+  } else if (destinations.destinations.length === 0) {
+    return <EmptyList
+      list={
+        <DropDownList
+          getPath={getGeneratedPath}
+          list={destinationsReferenceList}
+          filterPlaceholder="Filter by destination name"
+        />
+      }
+      title="Destinations list is still empty"
+      unit="destination"
+    />;
+  }
+
   return (
     <Switch>
-      <Route path={destinationPageRoutes.root} exact component={DestinationsList} />
-      <Route path={destinationPageRoutes.newDestination} strict={false} exact component={DestinationEditor} />
-      <Route path={destinationPageRoutes.editDestination} strict={false} exact component={DestinationEditor} />
+      <Route
+        path={destinationPageRoutes.root}
+        exact
+        component={getComponent<CommonDestinationPageProps>(DestinationsList, additionalProps)}
+      />
+      <Route
+        path={destinationPageRoutes.newDestination}
+        strict={false}
+        exact
+        component={getComponent<CommonDestinationPageProps>(DestinationEditor, additionalProps)}
+      />
+      <Route
+        path={destinationPageRoutes.editDestination}
+        strict={false}
+        exact
+        component={getComponent<CommonDestinationPageProps>(DestinationEditor, additionalProps)}
+      />
     </Switch>
   );
 };
