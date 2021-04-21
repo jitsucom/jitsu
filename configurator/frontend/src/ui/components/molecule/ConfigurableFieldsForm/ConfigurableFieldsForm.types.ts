e22a8897ebb097422e6604986d1e4dae9cd68582
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-21 15:24:38 +0300

    Move Props to ConfigurableFieldsForm tsx file, add initialValue to each field, remove unused imports

--- configurator/frontend/src/ui/components/molecule/ConfigurableFieldsForm/ConfigurableFieldsForm.types.ts
+++
@@ -1,7 +0,0 @@
-import { Parameter } from '@catalog/sources/types';
-import { FormInstance } from 'antd/lib/form/hooks/useForm';
-
-export interface Props {
-  fieldsParamsList: Parameter[];
-  form: FormInstance<any>;
-}
