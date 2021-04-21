d8b472f2d67e90853db729a25159e30118f28138
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-21 18:18:53 +0300

    #300 Destinations list, destinations editor fixes

--- configurator/frontend/src/ui/pages/DestinationsPage/partials/DestinationEditor/DestinationEditor.utils.ts
+++ configurator/frontend/src/ui/pages/DestinationsPage/partials/DestinationEditor/DestinationEditor.utils.ts
@@ -1,8 +1,20 @@
 import ApplicationServices from '@service/ApplicationServices';
 import Marshal from '@./lib/commons/marshalling';
+import { handleError } from '@./lib/components/components';
 
 const destinationEditorUtils = {
-  testConnection: config => ApplicationServices.get().backendApiClient.post('/destinations/test', Marshal.toPureJson(config))
+  testConnection: async(dst: DestinationData) => {
+    try {
+      await ApplicationServices.get().backendApiClient.post('/destinations/test', Marshal.toPureJson(dst));
+
+      dst._connectionTestOk = true;
+    } catch(error) {
+      dst._connectionTestOk = false;
+      dst._connectionErrorMessage = error;
+
+      handleError(error, 'Unable to test connection with filled data');
+    }
+  }
 };
 
 export { destinationEditorUtils };
