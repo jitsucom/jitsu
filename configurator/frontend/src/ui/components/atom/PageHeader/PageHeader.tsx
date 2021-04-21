0e41dfb706fa080209a6af429fae9a4d407e0618
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-21 15:23:01 +0300

    Reusable PageHeader component

---
+++ configurator/frontend/src/ui/components/atom/PageHeader/PageHeader.tsx
@@ -0,0 +1,21 @@
+// @Libs
+import { memo } from 'react';
+// @Styles
+import styles from './PageHeader.module.less';
+
+export interface Props {
+  icon: React.ReactNode;
+  title: string;
+  mode: 'edit' | 'add';
+}
+
+const PageHeaderComponent = ({ icon, title, mode }: Props) => (
+  <div className="flex flex-row items-center space-x-1 text-text">
+    <div className={styles.connectorPic}>{icon}</div>
+    <div className="">{title} ({mode === 'add' ? 'add new' : 'edit'})</div>
+  </div>
+);
+
+PageHeaderComponent.displayName = 'PageHeader';
+
+export const PageHeader = memo(PageHeaderComponent);
