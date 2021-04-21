19464e01d41c687952d6db01a971a2136b4e6694
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-21 11:02:22 +0300

    Temp commit before global refactoring

--- configurator/frontend/src/index.tsx
+++ configurator/frontend/src/index.tsx
@@ -1,5 +1,5 @@
 // @Libs
-import React from 'react';
+import React  from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import * as monacoEditor from 'monaco-editor';
