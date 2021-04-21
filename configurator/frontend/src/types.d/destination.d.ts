f2b42a7a6af7c48d73f8b3232441a3c6d351f0e9
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-21 11:20:50 +0300

    #300 Initial Destination props

--- configurator/frontend/src/types.d/destination.d.ts
+++ configurator/frontend/src/types.d/destination.d.ts
@@ -1,3 +1,14 @@
 declare interface DestinationData {
-  [key: string]: any;
+  // private _mappings: FieldMappings = new FieldMappings([], true);
+  // protected readonly _uid = randomId();
+  // private _comment: string = null;
+  // protected readonly _type: string;
+  // protected readonly _onlyKeys = [];
+
+  _id: string;
+  _connectionTestOk: boolean;
+  _connectionErrorMessage: React.ReactNode;
+  _formData: {
+    [key: string]: any;
+  };
 }
