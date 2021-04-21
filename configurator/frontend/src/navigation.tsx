048f4841dbb4204293ebabc92c0dc7a32edf1b3b
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-21 13:27:16 +0300

    Some fixes after rebase onto master

--- configurator/frontend/src/navigation.tsx
+++ configurator/frontend/src/navigation.tsx
@@ -2,7 +2,7 @@
 import React, { ExoticComponent, ReactNode } from 'react';
 import { useLocation } from 'react-router-dom';
 // @Routes
-import { sourcesPageRoutes } from '@page/SourcesPage/routes';
+import { routes as sourcesPageRoutes } from '@page/SourcesPage/routes';
 import { destinationPageRoutes } from '@page/DestinationsPage/DestinationsPage.routes';
 // @???
 import ComponentTest from './lib/components/componentTest';
