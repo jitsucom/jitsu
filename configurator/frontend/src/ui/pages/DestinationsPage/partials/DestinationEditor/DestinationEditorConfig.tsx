42262f3bf5883c05de8880229fdcda984b3fabdf
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-24 01:05:26 +0300

    DestinationEditor fixes

--- configurator/frontend/src/ui/pages/DestinationsPage/partials/DestinationEditor/DestinationEditorConfig.tsx
+++ configurator/frontend/src/ui/pages/DestinationsPage/partials/DestinationEditor/DestinationEditorConfig.tsx
@@ -7,13 +7,6 @@ import { ConfigurableFieldsForm } from '@molecule/ConfigurableFieldsForm';
 import { Destination } from '@catalog/destinations/types';
 import { FormInstance } from 'antd/lib/form/hooks/useForm';
 
-const initialValues = {
-  '_formData.pghost': 'ec2-34-194-198-238.compute-1.amazonaws.com',
-  '_formData.pgdatabase': 'd1p1tiidq6d6b7',
-  '_formData.pguser': 'yjnjwapyryobyp',
-  '_formData.pgpassword': '8bc09f6ef6666890f14368ad9cd2b2d602006531a305e3c0fb84289329d56375'
-};
-
 export interface Props {
   destinationData: DestinationData;
   destinationReference: Destination;
@@ -30,7 +23,6 @@ const DestinationEditorConfig = ({ destinationData, destinationReference, form, 
       form={form}
       autoComplete="off"
       onChange={handleSomeFieldChange}
-      initialValues={initialValues}
     >
       <ConfigurableFieldsForm initialValues={destinationData} fieldsParamsList={destinationReference.parameters} form={form}/>
     </Form>
