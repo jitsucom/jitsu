bc131e0178bb2ff8fd34f89ac35eb475a96833a5
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-25 22:07:48 +0300

    Mapping typing, hide dst field on remove action

--- configurator/frontend/src/types.d/mapping.d.ts
+++ configurator/frontend/src/types.d/mapping.d.ts
@@ -1,1 +1,12 @@
 declare type MappingAction = 'erase' | 'cast/int' | 'cast/double' | 'cast/date' | 'move';
+
+declare interface MappingRow {
+  _srcField: string;
+  _dstField: string;
+  _action: MappingAction;
+}
+
+declare interface Mapping {
+  _mapping: MappingRow[];
+  _keepUnmappedFields: boolean;
+}
