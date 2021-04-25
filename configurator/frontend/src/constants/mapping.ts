7af0462e737c14c0f9ee15117f1a3827bf31482a
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-25 13:18:42 +0300

    Destination mapping editor, blank

---
+++ configurator/frontend/src/constants/mapping.ts
@@ -0,0 +1,9 @@
+const MAPPING_NAMES: Record<MappingAction, string> = {
+  erase: 'Remove field',
+  'cast/int': 'Cast to INT',
+  'cast/double': 'Cast to DOUBLE',
+  'cast/date': 'Cast to DATE',
+  move: 'Move field'
+};
+
+export { MAPPING_NAMES };
