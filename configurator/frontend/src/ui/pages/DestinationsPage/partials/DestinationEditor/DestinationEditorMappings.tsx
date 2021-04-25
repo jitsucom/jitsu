4f0043acb547c3ec478653ca40cbe16aee4dcacd
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-26 01:13:32 +0300

    Fill mappings form with template values

--- configurator/frontend/src/ui/pages/DestinationsPage/partials/DestinationEditor/DestinationEditorMappings.tsx
+++ configurator/frontend/src/ui/pages/DestinationsPage/partials/DestinationEditor/DestinationEditorMappings.tsx
@@ -1,33 +1,39 @@
 // @Libs
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { Button, Form, Input, Radio, Select } from 'antd';
+// @Components
+import { ComingSoon } from '@atom/ComingSoon';
 // @Types
 import { FormInstance } from 'antd/lib/form/hooks/useForm';
 import { FormListFieldData, FormListOperation } from 'antd/es/form/FormList';
 // @Icons
 import PlusOutlined from '@ant-design/icons/lib/icons/PlusOutlined';
 import DeleteOutlined from '@ant-design/icons/lib/icons/DeleteOutlined';
+import PlayCircleFilled from '@ant-design/icons/lib/icons/PlayCircleFilled';
+import FileTextOutlined from '@ant-design/icons/lib/icons/FileTextOutlined';
 // @Constants
-import { MAPPING_NAMES } from '@./constants/mapping';
+import { MAPPINGS_REFERENCE_MAP, MAPPING_NAMES } from '@./constants/mapping';
 import { DESTINATION_EDITOR_MAPPING } from '@./embeddedDocs/mappings';
 // @Styles
 import styles from './DestinationEditor.module.less';
-import { ComingSoon } from '@atom/ComingSoon';
-import ApiOutlined from '@ant-design/icons/lib/icons/ApiOutlined';
-import PlayCircleFilled from '@ant-design/icons/lib/icons/PlayCircleFilled';
+// @Catalog mappings
+import mappings from '@./catalog/mappings/lib';
 
 export interface Props {
   form: FormInstance;
   initialValues: Mapping;
+  destinationType: string;
 }
 
-const DestinationEditorMappings = ({ form, initialValues }: Props) => {
+const DestinationEditorMappings = ({ form, initialValues, destinationType }: Props) => {
   const [mappingActions, setMappingActions] = useState<{ [key: number]: string }>(
     initialValues?._mapping?.reduce((accumulator: { [key: number]: string }, current: MappingRow, index: number) => ({
       ...accumulator,
       [index]: current._action
     }), {})
   );
+
+  const defaultMappingLibrary = useMemo(() => mappings?.[MAPPINGS_REFERENCE_MAP[destinationType]], [destinationType]);
 
   const handleAddField = useCallback(
     (add: FormListOperation['add']) => () => add({ _srcField: '', _dstField: '', _action: '' }),
@@ -46,12 +52,32 @@ const DestinationEditorMappings = ({ form, initialValues }: Props) => {
     });
   }, [mappingActions]);
 
+  const handleUseTemplate = useCallback(() => {
+    const mappings = {
+      '_mappings._keepUnmappedFields': defaultMappingLibrary.keepUnmappedFields,
+      '_mappings._mapping': defaultMappingLibrary.mappings.map(row => ({
+        _srcField: row.src,
+        _dstField: row.dst,
+        _action: row.action
+      }))
+    };
+
+    form.setFieldsValue({ ...mappings });
+
+    const actions = defaultMappingLibrary.mappings.reduce((accumulator: { [key: number]: string }, current: any, index: number) => ({
+      ...accumulator,
+      [index]: current.action
+    }), {});
+
+    setMappingActions(actions);
+  }, [defaultMappingLibrary, form]);
+
   return (
     <>
       <h3>Edit field mappings</h3>
       <article>{DESTINATION_EDITOR_MAPPING}</article>
 
-      <Form form={form} onFinish={values => console.log(values)}>
+      <Form form={form}>
         <Form.Item name="_mappings._keepUnmappedFields" initialValue={initialValues?._keepUnmappedFields}>
           <Radio.Group buttonStyle="solid">
             <Radio.Button value={true}>Keep unmapped fields</Radio.Button>
@@ -91,7 +117,7 @@ const DestinationEditorMappings = ({ form, initialValues }: Props) => {
 
                           <div className={styles.mapInputWrap}>
                             {
-                              mappingActions[field.name] !== 'erase' && (
+                              mappingActions?.[field.name] !== 'erase' && (
                                 <>
                                   <label className={styles.mapInputLabel} htmlFor={`dst-field-${field.name}`}>To: </label>
                                   <Form.Item name={[field.name, '_dstField']}>
@@ -118,9 +144,17 @@ const DestinationEditorMappings = ({ form, initialValues }: Props) => {
                     Add new Field Mapping
                   </Button>
 
-                  <Button className={styles.btn} icon={<PlayCircleFilled/>} disabled={true}>
+                  <Button className={styles.btn} icon={<PlayCircleFilled />} disabled={true}>
                     <ComingSoon render="Test Mapping" documentation="Try created mapping" />
                   </Button>
+
+                  {
+                    defaultMappingLibrary && (
+                      <Button className={styles.btn} icon={<FileTextOutlined />} onClick={handleUseTemplate}>
+                        Use mappings template
+                      </Button>
+                    )
+                  }
                 </div>
               </div>
             )
