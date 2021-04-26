caf62f6196da33d845e12532c5bb0902cab29007
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-26 19:07:33 +0300

    Temporary commit with TS errors

--- configurator/frontend/src/ui/pages/SourcesPage/SourcesPage.tsx
+++ configurator/frontend/src/ui/pages/SourcesPage/SourcesPage.tsx
@@ -5,9 +5,8 @@ import { Route, Switch } from 'react-router-dom';
 import { routes } from './routes';
 // @Components
 import { SourcesList } from './partials/SourcesList';
-import { AddSource } from './partials/AddSource';
-import { EditSource } from './partials/EditSource';
-import { CenteredSpin } from '@./lib/components/components';
+import { SourceEditor } from './partials/SourceEditor';
+import { CenteredError, CenteredSpin } from '@./lib/components/components';
 // @Services
 import ApplicationServices from '@service/ApplicationServices';
 // @Styles
@@ -17,6 +16,7 @@ import { getComponent } from '@hocs/getComponent';
 // @Types
 import { BreadcrumbsProps } from '@molecule/Breadcrumbs/Breadcrumbs.types';
 import { PageProps } from '@./navigation';
+import useLoader from '@hooks/useLoader';
 
 export interface CollectionSourceData {
   sources: SourceData[];
@@ -26,54 +26,52 @@ export interface CollectionSourceData {
 export interface CommonSourcePageProps {
   sources: SourceData[];
   projectId: string;
-  setSources: Dispatch<SetStateAction<CollectionSourceData>>;
+  updateSources: Dispatch<SetStateAction<CollectionSourceData>>;
   setBreadcrumbs: (breadcrumbs: BreadcrumbsProps) => void;
+  editorMode?: 'edit' | 'add';
 }
 
 const SourcesPage = (props: PageProps) => {
-  const [sources, setSources] = useState<CollectionSourceData>();
-
   const services = useMemo(() => ApplicationServices.get(), []);
 
-  useEffect(() => {
-    services.storageService.get('sources', services.activeProject.id).then(({ _lastUpdated, ...response }) => {
-      setSources(response);
-    });
-  }, [setSources, services, services.activeProject.id]);
+  const [error, sources, updateSources] = useLoader(
+    async() => await services.storageService.get('sources', services.activeProject.id)
+  );
 
   const additionalProps = useMemo(() => ({
     projectId: services.activeProject.id,
     sources: sources?.sources,
-    setSources,
+    updateSources,
     setBreadcrumbs: props.setBreadcrumbs
-  }), [sources?.sources, services.activeProject.id]);
+  }), [props.setBreadcrumbs, sources?.sources, services.activeProject.id, updateSources]);
 
-  return <>
-    {!sources ?
-      <CenteredSpin/>
-      :
-      (
-        <Switch>
-          <Route
-            path={routes.root}
-            exact
-            render={getComponent<CommonSourcePageProps>(SourcesList, additionalProps)}
-          />
-          <Route
-            path={[routes.add, routes.addExact]}
-            strict={false}
-            exact
-            render={getComponent<CommonSourcePageProps>(AddSource, additionalProps)}
-          />
-          <Route
-            path={[routes.edit, routes.editExact]}
-            strict={false}
-            exact
-            render={getComponent<CommonSourcePageProps>(EditSource, additionalProps)}
-          />
-        </Switch>
-      )}
-  </>
+  if (error) {
+    return <CenteredError error={error} />;
+  } else if (!sources) {
+    return <CenteredSpin />;
+  }
+
+  return (
+    <Switch>
+      <Route
+        path={routes.root}
+        exact
+        render={getComponent<CommonSourcePageProps>(SourcesList, additionalProps)}
+      />
+      <Route
+        path={[routes.add, routes.addExact]}
+        strict={false}
+        exact
+        render={getComponent<CommonSourcePageProps>(SourceEditor, { ...additionalProps, editorMode: 'add' })}
+      />
+      <Route
+        path={[routes.edit, routes.editExact]}
+        strict={false}
+        exact
+        render={getComponent<CommonSourcePageProps>(SourceEditor, { ...additionalProps, editorMode: 'edit' })}
+      />
+    </Switch>
+  );
 };
 
 SourcesPage.displayName = 'SourcesPage';
