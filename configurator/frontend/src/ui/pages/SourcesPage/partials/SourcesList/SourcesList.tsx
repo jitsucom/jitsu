a0ebfd09ea88590b0714038444442145942b72f1
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-27 15:36:52 +0300

    Use DropDownList component for SourcesLiost and DestinationsList, rename sources routes

--- configurator/frontend/src/ui/pages/SourcesPage/partials/SourcesList/SourcesList.tsx
+++ configurator/frontend/src/ui/pages/SourcesPage/partials/SourcesList/SourcesList.tsx
@@ -1,13 +1,13 @@
 // @Libs
 import React, { useCallback, useEffect, useMemo } from 'react';
-import { Button, Dropdown, List, message, Modal } from 'antd';
+import { generatePath } from 'react-router-dom';
+import { Button, Dropdown, List, message } from 'antd';
 import { snakeCase } from 'lodash';
 // @Components
-import { ConnectorsCatalog } from '../_common/ConnectorsCatalog';
+import { DropDownList } from '@molecule/DropDownList';
 import { SourcesListItem } from './SourcesListItem';
 // @Icons
 import PlusOutlined from '@ant-design/icons/lib/icons/PlusOutlined';
-import ExclamationCircleOutlined from '@ant-design/icons/lib/icons/ExclamationCircleOutlined';
 // @Services
 import ApplicationServices from '@service/ApplicationServices';
 // @Types
@@ -19,21 +19,11 @@ import styles from './SourcesList.module.less';
 // @Sources
 import { allSources } from '@catalog/sources/lib';
 // @Routes
-import { routes } from '@page/SourcesPage/routes';
+import { sourcesPageRoutes } from '@page/SourcesPage/routes';
+import { DropDownListItem } from '@molecule/DropDownList/DropDownList';
 
 const SourcesList = ({ projectId, sources, updateSources, setBreadcrumbs }: CommonSourcePageProps) => {
   const services = useMemo(() => ApplicationServices.get(), []);
-
-  useEffect(() => {
-    setBreadcrumbs(withHome({
-      elements: [
-        { title: 'Sources', link: routes.root },
-        {
-          title: 'Sources List'
-        }
-      ]
-    }));
-  }, [setBreadcrumbs])
 
   const sourcesMap = useMemo(
     () =>
@@ -46,6 +36,22 @@ const SourcesList = ({ projectId, sources, updateSources, setBreadcrumbs }: Comm
       ),
     []
   );
+
+  const isFirstSingerType = useCallback((list: DropDownListItem[], item: DropDownListItem, index: number) => {
+    return !item.isSingerType && list[index + 1]?.isSingerType ? styles.isFirstSingerTap : undefined;
+  }, []);
+
+  const dropDownList = useMemo(() => <DropDownList
+    list={allSources.map((src: SourceConnector) => ({
+      title: src.displayName,
+      id: src.id,
+      icon: src.pic,
+      link: generatePath(sourcesPageRoutes.addExact, { source: src.id }),
+      isSingerType: src.isSingerType
+    }))}
+    getClassName={isFirstSingerType}
+    filterPlaceholder="Filter by source name or id"
+  />, [isFirstSingerType]);
 
   const handleDeleteSource = useCallback(
     (sourceId: string) => {
@@ -60,36 +66,43 @@ const SourcesList = ({ projectId, sources, updateSources, setBreadcrumbs }: Comm
     [sources, updateSources, services.storageService, projectId]
   );
 
+  useEffect(() => {
+    setBreadcrumbs(withHome({
+      elements: [
+        { title: 'Sources', link: sourcesPageRoutes.root },
+        {
+          title: 'Sources List'
+        }
+      ]
+    }));
+  }, [setBreadcrumbs]);
+
   return (
     <>
       {sources?.length > 0
         ? <>
           <div className="mb-5">
-            <Dropdown trigger={['click']} overlay={<ConnectorsCatalog />}>
+            <Dropdown trigger={['click']} overlay={dropDownList}>
               <Button type="primary" icon={<PlusOutlined />}>Add source</Button>
             </Dropdown>
           </div>
 
           <List key="sources-list" className="sources-list" itemLayout="horizontal" split={true}>
-            {sources.map((source) => {
-              const sourceProto = sourcesMap[source.sourceProtoType];
-
-              return (
-                <SourcesListItem
-                  sourceData={source}
-                  handleDeleteSource={handleDeleteSource}
-                  sourceProto={sourceProto}
-                  sourceId={source.sourceId}
-                  key={source.sourceId}
-                />
-              );
-            })}
+            {sources.map((source) => (
+              <SourcesListItem
+                sourceData={source}
+                handleDeleteSource={handleDeleteSource}
+                sourceProto={sourcesMap[source.sourceProtoType]}
+                sourceId={source.sourceId}
+                key={source.sourceId}
+              />
+            ))}
           </List>
         </>
         : <div className={styles.empty}>
           <h3 className="text-2xl">Sources list is still empty</h3>
           <div>
-            <Dropdown placement="bottomCenter" trigger={['click']} overlay={<ConnectorsCatalog />}>
+            <Dropdown placement="bottomCenter" trigger={['click']} overlay={dropDownList}>
               <Button type="primary" size="large" icon={<PlusOutlined />}>Add source</Button>
             </Dropdown>
           </div>
