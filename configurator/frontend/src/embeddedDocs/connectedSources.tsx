1a01213df2f85fb797c0dc78cafe57b45befc9d8
Hovansky Alexander <alex.hovansky@gmail.com>
2021-04-27 17:14:01 +0300

    Add connected _apiKeys, fix destination saving

---
+++ configurator/frontend/src/embeddedDocs/connectedSources.tsx
@@ -0,0 +1,3 @@
+const DESTINATIONS_CONNECTED_SOURCES = <p>Connector is a source of data from platform API or database. You can read more about connectors in our <a href="https://jitsu.com/docs/sources-configuration" target="_blank" rel="noreferrer">documentation</a>.</p>;
+
+export { DESTINATIONS_CONNECTED_SOURCES };
