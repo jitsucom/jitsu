7ae2c64ba3e070d42e79871ba663eae08e75c287
vklimontovich <vladimir@jitsu.com>
2021-04-28 17:21:29 +0300

    New interface for connected destinations & sources

--- configurator/frontend/src/embeddedDocs/destinationsConnectedItems.tsx
+++ configurator/frontend/src/embeddedDocs/destinationsConnectedItems.tsx
@@ -1,4 +1,8 @@
-const DESTINATIONS_CONNECTED_SOURCES = <p>Connector is a source of data from platform API or database. You can read more about connectors in our <a href="https://jitsu.com/docs/sources-configuration" target="_blank" rel="noreferrer">documentation</a>.</p>;
+const DESTINATIONS_CONNECTED_SOURCES = <>
+  Connector is a source of data from platform API or database. You can read more about connectors in our
+  {' '}
+  <a href="https://jitsu.com/docs/sources-configuration" target="_blank" rel="noreferrer">documentation</a>.
+</>;
 
 const DESTINATIONS_CONNECTED_API_KEYS = '';
 
