394131109f6fda394a671f1c0316ac26cf6c7feb
vklimontovich <vladimir@jitsu.com>
2021-04-28 17:52:32 +0300

    #316 - implemented

--- configurator/frontend/src/catalog/destinations/lib/postgres.tsx
+++ configurator/frontend/src/catalog/destinations/lib/postgres.tsx
@@ -34,6 +34,7 @@ const icon = <svg viewBox="0 0 25.6 25.6" xmlns="http://www.w3.org/2000/svg">
 </svg>;
 
 const postgresDestination: Destination = {
+  syncFromSourcesStatus: 'supported',
   id: 'postgres',
   displayName: 'Postgres SQL',
   ui: {
