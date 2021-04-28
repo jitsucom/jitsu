3e40cf98168a9799c054b8319bd8a8feb087d746
vklimontovich <vladimir@jitsu.com>
2021-04-28 17:21:29 +0300

    New interface for connected destinations & sources

--- configurator/frontend/src/ui/pages/DestinationsPage/partials/DestinationEditor/DestinationEditorConnectors.tsx
+++ configurator/frontend/src/ui/pages/DestinationsPage/partials/DestinationEditor/DestinationEditorConnectors.tsx
@@ -1,7 +1,7 @@
 // @Libs
-import React, { useCallback, useMemo } from 'react';
+import React, { ReactNode, useCallback, useMemo } from 'react';
 import { generatePath, Link, useHistory } from 'react-router-dom';
-import { Form } from 'antd';
+import { Button, Collapse, Form } from 'antd';
 // @Hooks
 import useLoader from '@hooks/useLoader';
 // @Services
@@ -9,7 +9,9 @@ import ApplicationServices from '@service/ApplicationServices';
 // @Components
 import { ConnectedItems } from '@organism/ConnectedItems';
 import { ListItemDescription } from '@atom/ListItemDescription';
-import { CenteredError, CenteredSpin } from '@./lib/components/components';
+import { CenteredError, CenteredSpin, CodeInline } from '@./lib/components/components';
+import { NavLink } from 'react-router-dom';
+
 // @Types
 import { FormInstance } from 'antd/lib/form/hooks/useForm';
 import { ConnectedItem } from '@organism/ConnectedItems';
@@ -23,11 +25,15 @@ import {
 // @Routes
 import { sourcesPageRoutes } from '@page/SourcesPage/routes';
 import { sourcePageUtils } from '@page/SourcesPage/SourcePage.utils';
+import Error from '../../../../../../../../calendso/pages/auth/error';
+import Icon from '@ant-design/icons';
+import { NameWithPicture } from '@organism/ConnectedItems/ConnectedItems';
 
 export interface Props {
   form: FormInstance;
   initialValues: DestinationData;
 }
+
 
 const DestinationEditorConnectors = ({ form, initialValues }: Props) => {
   const history = useHistory();
@@ -35,84 +41,84 @@ const DestinationEditorConnectors = ({ form, initialValues }: Props) => {
   const service = ApplicationServices.get();
 
   const [sourcesError, sourcesData] = useLoader(async() => await service.storageService.get('sources', service.activeProject.id));
-  const [APIKeysError, APIKeysData] = useLoader(async() => await service.storageService.get('api_keys', service.activeProject.id));
+  const [apiKeysError, apiKeysData] = useLoader(async() => await service.storageService.get('api_keys', service.activeProject.id));
 
   const handleEditAction = useCallback((id: string) => () => history.push(generatePath(sourcesPageRoutes.editExact, { sourceId: id })), [history]);
 
   const sourcesList = useMemo<ConnectedItem[]>(
     () => sourcesData?.sources
-      ? sourcesData.sources?.map((src: SourceData) => {
+      ?
+      sourcesData.sources?.map((src: SourceData) => {
         const proto = allSources.find(s => s.id === src.sourceType);
 
         return {
-          itemKey: src.sourceId,
           id: src.sourceId,
-          title: sourcePageUtils.getTitle(src),
-          icon: proto?.pic,
-          description: proto?.displayName,
-          actions: [{ key: 'edit', method: handleEditAction, title: 'Edit' }]
+          title: <><NameWithPicture icon={proto?.pic}><b>{proto?.displayName}: </b> {sourcePageUtils.getTitle(src)}</NameWithPicture></>,
+          description: null
         };
       })
-      : [],
+      :
+      [],
     [handleEditAction, sourcesData?.sources]
   );
 
   const apiKeysList = useMemo<ConnectedItem[]>(
-    () => APIKeysData?.keys
-      ? APIKeysData.keys.map((key: APIKey) => ({
-        itemKey: key.uid,
-        title: key.uid,
+    () => apiKeysData?.keys
+      ?
+      apiKeysData.keys.map((key: APIKey) => ({
+        title: <code>{key.uid}</code>,
         id: key.uid,
-        additional: key.origins?.length ? <ListItemDescription render={<>Origins: {key.origins.join(', ')}</>} /> : undefined,
-        description: <ListItemDescription render={<>Server secret: {key.serverAuth}<br />Client secret: {key.jsAuth}</>} />
+        description: <div className="align-middle">Server secret: <code>{key.serverAuth}</code> / Client secret: <code>{key.jsAuth}</code></div>
       }))
-      : [],
-    [APIKeysData?.keys]
+      :
+      [],
+    [apiKeysData?.keys]
   );
+
+  if (apiKeysError || sourcesError) {
+    return <CenteredError error={apiKeysError || sourcesError}/>
+  } else if (!sourcesData || !apiKeysData) {
+    return <CenteredSpin/>
+  }
+
+  let activeKey;
+  if (apiKeysList.length > 0 || sourcesData.sources.length === 0 && apiKeysList.length === 0) {
+    activeKey = 'keys';
+  } else {
+    activeKey = 'connectors'
+  }
 
   return (
     <>
       <Form form={form} name="connected-sources">
-        <div className="mb-10">
-          <h3>Choose sources</h3>
-          <article className="mb-5">
-            {DESTINATIONS_CONNECTED_SOURCES}
-            {
-              sourcesData && !sourcesData?.sources?.length && <p>If you haven't added any connectors yet you can do it <Link to="/sources">here</Link>.</p>
-            }
-          </article>
-          {
-            sourcesError
-              ? <CenteredError error={sourcesError} />
-              : !sourcesData
-                ? <CenteredSpin />
-                : <ConnectedItems
-                  form={form}
-                  fieldName="_sources"
-                  itemsList={sourcesList}
-                  warningMessage={<p>Please, choose at least one source.</p>}
-                  initialValues={initialValues?._sources}
-                />
-          }
-        </div>
-
-        <div>
-          <h3>Choose API keys</h3>
-          {DESTINATIONS_CONNECTED_API_KEYS}
-          {
-            APIKeysError
-              ? <CenteredError error={APIKeysError} />
-              : !APIKeysData
-                ? <CenteredSpin />
-                : <ConnectedItems
-                  form={form}
-                  fieldName="_onlyKeys"
-                  itemsList={apiKeysList}
-                  warningMessage={<p>Please, choose at least one API key.</p>}
-                  initialValues={initialValues?._onlyKeys}
-                />
-          }
-        </div>
+        <div className="text-xs italic text-secondaryText mb-5">{DESTINATIONS_CONNECTED_SOURCES}</div>
+        <Collapse ghost defaultActiveKey={activeKey}>
+          <Collapse.Panel header={<b>Linked API Keys (<NavLink to="/api_keys">edit API keys</NavLink>)</b>} key="keys">
+            <div className="pl-6">
+              <ConnectedItems
+                form={form}
+                fieldName="_onlyKeys"
+                itemsList={apiKeysList}
+                warningMessage={<p>Please, choose at least one API key.</p>}
+                initialValues={initialValues?._onlyKeys}/>
+            </div>
+          </Collapse.Panel>
+          <Collapse.Panel header={<b>Linked Connectors (<NavLink to="/api_keys">edit connectors</NavLink>)</b>} key="connectors">
+            <div className="pl-6">
+              {
+                sourcesData && !sourcesData?.sources?.length &&
+                <p className="text-sm text-secondaryText">You don't have any connectors you can link to the destination. You can add them <Link to="/sources">here</Link>.</p>
+              }
+              <ConnectedItems
+                form={form}
+                fieldName="_sources"
+                itemsList={sourcesList}
+                warningMessage={<p>Please, choose at least one source.</p>}
+                initialValues={initialValues?._sources}
+              />
+            </div>
+          </Collapse.Panel>
+        </Collapse>
       </Form>
     </>
   );
