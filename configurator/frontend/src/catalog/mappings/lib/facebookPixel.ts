d8af348827613b55f3848370318fff36959c453c
Sergey Burykin <sburykin@jitsu.com>
2021-04-30 10:52:35 +0300

    added JS SDK 2.0 mappings

--- configurator/frontend/src/catalog/mappings/lib/facebookPixel.ts
+++ configurator/frontend/src/catalog/mappings/lib/facebookPixel.ts
@@ -14,7 +14,17 @@ const mapping: Mapping = {
             "action": "move"
         },
         {
+            "src": "/eventn_ctx_event_id",
+            "dst": "/event_id",
+            "action": "move"
+        },
+        {
             "src": "/eventn_ctx/url",
+            "dst": "/event_source_url",
+            "action": "move"
+        },
+        {
+            "src": "/url",
             "dst": "/event_source_url",
             "action": "move"
         },
@@ -29,7 +39,17 @@ const mapping: Mapping = {
             "action": "move"
         },
         {
+            "src": "/user/email",
+            "dst": "/user_data/em",
+            "action": "move"
+        },
+        {
             "src": "/eventn_ctx/user_agent",
+            "dst": "/user_data/client_user_agent",
+            "action": "move"
+        },
+        {
+            "src": "/user_agent",
             "dst": "/user_data/client_user_agent",
             "action": "move"
         }
