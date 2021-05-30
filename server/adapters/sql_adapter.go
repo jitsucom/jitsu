a94990d4ace32d5cb569495e14e7218ff0d982fe
Maksim Lebedev <alessarik@gmail.com>
2021-05-30 21:27:54 +0300

    Beta implementation

--- server/adapters/sql_adapter.go
+++ server/adapters/sql_adapter.go
@@ -6,4 +6,5 @@ type SQLAdapter interface {
 	GetTableSchema(tableName string) (*Table, error)
 	CreateTable(schemaToCreate *Table) error
 	PatchTableSchema(schemaToAdd *Table) error
+	BulkUpdate(table *Table, objects []map[string]interface{}, deleteConditions *DeleteConditions) error
 }
