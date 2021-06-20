a7cc2b69f5b3aef392c08d1039134f20aa488d38
taletski <kirill.taletski@gmail.com>
2021-06-20 20:53:37 +0300

    implements listening for user events

--- configurator/frontend/src/lib/components/OnboardingTour/steps/OnboardingTourReceiveEvent/OnboardingTourReceiveEvent.tsx
+++ configurator/frontend/src/lib/components/OnboardingTour/steps/OnboardingTourReceiveEvent/OnboardingTourReceiveEvent.tsx
@@ -1,7 +1,9 @@
-// @components
+// @Components
 import { Button, Spin } from 'antd'
-
-// @styles
+import { useEffect, useRef } from 'react'
+// @Services
+import ApplicationServices from '@./lib/services/ApplicationServices';
+// @Styles
 import styles from './OnboardingTourReceiveEvent.module.less'
 
 type Props = {
@@ -9,10 +11,32 @@ type Props = {
    handleGoBack: () => void;
  }
 
+const services = ApplicationServices.get();
+
+const POLLING_INTERVAL_MS = 1000;
+
 export const OnboardingTourReceiveEvent: React.FC<Props> = function({
   handleGoNext,
   handleGoBack
 }) {
+  const intervalRef = useRef<number | null>(null);
+
+  useEffect(() => {
+    intervalRef.current = window.setInterval(async() => {
+      const events = await services.backendApiClient.get(
+        `/events/cache?project_id=${services.activeProject.id}&limit=5`, { proxy: true }
+      );
+      if (events?.total_events) {
+        window.clearInterval(intervalRef.current);
+        handleGoNext();
+      }
+    }, POLLING_INTERVAL_MS)
+
+    return () => {
+      window.clearInterval(intervalRef.current);
+    }
+  }, [])
+
   return (<div className={styles.mainContainer}>
     <h1 className={styles.header}>
       {'Test Your Setup'}
@@ -26,7 +50,6 @@ export const OnboardingTourReceiveEvent: React.FC<Props> = function({
     <div className={styles.controlsContainer}>
       <Button type="text" className={styles.withButtonsMargins} onClick={handleGoNext}>{'Skip Verification'}</Button>
       <Button type="ghost" className={styles.withButtonsMargins} onClick={handleGoBack}>{'Back to Instructions'}</Button>
-      {/* <Button type="primary" className={styles.withButtonsMargins} onClick={handleGoNext}>{'Next'}</Button> */}
     </div>
   </div>);
 }
