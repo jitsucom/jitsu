1b5da8399cec373e2c5c4f3c510543a136c10576
Maksim Lebedev <alessarik@gmail.com>
2021-06-26 17:12:33 +0300

    Restoring event

--- server/handlers/pixel.go
+++ server/handlers/pixel.go
@@ -2,9 +2,12 @@ package handlers
 
 import (
 	"encoding/base64"
+	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jitsucom/jitsu/server/jsonutils"
 	"github.com/jitsucom/jitsu/server/logging"
 )
 
@@ -27,5 +30,43 @@ func NewPixelHandler() *PixelHandler {
 }
 
 func (handler *PixelHandler) Handle(c *gin.Context) {
+	go doTracking(c.Copy())
+
 	c.Data(http.StatusOK, "image/gif", handler.data)
+}
+
+func doTracking(c *gin.Context) {
+	event := map[string]interface{}{}
+
+	parameters := c.Request.URL.Query()
+
+	data := parameters.Get("data")
+	if data != "" {
+		value, err := base64.StdEncoding.DecodeString(data)
+		if err != nil {
+			logging.Debugf("Error decoding string: %v", err)
+		} else {
+			err = json.Unmarshal(value, &event)
+			if err != nil {
+				logging.Debugf("Error parsing JSON event: %v", err)
+			}
+		}
+	}
+
+	for key, value := range parameters {
+		if key == "data" {
+			continue
+		}
+
+		converted := strings.ReplaceAll(key, ".", "/")
+		path := jsonutils.NewJSONPath(converted)
+		path.Set(event, value)
+	}
+
+	compatibility := false
+	if _, ok := event["compat"]; ok {
+		compatibility = true
+	}
+
+	logging.Infof("Compatibility: %v, Event: %v", compatibility, event)
 }
