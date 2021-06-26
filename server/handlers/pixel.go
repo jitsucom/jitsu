3b81f63eb766e654d9ce0d210efbced5a6be8ace
Maksim Lebedev <alessarik@gmail.com>
2021-06-26 19:15:20 +0300

    Enrich event

--- server/handlers/pixel.go
+++ server/handlers/pixel.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jitsucom/jitsu/server/jsonutils"
 	"github.com/jitsucom/jitsu/server/logging"
+	"github.com/jitsucom/jitsu/server/timestamp"
 )
 
 const TRACKING_PIXEL = "R0lGODlhAQABAIAAAAAAAP8AACH5BAEAAAEALAAAAAABAAEAAAICTAEAOw=="
@@ -38,6 +39,12 @@ func (handler *PixelHandler) Handle(c *gin.Context) {
 func doTracking(c *gin.Context) {
 	event := map[string]interface{}{}
 
+	restoreEvent(event, c)
+
+	enrichEvent(event, c)
+}
+
+func restoreEvent(event map[string]interface{}, c *gin.Context) {
 	parameters := c.Request.URL.Query()
 
 	data := parameters.Get("data")
@@ -62,11 +69,75 @@ func doTracking(c *gin.Context) {
 		path := jsonutils.NewJSONPath(converted)
 		path.Set(event, value)
 	}
+}
 
+func enrichEvent(event map[string]interface{}, c *gin.Context) {
 	compatibility := false
 	if _, ok := event["compat"]; ok {
 		compatibility = true
 	}
 
-	logging.Infof("Compatibility: %v, Event: %v", compatibility, event)
+	urlField := "url"
+	hostField := "doc_host"
+	pathField := "doc_path"
+	searchField := "doc_search"
+	userIdField := "user/anonymous_id"
+	agentField := "user_agent"
+	timeField := "utc_time"
+	if compatibility {
+		urlField = "eventn_ctx/url"
+		hostField = "eventn_ctx/doc_host"
+		pathField = "eventn_ctx/doc_path"
+		searchField = "eventn_ctx/doc_search"
+		userIdField = "eventn_ctx/user/anonymous_id"
+		agentField = "eventn_ctx/user_agent"
+		timeField = "eventn_ctx/utc_time"
+	}
+
+	path := jsonutils.NewJSONPath(urlField)
+	if _, exist := path.Get(event); !exist {
+		path.Set(event, c.Request.RemoteAddr)
+	}
+
+	path = jsonutils.NewJSONPath(hostField)
+	if _, exist := path.Get(event); !exist {
+		path.Set(event, c.Request.Host)
+	}
+
+	path = jsonutils.NewJSONPath(pathField)
+	if _, exist := path.Get(event); !exist {
+		path.Set(event, c.Request.URL.Path)
+	}
+
+	path = jsonutils.NewJSONPath(searchField)
+	if _, exist := path.Get(event); !exist {
+		path.Set(event, c.Request.URL.RawQuery)
+	}
+
+	path = jsonutils.NewJSONPath(userIdField)
+	if _, exist := path.Get(event); !exist {
+		domain, ok := event["cookie_domain"]
+		if !ok {
+			domain = c.Request.Host
+		}
+
+		if domain_str, ok := domain.(string); ok {
+			cookie, err := c.Request.Cookie(domain_str)
+			if err == nil && cookie != nil {
+				path.Set(event, cookie.Value)
+			}
+		}
+	}
+
+	path = jsonutils.NewJSONPath(agentField)
+	if _, exist := path.Get(event); !exist {
+		path.Set(event, c.Request.UserAgent())
+	}
+
+	path = jsonutils.NewJSONPath(timeField)
+	if _, exist := path.Get(event); !exist {
+		path.Set(event, timestamp.NowUTC())
+	}
+
+	event["src"] = "jitsu_gif"
 }
