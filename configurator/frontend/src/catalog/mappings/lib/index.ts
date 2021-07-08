06cdcb112c4cebb2426657a2e6f3691e78efa208
Sergey Burykin <sburykin@jitsu.com>
2021-07-08 20:40:13 +0300

    229 added amplitude destination

--- configurator/frontend/src/catalog/mappings/lib/index.ts
+++ configurator/frontend/src/catalog/mappings/lib/index.ts
@@ -1,9 +1,10 @@
 import facebookPixel from './facebookPixel'
 import googleAnalytics from './googleAnalytics'
 import segment from './segment'
+import amplitude from './amplitude'
 
 const mappings = {
-  facebookPixel, googleAnalytics, segment
+  facebookPixel, googleAnalytics, segment, amplitude
 };
 
 export default mappings;
