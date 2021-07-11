efa3e8cffdc12de31c0de96fa37a6e1ad8bc3098
Maksim Lebedev <alessarik@gmail.com>
2021-07-11 12:48:26 +0300

    Driver for Amplitude

---
+++ server/drivers/amplitude/amplitude.go
@@ -0,0 +1,142 @@
+package amplitude
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jitsucom/jitsu/server/drivers/base"
+)
+
+// Amplitude is a Amplitude driver.
+// It is used in syncing data from Amplitude.
+type Amplitude struct {
+	adapter *AmplitudeAdapter
+	config  *AmplitudeConfig
+
+	collection *base.Collection
+}
+
+func init() {
+	base.RegisterDriver(base.AmplitudeType, NewAmplitude)
+	base.RegisterTestConnectionFunc(base.AmplitudeType, TestAmplitude)
+}
+
+// NewAmplitude returns configured Amplitude driver instance
+func NewAmplitude(ctx context.Context, sourceConfig *base.SourceConfig, collection *base.Collection) (base.Driver, error) {
+	config := &AmplitudeConfig{}
+	if err := base.UnmarshalConfig(sourceConfig.Config, config); err != nil {
+		return nil, err
+	}
+
+	if err := config.Validate(); err != nil {
+		return nil, err
+	}
+
+	adapter, err := NewAmplitudeAdapter(sourceConfig.SourceID, config.ApiKey, config.SecretKey)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = adapter.GetStatus(); err != nil {
+		return nil, err
+	}
+
+	return &Amplitude{
+		adapter:    adapter,
+		config:     config,
+		collection: collection,
+	}, nil
+}
+
+// TestAmplitude tests connection to Amplitude without creating Driver instance
+func TestAmplitude(sourceConfig *base.SourceConfig) error {
+	config := &AmplitudeConfig{}
+	if err := base.UnmarshalConfig(sourceConfig.Config, config); err != nil {
+		return err
+	}
+
+	if err := config.Validate(); err != nil {
+		return err
+	}
+
+	adapter, err := NewAmplitudeAdapter(sourceConfig.SourceID, config.ApiKey, config.SecretKey)
+	if err != nil {
+		return err
+	}
+	defer adapter.Close()
+
+	if err = adapter.GetStatus(); err != nil {
+		return err
+	}
+
+	collection := &base.Collection{
+		Name:           "events",
+		DaysBackToLoad: 10,
+	}
+
+	amplitude := &Amplitude{
+		adapter:    adapter,
+		config:     config,
+		collection: collection,
+	}
+
+	intervals, err := amplitude.GetAllAvailableIntervals()
+	if err != nil {
+		return err
+	}
+
+	for _, interval := range intervals {
+		if _, err := amplitude.GetObjectsFor(interval); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (a *Amplitude) Close() error {
+	return a.adapter.Close()
+}
+
+func (a *Amplitude) GetAllAvailableIntervals() ([]*base.TimeInterval, error) {
+	var intervals []*base.TimeInterval
+
+	daysBackToLoad := base.DefaultDaysBackToLoad
+	if a.collection.DaysBackToLoad > 0 {
+		daysBackToLoad = a.collection.DaysBackToLoad
+	}
+
+	now := time.Now().UTC()
+	for i := 0; i < daysBackToLoad; i++ {
+		date := now.AddDate(0, 0, -i)
+		intervals = append(intervals, base.NewTimeInterval(base.DAY, date))
+	}
+
+	return intervals, nil
+}
+
+func (a *Amplitude) GetCollectionMetaKey() string {
+	return a.collection.Name + "_" + a.GetCollectionTable()
+}
+
+func (a *Amplitude) GetCollectionTable() string {
+	return a.collection.GetTableName()
+}
+
+func (a *Amplitude) GetObjectsFor(interval *base.TimeInterval) ([]map[string]interface{}, error) {
+	if a.collection.Name == "events" {
+		eventsArray, err := a.adapter.GetEvents(interval)
+		if err != nil {
+			return nil, err
+		}
+
+		return eventsArray, nil
+	}
+
+	return nil, fmt.Errorf("Unknown collection for amplitude: %v", a.collection.Name)
+}
+
+func (a *Amplitude) Type() string {
+	return base.AmplitudeType
+}
