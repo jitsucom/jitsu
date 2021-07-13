bf046750de0d799d78d6e1a827113e6586d3e14c
Ildar Nurislamov <absorbb@gmail.com>
2021-07-13 16:27:20 +0300

    fix

--- server/templates/jtemplate_test.go
+++ server/templates/jtemplate_test.go
@@ -10,17 +10,17 @@ import (
 )
 
 type templateTestData = struct {
-	name string
+	name      string
 	expFormat string
-	template string
-	event  events.Event
-	expected interface{}
+	template  string
+	event     events.Event
+	expected  interface{}
 }
 
 var templateTest = []templateTestData{
 	{"go1", "go", "{{if .metric_type }}{{ .metric_type }}_test{{else}}{{ .app }}_web_test {{end}}", events.Event{"metric_type": "ogon", "app": "neagon"}, "ogon_test"},
-	{"go2", "go","Hello {{ .metric_type }}", events.Event{"metric_type": "ogon", "app": "neagon"}, "Hello ogon"},
-	{"go3", "go",`{{if or (eq .event_type "user_identify") (eq .event_type "identify")}}
+	{"go2", "go", "Hello {{ .metric_type }}", events.Event{"metric_type": "ogon", "app": "neagon"}, "Hello ogon"},
+	{"go3", "go", `{{if or (eq .event_type "user_identify") (eq .event_type "identify")}}
           {{"identifies"}}
         {{else}}
           {{if or (eq .event_type "page") (eq .event_type "pageview")}}
@@ -29,22 +29,21 @@ var templateTest = []templateTestData{
             {{.event_type}}
           {{end}}
         {{end}}`, events.Event{"event_type": "page"}, "pages"},
-	{"go_json","go", "{\n\t\"blocks\": [\n\t\t{\n\t\t\t\"type\": \"header\",\n\t\t\t\"text\": {\n\t\t\t\t\"type\": \"plain_text\",\n\t\t\t\t\"text\": \"🤘 {{.event_type}} from {{.user.email}}\",\n\t\t\t\t\"emoji\": true\n\t\t\t}\n\t\t},\n\t\t{\n\t\t\t\"type\": \"section\",\n\t\t\t\"text\": {\n\t\t\t\t\"type\": \"mrkdwn\",\n\t\t\t\t\"text\": \"User {{.user.email}} has sent *{{.event_type}}* in <https://exmaple.com?email={{urlquery .user.email}}>\"\n\t\t\t}\n\t\t},\n\t\t{\n\t\t\t\"type\": \"section\",\n\t\t\t\"text\": {\n\t\t\t\t\"type\": \"mrkdwn\",\n\t\t\t\t\"text\": \"*Data*:```{{ json_indent_quote . }}```\"\n\t\t\t}\n\t\t}\n\t]\n}",
+	{"go_json", "go", "{\n\t\"blocks\": [\n\t\t{\n\t\t\t\"type\": \"header\",\n\t\t\t\"text\": {\n\t\t\t\t\"type\": \"plain_text\",\n\t\t\t\t\"text\": \"🤘 {{.event_type}} from {{.user.email}}\",\n\t\t\t\t\"emoji\": true\n\t\t\t}\n\t\t},\n\t\t{\n\t\t\t\"type\": \"section\",\n\t\t\t\"text\": {\n\t\t\t\t\"type\": \"mrkdwn\",\n\t\t\t\t\"text\": \"User {{.user.email}} has sent *{{.event_type}}* in <https://exmaple.com?email={{urlquery .user.email}}>\"\n\t\t\t}\n\t\t},\n\t\t{\n\t\t\t\"type\": \"section\",\n\t\t\t\"text\": {\n\t\t\t\t\"type\": \"mrkdwn\",\n\t\t\t\t\"text\": \"*Data*:```{{ json_indent_quote . }}```\"\n\t\t\t}\n\t\t}\n\t]\n}",
 		events.Event{"event_type": "page", "user": object{"email": "test@test.com"}}, "{\n\t\"blocks\": [\n\t\t{\n\t\t\t\"type\": \"header\",\n\t\t\t\"text\": {\n\t\t\t\t\"type\": \"plain_text\",\n\t\t\t\t\"text\": \"🤘 page from test@test.com\",\n\t\t\t\t\"emoji\": true\n\t\t\t}\n\t\t},\n\t\t{\n\t\t\t\"type\": \"section\",\n\t\t\t\"text\": {\n\t\t\t\t\"type\": \"mrkdwn\",\n\t\t\t\t\"text\": \"User test@test.com has sent *page* in <https://exmaple.com?email=test%40test.com>\"\n\t\t\t}\n\t\t},\n\t\t{\n\t\t\t\"type\": \"section\",\n\t\t\t\"text\": {\n\t\t\t\t\"type\": \"mrkdwn\",\n\t\t\t\t\"text\": \"*Data*:```{\\n  \\\"event_type\\\": \\\"page\\\",\\n  \\\"user\\\": {\\n    \\\"email\\\": \\\"test@test.com\\\"\\n  }\\n}```\"\n\t\t\t}\n\t\t}\n\t]\n}"},
-	{"js_json","javascript", "var koza = \"🤘\"\nreturn {\n\t\"blocks\": [\n\t\t{\n\t\t\t\"type\": \"header\",\n\t\t\t\"text\": {\n\t\t\t\t\"type\": \"plain_text\",\n\t\t\t\t\"text\": `${koza} ${$.event_type} from ${$.user.email}`,\n\t\t\t\t\"emoji\": true\n\t\t\t}\n\t\t},\n\t\t{\n\t\t\t\"type\": \"section\",\n\t\t\t\"text\": {\n\t\t\t\t\"type\": \"mrkdwn\",\n\t\t\t\t\"text\": `User ${$.user.email} has sent *${$.event_type}* in <https://example.com?email=${encodeURIComponent($.user?.email)}>`\n\t\t\t}\n\t\t},\n\t\t{\n\t\t\t\"type\": \"section\",\n\t\t\t\"text\": {\n\t\t\t\t\"type\": \"mrkdwn\",\n\t\t\t\t\"text\": \"*Data*:```\" + json_indent_quote($) + \"```\"\n\t\t\t}\n\t\t}\n\t]\n}",
+	{"js_json", "javascript", "var koza = \"🤘\"\nreturn {\n\t\"blocks\": [\n\t\t{\n\t\t\t\"type\": \"header\",\n\t\t\t\"text\": {\n\t\t\t\t\"type\": \"plain_text\",\n\t\t\t\t\"text\": `${koza} ${$.event_type} from ${$.user.email}`,\n\t\t\t\t\"emoji\": true\n\t\t\t}\n\t\t},\n\t\t{\n\t\t\t\"type\": \"section\",\n\t\t\t\"text\": {\n\t\t\t\t\"type\": \"mrkdwn\",\n\t\t\t\t\"text\": `User ${$.user.email} has sent *${$.event_type}* in <https://example.com?email=${encodeURIComponent($.user?.email)}>`\n\t\t\t}\n\t\t},\n\t\t{\n\t\t\t\"type\": \"section\",\n\t\t\t\"text\": {\n\t\t\t\t\"type\": \"mrkdwn\",\n\t\t\t\t\"text\": \"*Data*:```\" + json_indent_quote($) + \"```\"\n\t\t\t}\n\t\t}\n\t]\n}",
 		events.Event{"event_type": "page", "user": object{"email": "test@test.com"}},
-		object{"blocks":[]interface{}{object{"text":object{"emoji":true, "text":"🤘 page from test@test.com", "type":"plain_text"}, "type":"header"}, object{"text":object{"text":"User test@test.com has sent *page* in <https://example.com?email=test%40test.com>", "type":"mrkdwn"}, "type":"section"}, object{"text":object{"text":"*Data*:```{\\n  \\\"event_type\\\": \\\"page\\\",\\n  \\\"user\\\": {\\n    \\\"email\\\": \\\"test@test.com\\\"\\n  }\\n}```", "type":"mrkdwn"}, "type":"section"}}}},
+		object{"blocks": []interface{}{object{"text": object{"emoji": true, "text": "🤘 page from test@test.com", "type": "plain_text"}, "type": "header"}, object{"text": object{"text": "User test@test.com has sent *page* in <https://example.com?email=test%40test.com>", "type": "mrkdwn"}, "type": "section"}, object{"text": object{"text": "*Data*:```{\\n  \\\"event_type\\\": \\\"page\\\",\\n  \\\"user\\\": {\\n    \\\"email\\\": \\\"test@test.com\\\"\\n  }\\n}```", "type": "mrkdwn"}, "type": "section"}}}},
 	{"go_and_js_error", "error", "if (true) {{return func(){ .metric_type }}}", events.Event{"metric_type": "ogon", "app": "neagon"}, fmt.Errorf("2 errors occurred")},
-	{"golike_js","javascript", "if (true) {var a = function(){ metric_type }} return $.metric_type", events.Event{"metric_type": "ogon", "app": "neagon"}, "ogon"},
+	{"golike_js", "javascript", "if (true) {var a = function(){ metric_type }} return $.metric_type", events.Event{"metric_type": "ogon", "app": "neagon"}, "ogon"},
 
-	{"const: \"table_name\"","constant", "\"table_name\"", events.Event{"metric_type": "ogon"}, "\"table_name\""},
-	{"const: table_name","constant", "table_name", events.Event{"metric_type": "ogon"}, "table_name"},
-	{"const: table name (err)","constant", "table name", events.Event{"metric_type": "ogon"}, fmt.Errorf("javascript error: ReferenceError")},
-	{"const: \"table name\"","constant", "\"table name\"", events.Event{"metric_type": "ogon"}, "\"table name\""},
-	{"const: data_base.table1","constant", "data_base.table1", events.Event{"metric_type": "ogon"}, "data_base.table1"},
-	{"const: \"data base\".\"table1\"","constant", "\"data base\".\"table1\"", events.Event{"metric_type": "ogon"}, "\"data base\".\"table1\""},
-	{"const: url","constant", "https://hooks.slack.com/services/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", events.Event{"metric_type": "ogon"}, "https://hooks.slack.com/services/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"},
-
+	{"const: \"table_name\"", "constant", "\"table_name\"", events.Event{"metric_type": "ogon"}, "\"table_name\""},
+	{"const: table_name", "constant", "table_name", events.Event{"metric_type": "ogon"}, "table_name"},
+	{"const: table name (err)", "constant", "table name", events.Event{"metric_type": "ogon"}, fmt.Errorf("javascript error: ReferenceError")},
+	{"const: \"table name\"", "constant", "\"table name\"", events.Event{"metric_type": "ogon"}, "\"table name\""},
+	{"const: data_base.table1", "constant", "data_base.table1", events.Event{"metric_type": "ogon"}, "data_base.table1"},
+	{"const: \"data base\".\"table1\"", "constant", "\"data base\".\"table1\"", events.Event{"metric_type": "ogon"}, "\"data base\".\"table1\""},
+	{"const: url", "constant", "https://example.com/123/abc123", events.Event{"metric_type": "ogon"}, "https://example.com/123/abc123"},
 }
 
 func TestJtemplate(t *testing.T) {
