e35ff06b1304d83d540efb19127a1aa3aff78911
taletski <kirill@taletski.com>
2021-07-27 13:48:29 +0300

    adds responsive height and width to all svgs

--- configurator/frontend/src/catalog/destinations/lib/clickhouse.tsx
+++ configurator/frontend/src/catalog/destinations/lib/clickhouse.tsx
@@ -4,15 +4,22 @@ import { arrayOf, stringType } from '../../sources/types';
 import * as React from 'react';
 import { ReactNode } from 'react';
 
-let icon: ReactNode = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 8">
-  <style>{'.o{fill:#fc0}.r{fill:red}'}</style>
-  <path d="M0,7 h1 v1 h-1 z" className="r"/>
-  <path d="M0,0 h1 v7 h-1 z" className="o"/>
-  <path d="M2,0 h1 v8 h-1 z" className="o"/>
-  <path d="M4,0 h1 v8 h-1 z" className="o"/>
-  <path d="M6,0 h1 v8 h-1 z" className="o"/>
-  <path d="M8,3.25 h1 v1.5 h-1 z" className="o"/>
-</svg>
+let icon: ReactNode = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="100%"
+    width="100%"
+    viewBox="0 0 9 8"
+  >
+    <style>{'.o{fill:#fc0}.r{fill:red}'}</style>
+    <path d="M0,7 h1 v1 h-1 z" className="r" />
+    <path d="M0,0 h1 v7 h-1 z" className="o" />
+    <path d="M2,0 h1 v8 h-1 z" className="o" />
+    <path d="M4,0 h1 v8 h-1 z" className="o" />
+    <path d="M6,0 h1 v8 h-1 z" className="o" />
+    <path d="M8,3.25 h1 v1.5 h-1 z" className="o" />
+  </svg>
+);
 
 const destination = {
   description: <>
