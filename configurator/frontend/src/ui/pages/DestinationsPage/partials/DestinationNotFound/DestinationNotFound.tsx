540138b471923a56ca8e004b4ff7c1343ec7c75a
taletski <kirill@taletski.com>
2021-08-02 13:57:21 +0300

    adds DestinationNotFound page

---
+++ configurator/frontend/src/ui/pages/DestinationsPage/partials/DestinationNotFound/DestinationNotFound.tsx
@@ -0,0 +1,32 @@
+import { Button } from 'antd';
+import { useHistory } from 'react-router-dom';
+import { destinationPageRoutes } from '../../DestinationsPage.routes';
+
+type Props = {
+  destinationId?: string;
+};
+
+export const DestinationNotFound: React.FC<Props> = ({ destinationId }) => {
+  const history = useHistory();
+  return (
+    <section className="flex flex-col justify-center items-center w-full h-full">
+      <span className="text-7xl" role="img">
+        {'🕵'}
+      </span>
+      <h3 className="text-4xl">
+        Destination{' '}
+        {destinationId && (
+          <span className="font-extrabold">{destinationId}</span>
+        )}{' '}
+        not found
+      </h3>
+      <Button
+        type="primary"
+        size="large"
+        onClick={() => history.push(destinationPageRoutes.root)}
+      >
+        {'Go to Destinations List'}
+      </Button>
+    </section>
+  );
+};
