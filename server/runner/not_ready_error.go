7d113366bfc691f3bad640060577f61fc521193e
Sergey Burykin <sburykin@jitsu.com>
2021-08-24 12:41:42 +0300

    fixed: not ready error configs reloading interval docs

--- server/runner/not_ready_error.go
+++ server/runner/not_ready_error.go
@@ -10,9 +10,9 @@ func NewNotReadyError(previousError string) *NotReadyError {
 
 //Error returns not ready error
 func (nre *NotReadyError) Error() string {
-	msg := "not ready"
+	msg := "not ready. Please retry attempt a bit later"
 	if nre.previousError != "" {
-		msg += ": " + nre.previousError
+		msg += ". previous error: " + nre.previousError
 	}
 
 	return msg
