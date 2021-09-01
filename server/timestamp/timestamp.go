e20adca333628424f82f39f643eebc2f1ca47be8
Maksim Lebedev <Alessarik@gmail.com>
2021-09-01 22:34:07 +0300

    Using atomic bool

--- server/timestamp/timestamp.go
+++ server/timestamp/timestamp.go
@@ -1,26 +1,30 @@
 package timestamp
 
-import "time"
+import (
+	"time"
+
+	"go.uber.org/atomic"
+)
 
 var (
 	// The value of freezed time that is used in all tests
 	freezedTime = time.Date(2020, 06, 16, 23, 0, 0, 0, time.UTC)
 
 	// Indicator shows that time was freezed or was not freezed
-	timeFreezed = false
+	timeFreezed = atomic.NewBool(false)
 )
 
 func Now() time.Time {
-	if timeFreezed {
+	if timeFreezed.Load() {
 		return freezedTime
 	}
 	return time.Now()
 }
 
 func FreezeTime() {
-	timeFreezed = true
+	timeFreezed.Store(true)
 }
 
 func UnfreezeTime() {
-	timeFreezed = false
+	timeFreezed.Store(false)
 }
