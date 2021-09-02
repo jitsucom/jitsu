c36abce8fc87b9a02d7d0cf6bad2b92dc42bab94
Sergey Burykin <sburykin@jitsu.com>
2021-09-02 17:30:22 +0300

    fixed time.Now -> timestamp.Now

--- server/logging/proxy.go
+++ server/logging/proxy.go
@@ -1,10 +1,8 @@
 package logging
 
 import (
-	"io"
-	"time"
-
 	"github.com/jitsucom/jitsu/server/timestamp"
+	"io"
 )
 
 type DateTimeWriterProxy struct {
@@ -12,7 +10,7 @@ type DateTimeWriterProxy struct {
 }
 
 func (wp DateTimeWriterProxy) Write(bytes []byte) (int, error) {
-	return wp.writer.Write([]byte(time.Now().UTC().Format(timestamp.LogsLayout) + " " + string(bytes)))
+	return wp.writer.Write([]byte(timestamp.Now().UTC().Format(timestamp.LogsLayout) + " " + string(bytes)))
 }
 
 type PrefixDateTimeProxy struct {
@@ -25,5 +23,5 @@ func NewPrefixDateTimeProxy(prefix string, writer io.Writer) io.Writer {
 }
 
 func (pwp PrefixDateTimeProxy) Write(bytes []byte) (int, error) {
-	return pwp.writer.Write([]byte(time.Now().UTC().Format(timestamp.LogsLayout) + " " + pwp.prefix + " " + string(bytes)))
+	return pwp.writer.Write([]byte(timestamp.Now().UTC().Format(timestamp.LogsLayout) + " " + pwp.prefix + " " + string(bytes)))
 }
