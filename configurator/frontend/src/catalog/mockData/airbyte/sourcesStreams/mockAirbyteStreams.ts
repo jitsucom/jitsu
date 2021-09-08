5f31df7df036f1393112b01e9212ef4b11e0fb27
taletski <kirill@taletski.com>
2021-09-08 12:26:44 +0300

    prepares the testing framework for the airbyte streams

--- configurator/frontend/src/catalog/mockData/airbyte/sourcesStreams/mockAirbyteStreams.ts
+++ configurator/frontend/src/catalog/mockData/airbyte/sourcesStreams/mockAirbyteStreams.ts
@@ -1,114 +1,114 @@
 export const mockAirbyteSourcesStreams = [
-  {
-    name: 'annotations',
-    json_schema: {
-      $schema: 'http://json-schema.org/draft-07/schema#',
-      type: 'object',
-      additionalProperties: false,
-      properties: {
-        date: { type: ['null', 'string'], format: 'date-time' },
-        project_id: { type: ['null', 'integer'] },
-        id: { type: ['null', 'integer'] },
-        description: { type: ['null', 'string'] }
-      }
-    },
-    supported_sync_modes: ['full_refresh'],
-    source_defined_primary_key: [['id']]
-  },
-  {
-    name: 'cohorts',
-    json_schema: {
-      $schema: 'http://json-schema.org/draft-07/schema#',
-      type: 'object',
-      additionalProperties: false,
-      properties: {
-        id: { type: ['null', 'integer'] },
-        name: { type: ['null', 'string'] },
-        description: { type: ['null', 'string'] },
-        created: { type: ['null', 'string'], format: 'date-time' },
-        count: { type: ['null', 'integer'] },
-        is_visible: { type: ['null', 'integer'] },
-        project_id: { type: ['null', 'integer'] }
-      }
-    },
-    supported_sync_modes: ['full_refresh'],
-    source_defined_primary_key: [['id']]
-  },
-  {
-    name: 'cohort_members',
-    json_schema: {
-      $schema: 'http://json-schema.org/draft-07/schema#',
-      type: 'object',
-      additionalProperties: true,
-      properties: {
-        cohort_id: { type: ['null', 'integer'] },
-        distinct_id: { type: ['null', 'string'] }
-      }
-    },
-    supported_sync_modes: ['full_refresh'],
-    source_defined_primary_key: [['distinct_id']]
-  },
-  {
-    name: 'engage',
-    json_schema: {
-      $schema: 'http://json-schema.org/draft-07/schema#',
-      type: 'object',
-      additionalProperties: true,
-      properties: { distinct_id: { type: ['null', 'string'] } }
-    },
-    supported_sync_modes: ['full_refresh'],
-    source_defined_primary_key: [['distinct_id']]
-  },
-  {
-    name: 'export',
-    json_schema: {
-      $schema: 'http://json-schema.org/draft-07/schema#',
-      type: 'object',
-      additionalProperties: true,
-      properties: {
-        event: { type: ['null', 'string'] },
-        distinct_id: { type: ['null', 'string'] },
-        time: { type: ['null', 'string'], format: 'date-time' },
-        labels: {
-          anyOf: [
-            { type: 'array', items: { type: 'string' } },
-            { type: 'null' }
-          ]
-        },
-        sampling_factor: { type: ['null', 'integer'] },
-        dataset: { type: ['null', 'string'] },
-        browser_version: { type: ['null', 'string'] },
-        current_url: { type: ['null', 'string'] },
-        device_id: { type: ['null', 'string'] },
-        initial_referrer: { type: ['null', 'string'] },
-        initial_referring_domain: { type: ['null', 'string'] },
-        lib_version: { type: ['null', 'string'] },
-        screen_height: { type: ['null', 'string'] },
-        screen_width: { type: ['null', 'string'] },
-        mp_lib: { type: ['null', 'string'] },
-        event_name: { type: ['null', 'string'] },
-        mp_country_code: { type: ['null', 'string'] },
-        region: { type: ['null', 'string'] },
-        city: { type: ['null', 'string'] },
-        browser: { type: ['null', 'string'] },
-        os: { type: ['null', 'string'] },
-        referrer: { type: ['null', 'string'] },
-        referring_domain: { type: ['null', 'string'] },
-        search_engine: { type: ['null', 'string'] },
-        device: { type: ['null', 'string'] },
-        utm_source: { type: ['null', 'string'] },
-        utm_campaign: { type: ['null', 'string'] },
-        mp_keyword: { type: ['null', 'string'] },
-        duration_s: { type: ['null', 'string'] },
-        event_count: { type: ['null', 'string'] },
-        origin_end: { type: ['null', 'string'] },
-        origin_start: { type: ['null', 'string'] }
-      }
-    },
-    supported_sync_modes: ['full_refresh', 'incremental'],
-    source_defined_cursor: true,
-    default_cursor_field: ['time']
-  },
+  // {
+  //   name: 'annotations',
+  //   json_schema: {
+  //     $schema: 'http://json-schema.org/draft-07/schema#',
+  //     type: 'object',
+  //     additionalProperties: false,
+  //     properties: {
+  //       date: { type: ['null', 'string'], format: 'date-time' },
+  //       project_id: { type: ['null', 'integer'] },
+  //       id: { type: ['null', 'integer'] },
+  //       description: { type: ['null', 'string'] }
+  //     }
+  //   },
+  //   supported_sync_modes: ['full_refresh'],
+  //   source_defined_primary_key: [['id']]
+  // },
+  // {
+  //   name: 'cohorts',
+  //   json_schema: {
+  //     $schema: 'http://json-schema.org/draft-07/schema#',
+  //     type: 'object',
+  //     additionalProperties: false,
+  //     properties: {
+  //       id: { type: ['null', 'integer'] },
+  //       name: { type: ['null', 'string'] },
+  //       description: { type: ['null', 'string'] },
+  //       created: { type: ['null', 'string'], format: 'date-time' },
+  //       count: { type: ['null', 'integer'] },
+  //       is_visible: { type: ['null', 'integer'] },
+  //       project_id: { type: ['null', 'integer'] }
+  //     }
+  //   },
+  //   supported_sync_modes: ['full_refresh'],
+  //   source_defined_primary_key: [['id']]
+  // },
+  // {
+  //   name: 'cohort_members',
+  //   json_schema: {
+  //     $schema: 'http://json-schema.org/draft-07/schema#',
+  //     type: 'object',
+  //     additionalProperties: true,
+  //     properties: {
+  //       cohort_id: { type: ['null', 'integer'] },
+  //       distinct_id: { type: ['null', 'string'] }
+  //     }
+  //   },
+  //   supported_sync_modes: ['full_refresh'],
+  //   source_defined_primary_key: [['distinct_id']]
+  // },
+  // {
+  //   name: 'engage',
+  //   json_schema: {
+  //     $schema: 'http://json-schema.org/draft-07/schema#',
+  //     type: 'object',
+  //     additionalProperties: true,
+  //     properties: { distinct_id: { type: ['null', 'string'] } }
+  //   },
+  //   supported_sync_modes: ['full_refresh'],
+  //   source_defined_primary_key: [['distinct_id']]
+  // },
+  // {
+  //   name: 'export',
+  //   json_schema: {
+  //     $schema: 'http://json-schema.org/draft-07/schema#',
+  //     type: 'object',
+  //     additionalProperties: true,
+  //     properties: {
+  //       event: { type: ['null', 'string'] },
+  //       distinct_id: { type: ['null', 'string'] },
+  //       time: { type: ['null', 'string'], format: 'date-time' },
+  //       labels: {
+  //         anyOf: [
+  //           { type: 'array', items: { type: 'string' } },
+  //           { type: 'null' }
+  //         ]
+  //       },
+  //       sampling_factor: { type: ['null', 'integer'] },
+  //       dataset: { type: ['null', 'string'] },
+  //       browser_version: { type: ['null', 'string'] },
+  //       current_url: { type: ['null', 'string'] },
+  //       device_id: { type: ['null', 'string'] },
+  //       initial_referrer: { type: ['null', 'string'] },
+  //       initial_referring_domain: { type: ['null', 'string'] },
+  //       lib_version: { type: ['null', 'string'] },
+  //       screen_height: { type: ['null', 'string'] },
+  //       screen_width: { type: ['null', 'string'] },
+  //       mp_lib: { type: ['null', 'string'] },
+  //       event_name: { type: ['null', 'string'] },
+  //       mp_country_code: { type: ['null', 'string'] },
+  //       region: { type: ['null', 'string'] },
+  //       city: { type: ['null', 'string'] },
+  //       browser: { type: ['null', 'string'] },
+  //       os: { type: ['null', 'string'] },
+  //       referrer: { type: ['null', 'string'] },
+  //       referring_domain: { type: ['null', 'string'] },
+  //       search_engine: { type: ['null', 'string'] },
+  //       device: { type: ['null', 'string'] },
+  //       utm_source: { type: ['null', 'string'] },
+  //       utm_campaign: { type: ['null', 'string'] },
+  //       mp_keyword: { type: ['null', 'string'] },
+  //       duration_s: { type: ['null', 'string'] },
+  //       event_count: { type: ['null', 'string'] },
+  //       origin_end: { type: ['null', 'string'] },
+  //       origin_start: { type: ['null', 'string'] }
+  //     }
+  //   },
+  //   supported_sync_modes: ['full_refresh', 'incremental'],
+  //   source_defined_cursor: true,
+  //   default_cursor_field: ['time']
+  // },
   {
     name: 'funnels',
     json_schema: {
@@ -202,24 +202,24 @@ export const mockAirbyteSourcesStreams = [
     source_defined_cursor: true,
     default_cursor_field: ['date'],
     source_defined_primary_key: [['funnel_id'], ['date']]
-  },
-  {
-    name: 'revenue',
-    json_schema: {
-      $schema: 'http://json-schema.org/draft-07/schema#',
-      type: 'object',
-      additionalProperties: false,
-      properties: {
-        date: { type: ['null', 'string'], format: 'date' },
-        datetime: { type: ['null', 'string'], format: 'date-time' },
-        count: { type: ['null', 'integer'] },
-        paid_count: { type: ['null', 'integer'] },
-        amount: { type: ['null', 'number'], multipleOf: 1e-20 }
-      }
-    },
-    supported_sync_modes: ['full_refresh', 'incremental'],
-    source_defined_cursor: true,
-    default_cursor_field: ['date'],
-    source_defined_primary_key: [['date']]
   }
+  // {
+  //   name: 'revenue',
+  //   json_schema: {
+  //     $schema: 'http://json-schema.org/draft-07/schema#',
+  //     type: 'object',
+  //     additionalProperties: false,
+  //     properties: {
+  //       date: { type: ['null', 'string'], format: 'date' },
+  //       datetime: { type: ['null', 'string'], format: 'date-time' },
+  //       count: { type: ['null', 'integer'] },
+  //       paid_count: { type: ['null', 'integer'] },
+  //       amount: { type: ['null', 'number'], multipleOf: 1e-20 }
+  //     }
+  //   },
+  //   supported_sync_modes: ['full_refresh', 'incremental'],
+  //   source_defined_cursor: true,
+  //   default_cursor_field: ['date'],
+  //   source_defined_primary_key: [['date']]
+  // }
 ] as const;
