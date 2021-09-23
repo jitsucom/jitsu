559c0536ff8c97f047d342b8be3cf253bafaf053
Vladimir Klimontovich <vladimir@jitsu.com>
2021-09-22 17:48:25 -0700

    Billing redirects

--- configurator/frontend/src/ui/components/CurrentPlan/CurrentPlan.tsx
+++ configurator/frontend/src/ui/components/CurrentPlan/CurrentPlan.tsx
@@ -49,7 +49,9 @@ export const CurrentPlan: React.FC<CurrentPlanProps> = ({
             <Progress
               percent={usagaPct}
               showInfo={false}
-              status={usagaPct >= 100 ? 'exception' : 'active'}
+              status={usagaPct >= 100 ?
+                'exception' :
+                'active'}
             />
           </div>
           <table>
@@ -76,17 +78,23 @@ export const CurrentPlan: React.FC<CurrentPlanProps> = ({
             </tr>
           </table>
         </div>
-        <div className="text-center mt-2">
-          <a href="https://jitsu.com/pricing">Pricing</a> •{' '}
-          <a
-            onClick={() => {
-              onPlanChangeModalOpen();
-              services.analyticsService.track('upgrade_plan_requested');
-              setUpgradeDialogVisible(true);
-            }}
-          >
-            {planStatus.currentPlan.id === 'free' ? 'Upgrade' : 'Manage'}
-          </a>
+        <div className="text-center mt-2 flex flex-col items-center space-y-4">
+          {planStatus.currentPlan.id === 'free' ?
+            <a
+              onClick={() => {
+                onPlanChangeModalOpen()
+                onPlanChangeModalOpen();
+                services.analyticsService.track('upgrade_plan_requested');
+                setUpgradeDialogVisible(true);
+              }}><b>Upgrade</b></a> :
+            <a href={generateCustomerPortalLink({
+              project_id: services.activeProject.id,
+              user_email: services.userService.getUser().email,
+              return_url: window.location.href
+            })}>Manage Subscription</a>}
+
+            <a href="https://jitsu.com/pricing">Pricing Info</a>
+
         </div>
       </div>
       <PlanUpgradeDialog
@@ -103,91 +111,58 @@ export const PlanUpgradeDialog: React.FC<{
   hide: () => void;
   planStatus: PaymentPlanStatus;
 }> = ({ visible, hide, planStatus }) => {
-  const [selectedPlanId, setSelectedPlanId] = useState<string>(
-    planStatus.currentPlan.id
-  );
+  const [selectedPlanId, setSelectedPlanId] = useState<string>('growth');
   const [buttonLoading, setLoading] = useState(false);
   const services = useServices();
 
-  const selectedPlan = useMemo<PaymentPlan | null>(
-    () => paymentPlans[selectedPlanId] ?? null,
-    [selectedPlanId]
-  );
 
   const actionLabel =
-    selectedPlanId === planStatus.currentPlan.id ? (
-      'Subscription Active'
-    ) : selectedPlanId === 'enterprise' ? (
+    selectedPlanId === 'enterprise' ?
       <>
         <span className="mr-1">Contact</span>
         <Typography.Text copyable className="font-bold">
           sales@jitsu.com
         </Typography.Text>
-      </>
-    ) : selectedPlan.price_amount > planStatus.currentPlan.price_amount ? (
-      'Upgrade'
-    ) : (
-      'Downgrade'
-    );
+      </> : 'Upgrade'
 
   const buttonProps = (plan: PaymentPlan): ButtonProps => {
     return {
       className: cn(
         styles.optionButton,
-        selectedPlanId === plan.id ? styles.selectedOption : null
+        selectedPlanId === plan.id ?
+          styles.selectedOption :
+          null
       ),
       onClick: () => setSelectedPlanId(plan.id)
     };
   };
 
-  const handleProceedToCheckout = useCallback(async () => {
+  const handleProceedToCheckout = useCallback(async() => {
     setLoading(true);
     try {
       await services.analyticsService.track('upgrade_plan', {
-        event:
-          selectedPlan.price_amount > planStatus.currentPlan.price_amount
-            ? 'upgrade_plan'
-            : 'downgrade_plan',
-        plan: selectedPlan.id,
+        event: 'upgrade_plan',
+        plan: selectedPlanId,
         user: services.userService.getUser().email
       });
       const user = services.userService.getUser();
       window.location.href = generateCheckoutLink({
         project_id: user.projects[0].id,
-        current_plan_id: planStatus.currentPlan.id,
-        plan_id_to_purchase: selectedPlan.id,
         user_email: user.email,
-        success_url: window.location.href,
-        cancel_url: window.location.href
+        plan_id: selectedPlanId,
+        redirect_base: window.location.href
       });
     } catch (e) {
       handleError(e);
     } finally {
       setLoading(false);
     }
-  }, [selectedPlan, planStatus.currentPlan]);
-
-  const handleProceedToCustomerPortal = useCallback(() => {
-    setLoading(true);
-    try {
-      const user = services.userService.getUser();
-      window.location.href = generateCustomerPortalLink({
-        project_id: user.projects[0].id,
-        current_plan_id: planStatus.currentPlan.id,
-        user_email: user.email,
-        return_url: window.location.href
-      });
-    } catch (e) {
-      handleError(e);
-    } finally {
-      setLoading(false);
-    }
-  }, [selectedPlan]);
+  }, [selectedPlanId, planStatus.currentPlan]);
 
   return (
     <Modal
       destroyOnClose={true}
-      title={<h1 className="text-xl m-0 p-0">Manage Subscription</h1>}
+      title={<h1 className="text-xl m-0 p-0">Updgrade subscription</h1>}
       visible={visible}
       onCancel={() => {
         hide();
@@ -195,30 +170,9 @@ export const PlanUpgradeDialog: React.FC<{
       footer={visible}
     >
       <>
-        {planStatus.isStripeCustomer && (
-          <>
-            <h3 className="font-bold">
-              Manage Payment Methods and Billing Details
-            </h3>
-            <div className="flex justify-center items-center">
-              <Button
-                size="large"
-                type="primary"
-                className="mt-2 mb-2"
-                onClick={handleProceedToCustomerPortal}
-              >
-                Go To Customer Portal
-              </Button>
-            </div>
-            <Divider plain>or</Divider>
-          </>
-        )}
+
         <h3 className="font-bold">Subscribe to a Different Plan</h3>
         <div className={styles.plan}>
-          <div>
-            <div {...buttonProps(paymentPlans.free)}>Startup</div>
-            <div className={styles.planPrice}>FREE</div>
-          </div>
           <div>
             <div {...buttonProps(paymentPlans.growth)}>Growth</div>
             <div className={styles.planPrice}>$99 / month</div>
@@ -233,28 +187,34 @@ export const PlanUpgradeDialog: React.FC<{
           </div>
         </div>
         <div className="flex justify-center mt-6">
-          {selectedPlanId === 'enterprise' ? (
-            <span className="flex items-center text-base h-10">
+          {selectedPlanId === 'enterprise' ?
+            (
+              <span className="flex items-center text-base h-10">
               {actionLabel}
             </span>
-          ) : selectedPlanId === planStatus.currentPlan.id ? (
-            <span className="flex items-center text-base h-10">
+            ) :
+            selectedPlanId === planStatus.currentPlan.id ?
+              (
+                <span className="flex items-center text-base h-10">
               {actionLabel}
             </span>
-          ) : (
-            <Button
-              size="large"
-              loading={buttonLoading}
-              type={selectedPlanId === 'enterprise' ? 'ghost' : 'primary'}
-              disabled={selectedPlanId === planStatus.currentPlan.id}
-              onClick={handleProceedToCheckout}
-            >
-              {actionLabel}
-            </Button>
-          )}
+              ) :
+              (
+                <Button
+                  size="large"
+                  loading={buttonLoading}
+                  type={selectedPlanId === 'enterprise' ?
+                    'ghost' :
+                    'primary'}
+                  disabled={selectedPlanId === planStatus.currentPlan.id}
+                  onClick={handleProceedToCheckout}
+                >
+                  {actionLabel}
+                </Button>
+              )}
         </div>
         <div className="flex justify-center pt-3">
-          <a target="_blank" href="https://jitsu.com" rel="noreferrer">
+          <a target="_blank" href="https://jitsu.com/pricing" rel="noreferrer">
             Read more about pricing options
           </a>
         </div>
