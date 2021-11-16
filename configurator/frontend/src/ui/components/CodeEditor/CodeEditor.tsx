8a6fea0c85156aeeed19f02622cfc7f7eba79bf7
taletski <kirill@taletski.com>
2021-11-16 17:17:56 +0300

    applies lazy-loading to the Onboarding

--- configurator/frontend/src/ui/components/CodeEditor/CodeEditor.tsx
+++ configurator/frontend/src/ui/components/CodeEditor/CodeEditor.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import { CenteredSpin } from "lib/components/components"
 import { Props } from "./CodeEditor.types"
 
-// import CodeEditorComponent from "ui/components/CodeEditor/CodeEditorComponent"
 const CodeEditorComponent = React.lazy(() => import("ui/components/CodeEditor/CodeEditorComponent"))
 
 export const CodeEditor = (props: Props) => (
