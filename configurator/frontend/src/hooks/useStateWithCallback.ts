d1bd3a1a0d69e8d72d17fd3f4014d098cc011f60
taletski <kirill@taletski.com>
2021-11-20 16:12:08 +0300

    adds code debugger warning upon quit without saving

--- configurator/frontend/src/hooks/useStateWithCallback.ts
+++ configurator/frontend/src/hooks/useStateWithCallback.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import ReactDOM from "react-dom"
 
 type NewStateOrLazySetState<T> = T | ((prevState: T) => T)
@@ -25,30 +25,34 @@ export const useStateWithCallback = <T = any>(initialState: T): [T, SetState<T>]
     state: initialState,
   })
 
-  const setStateWithCallback = useCallback<SetState<T>>((newStateOrLazySetState, callback?) => {
+  const setStateWithCallback = useCallback<SetState<T>>(async (newStateOrLazySetState, callback?) => {
     /**
-     * `flushSync` disables state updates batching
-     * This is needed to prevent loosing callbacks in case of multiple calls of setStateWithCallback
-     * since those callbacks are stored in state
+     * `flushSync` disables state updates batching.
+     * This might be needed to prevent loosing callbacks in case of multiple calls of setStateWithCallback
+     * since those callbacks are stored in state.
+     *
+     * The problem with flushSync is that React can not flush during the rendering
+     * This means setStateWithCallback can not be called in the lifecycle methods -- react will throw warnings to console.
+     *
+     * Remove this code and comment later on if this proves to work fine without flushing.
      * */
-    ReactDOM.flushSync(() => {
-      /** this part works the same as stock setState */
-      if (newStateOrLazySetState instanceof Function) {
-        const lazySetState = newStateOrLazySetState
-        setState(prev => {
-          return {
-            state: lazySetState(prev.state),
-            callback,
-          }
-        })
-      } else {
-        const newState = newStateOrLazySetState
-        setState({
-          state: newState,
+
+    /** this part works the same as an ordinary setState */
+    if (newStateOrLazySetState instanceof Function) {
+      const lazySetState = newStateOrLazySetState
+      setState(prev => {
+        return {
+          state: lazySetState(prev.state),
           callback,
-        })
-      }
-    })
+        }
+      })
+    } else {
+      const newState = newStateOrLazySetState
+      setState({
+        state: newState,
+        callback,
+      })
+    }
   }, [])
 
   /** Runs the callback once state has changed */
