1d655595658159be9f4102ae93e1a90fefa988ff
Ildar Nurislamov <absorbb@gmail.com>
2021-12-09 21:22:07 +0300

    Fix

--- server/integration_tests/retroactive_users_recognition_test.go
+++ server/integration_tests/retroactive_users_recognition_test.go
@@ -161,7 +161,7 @@ func TestPostgresRetroactiveUsersRecognition(t *testing.T) {
 
 			time.Sleep(2 * time.Second)
 
-			objects, err := postgresContainer.GetAllSortedRows("recognition_events", "user_anonymous_id="+testData.anonymousId, "order by eventn_ctx_utc_time")
+			objects, err := postgresContainer.GetAllSortedRows("recognition_events", "eventn_ctx_user_anonymous_id="+testData.anonymousId, "order by eventn_ctx_utc_time")
 			require.NoError(t, err, "Error selecting all events")
 			require.Equal(t, testData.expCount, len(objects), "Rows count must be %d", testData.expCount)
 
