f400a4eaa5e7865330d39658d82593b254c9299d
Vladimir Klimontovich <vladimir@jitsu.com>
2022-01-13 12:51:49 +0300

    Node JS SDK - WIP

--- javascript-sdk/__tests__/npm.spec.ts
+++ javascript-sdk/__tests__/npm.spec.ts
@@ -1,20 +1,61 @@
-import {jitsuClient} from '../src/jitsu'
-import {JitsuClient} from '../src/interface'
-test('Test Jitsu Client npm only', async () => {
-  let fetchMock = jest.mock('node-fetch')
-  let jitsu: JitsuClient = jitsuClient({
-    fetch: fetchMock,
-    key: "Test",
-    tracking_host: "https://some-host"
-  });
-  const req: Request = {}
-  const res: Response = {}
-  await jitsu.id({
-    email: 'a@b.c',
-    id: 'someId'
-  }, true)
+import { jitsuClient } from "../src/jitsu";
+import { JitsuClient } from "../src/interface";
+import fs from "fs"
+import express from 'express'
+import bodyParser from 'body-parser';
+import * as core from "express-serve-static-core"
 
-  await jitsu.track('test', {
-    req, res
+let testServer = null;
+
+async function createTestServer()  {
+  const app = express();
+  app.use(bodyParser.json())
+  app.use(bodyParser.text())
+  testServer = await listen(app);
+}
+
+function listen(app: core.Express): Promise<any> {
+  return new Promise((resolve, reject) => {
+    const server = app.listen(0, () => {
+      (server.address() as any).port
+      resolve(server);
+    })
+  })
+}
+
+
+
+beforeAll(async () => {
+  await testServer();
+});
+
+afterAll(() => {
+  if (testServer) {
+    testServer.close();
+  }
+})
+
+test("Test Jitsu Client npm only", async () => {
+  let jitsu: JitsuClient = jitsuClient({
+    fetch: jest.fn((...args) => {
+      console.log("Called fetch with", args);
+      return Promise.resolve();
+    }),
+    key: "Test",
+    tracking_host: "https://some-host",
+  });
+  const req: Request = {};
+  const res: Response = {};
+  await jitsu.id(
+    {
+      email: "a@b.c",
+      id: "someId",
+    },
+    true
+  );
+
+  await jitsu.track("test", {
+    req,
+    res,
   });
 });
