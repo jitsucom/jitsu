33f75343a4e245c8d66652814216c31e38d85633
Ildar Nurislamov <absorbb@gmail.com>
2022-01-19 13:39:18 +0300

    Explicitly disable task executor if server.sync_tasks.pool.size set to 0 Use absolute path for server.static_files_dir in containerized builds Server gin: write panic logs to GlobalLogger and system error notifications avoid panic in GetClientOrigins

--- server/routers/router.go
+++ server/routers/router.go
@@ -2,11 +2,13 @@ package routers
 
 import (
 	"github.com/jitsucom/jitsu/server/geo"
+	"github.com/jitsucom/jitsu/server/logging"
 	"github.com/jitsucom/jitsu/server/multiplexing"
 	"github.com/jitsucom/jitsu/server/plugins"
 	"github.com/jitsucom/jitsu/server/wal"
 	"net/http"
 	"net/http/pprof"
+	"runtime/debug"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jitsucom/jitsu/server/appconfig"
@@ -33,7 +35,10 @@ func SetupRouter(adminToken string, metaStorage meta.Storage, destinations *dest
 	gin.SetMode(gin.ReleaseMode)
 
 	router := gin.New() //gin.Default()
-	router.Use(gin.Recovery())
+	router.Use(gin.RecoveryWithWriter(logging.GlobalLogsWriter, func(c *gin.Context, err interface{}) {
+		logging.SystemErrorf("Panic:\n%s\n%s", err, string(debug.Stack()))
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}))
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
