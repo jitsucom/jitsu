6163f20e927aa001d42ec869553cd54c64887fa8
Ildar Nurislamov <absorbb@gmail.com>
2022-01-21 18:50:15 +0300

    Fixes

--- configurator/backend/entrypoint.sh
+++ configurator/backend/entrypoint.sh
@@ -3,4 +3,4 @@ echo "Starting Jitsu Configurator $@"
 
 sudo chown -R $CONFIGURATOR_USER:$CONFIGURATOR_USER /home/$CONFIGURATOR_USER
 
-~/app/configurator -cfg=~/data/config/configurator.yaml -cr=true -dhid="$DOCKER_HUB_ID"
+~/app/configurator -cfg=/home/$CONFIGURATOR_USER/data/config/configurator.yaml -cr=true -dhid="$DOCKER_HUB_ID"
