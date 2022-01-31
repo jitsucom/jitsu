31f7e762a035c5c446b5c2a3d4253d69db12a708
Sergey Burykin <sburykin@jitsu.com>
2022-01-31 14:44:27 +0300

    added openapi configurator endpoints

--- configurator/backend/middleware/access.go
+++ configurator/backend/middleware/access.go
@@ -10,3 +10,8 @@ func (pa *ProjectAccess) HasAccess(projectID string) bool {
 	_, ok := pa.projects[projectID]
 	return ok || pa.adminAccess
 }
+
+//IsAdmin returns true if provided token has admin access
+func (pa *ProjectAccess) IsAdmin() bool {
+	return pa.adminAccess
+}
