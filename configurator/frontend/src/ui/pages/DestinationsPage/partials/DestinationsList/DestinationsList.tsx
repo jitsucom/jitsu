6426c48fa05e0e25447a938792029078abc1a1ee
taletski <kirill@taletski.com>
2022-02-04 14:48:39 +0300

    adds quota check in routing

--- configurator/frontend/src/ui/pages/DestinationsPage/partials/DestinationsList/DestinationsList.tsx
+++ configurator/frontend/src/ui/pages/DestinationsPage/partials/DestinationsList/DestinationsList.tsx
@@ -1,29 +1,21 @@
 // @Libs
-import { useCallback, useEffect, useMemo, useState } from "react"
-import { generatePath, useHistory } from "react-router-dom"
-import { Button, Dropdown, Modal } from "antd"
+import { useCallback, useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import { Button } from "antd"
 import { observer } from "mobx-react-lite"
-// @Services
-import { destinationsReferenceList, destinationsReferenceMap } from "catalog/destinations/lib"
 // @Store
 import { destinationsStore } from "stores/destinations"
 // @Components
-import { handleError } from "lib/components/components"
-import { DropDownList } from "ui/components/DropDownList/DropDownList"
 import { EmptyList } from "ui/components/EmptyList/EmptyList"
 // @Icons
 import PlusOutlined from "@ant-design/icons/lib/icons/PlusOutlined"
-// @Styles
-import styles from "./DestinationsList.module.less"
 // @Utils
 import { withHome } from "ui/components/Breadcrumbs/Breadcrumbs"
 // @Routes
 import { destinationPageRoutes } from "ui/pages/DestinationsPage/DestinationsPage.routes"
 // @Types
 import { CommonDestinationPageProps } from "ui/pages/DestinationsPage/DestinationsPage"
-import { Destination } from "catalog/destinations/types"
 import { useServices } from "../../../../../hooks/useServices"
-import { showQuotaLimitModal } from "../../../../../lib/services/billing"
 import { DestinationCard } from "../../../../components/DestinationCard/DestinationCard"
 
 const DestinationsListComponent = ({ setBreadcrumbs }: CommonDestinationPageProps) => {
@@ -31,50 +23,8 @@ const DestinationsListComponent = ({ setBreadcrumbs }: CommonDestinationPageProp
   const subscription = useServices().currentSubscription
 
   const handleAddClick = useCallback(() => {
-    if (
-      destinationsStore.allDestinations.length >= subscription.currentPlan.quota.destinations &&
-      !subscription.doNotBlock
-    ) {
-      showQuotaLimitModal(
-        subscription,
-        <>You current plan allows to have only {subscription.currentPlan.quota.destinations} destinations</>
-      )
-      return
-    }
     history.push(destinationPageRoutes.add)
   }, [history, subscription])
-
-  const dropDownList = useMemo(
-    () => (
-      <DropDownList
-        hideFilter
-        list={destinationsReferenceList
-          .filter(v => !v.hidden && !v.deprecated)
-          .map((dst: Destination) => ({
-            title: dst.displayName,
-            id: dst.id,
-            icon: dst.ui.icon,
-            handleClick: () => {
-              if (
-                destinationsStore.allDestinations.length >= subscription.currentPlan.quota.destinations &&
-                !subscription.doNotBlock
-              ) {
-                showQuotaLimitModal(
-                  subscription,
-                  <>You current plan allows to have only {subscription.currentPlan.quota.destinations} destinations</>
-                )
-                return
-              }
-              const link = generatePath(destinationPageRoutes.newExact, {
-                type: dst.id,
-              })
-              history.push(link)
-            },
-          }))}
-      />
-    ),
-    []
-  )
 
   useEffect(() => {
     setBreadcrumbs(
@@ -103,7 +53,6 @@ const DestinationsListComponent = ({ setBreadcrumbs }: CommonDestinationPageProp
 
       <div className="flex flex-wrap justify-center">
         {destinationsStore.destinations.map((dst: DestinationData) => {
-          const statLink = generatePath(destinationPageRoutes.statisticsExact, { id: dst._id })
           return <DestinationCard key={dst._uid} dst={dst} />
         })}
       </div>
