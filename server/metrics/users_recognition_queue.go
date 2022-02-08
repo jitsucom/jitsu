09441e91c8a339d449ae978fbfb29be5f0848a82
Ildar Nurislamov <absorbb@gmail.com>
2022-02-08 19:11:17 +0300

    User recognition: Separate prometheus metrics for anonymous and identified events User recognition: Set default value for users_recognition.pool.size = 10

--- server/metrics/users_recognition_queue.go
+++ server/metrics/users_recognition_queue.go
@@ -5,31 +5,45 @@ import (
 )
 
 var (
-	usersRecognitionQueueSize *prometheus.GaugeVec
+	usersRecognitionQueueSize = map[string]*prometheus.GaugeVec{}
 )
 
 func initUsersRecognitionQueue() {
-	usersRecognitionQueueSize = NewGaugeVec(prometheus.GaugeOpts{
+	usersRecognitionQueueSize["users_recognition"] = NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: "eventnative",
 		Subsystem: "users_recognition",
 		Name:      "queue_size",
 	}, []string{})
+	usersRecognitionQueueSize["users_identified"] = NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "eventnative",
+		Subsystem: "users_identified",
+		Name:      "queue_size",
+	}, []string{})
 }
 
-func InitialUsersRecognitionQueueSize(value int) {
+func InitialUsersRecognitionQueueSize(identifier string, value int) {
 	if Enabled() {
-		usersRecognitionQueueSize.WithLabelValues().Set(float64(value))
+		gaugeVec, ok := usersRecognitionQueueSize[identifier]
+		if ok {
+			gaugeVec.WithLabelValues().Set(float64(value))
+		}
 	}
 }
 
-func DequeuedRecognitionEvent() {
+func DequeuedRecognitionEvent(identifier string) {
 	if Enabled() {
-		usersRecognitionQueueSize.WithLabelValues().Sub(1)
+		gaugeVec, ok := usersRecognitionQueueSize[identifier]
+		if ok {
+			gaugeVec.WithLabelValues().Sub(1)
+		}
 	}
 }
 
-func EnqueuedRecognitionEvent() {
+func EnqueuedRecognitionEvent(identifier string) {
 	if Enabled() {
-		usersRecognitionQueueSize.WithLabelValues().Add(1)
+		gaugeVec, ok := usersRecognitionQueueSize[identifier]
+		if ok {
+			gaugeVec.WithLabelValues().Add(1)
+		}
 	}
 }
