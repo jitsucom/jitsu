aad5185ef237fb0f096ddedf588656a3cc7e6d52
Igor Kulkov <igor@jitsu.com>
2022-02-11 08:31:23 +0300

    fix

--- configurator/backend/storages/service.go
+++ configurator/backend/storages/service.go
@@ -6,8 +6,6 @@ import (
 	"io"
 	"time"
 
-	"github.com/jitsucom/jitsu/configurator/openapi"
-
 	"github.com/hashicorp/go-multierror"
 	"github.com/jitsucom/jitsu/configurator/destinations"
 	"github.com/jitsucom/jitsu/configurator/entities"
