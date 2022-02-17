de29b5f5160c9b0c0e7ab53e36cf3865e54cbf5c
Igor Kulkov <igor@jitsu.com>
2022-02-17 14:26:38 +0300

    use StringSet

--- configurator/backend/common/string_set.go
+++ configurator/backend/common/string_set.go
@@ -1,11 +1,14 @@
 package common
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"strings"
+)
 
 type StringSet map[string]bool
 
 func (s *StringSet) UnmarshalJSON(data []byte) error {
-	values := make([]string, 0)
+	values := make([]string, 0, strings.Count(string(data), ",")+1)
 	if err := json.Unmarshal(data, &values); err != nil {
 		return err
 	}
@@ -14,13 +17,18 @@ func (s *StringSet) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-func (s StringSet) MarshalJSON() ([]byte, error) {
-	values := make([]string, 0, len(s))
-	for value := range s {
-		values = append(values, value)
-	}
+func (s StringSet) Add(value string) {
+	s[value] = true
+}
 
-	return json.Marshal(values)
+func (s StringSet) AddAll(values ...string) {
+	for _, value := range values {
+		s.Add(value)
+	}
+}
+
+func (s StringSet) MarshalJSON() ([]byte, error) {
+	return json.Marshal(s.Values())
 }
 
 func (s StringSet) Values() []string {
@@ -34,9 +42,6 @@ func (s StringSet) Values() []string {
 
 func StringSetFrom(values []string) StringSet {
 	set := make(StringSet, len(values))
-	for _, value := range values {
-		set[value] = true
-	}
-
+	set.AddAll(values...)
 	return set
 }
