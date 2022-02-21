7a8cea98067044fcc7b13b255f97400fb6fba48a
taletski <kirill@taletski.com>
2022-02-21 12:20:57 +0300

    removes redundant code

--- configurator/frontend/main/src/stores/helpers.ts
+++ configurator/frontend/main/src/stores/helpers.ts
@@ -13,37 +13,6 @@ class ConnectionsHelper {
     this.destinationsStore = stores.destinations
   }
 
-  private async updateEntitiesConnections<T extends EntityData>(
-    entityId: string,
-    connectedEntitiesIds: string[],
-    connectedEntitiesSchema: { store: EntitiesStore<T>; idField: string; connectedEntitiesIdsField: string }
-  ): Promise<void> {
-    const patches: { [id: string]: Partial<T> } = {}
-    const { connectedEntitiesIdsField } = connectedEntitiesSchema
-    connectedEntitiesSchema.store.list.forEach(entity => {
-      const entityShouldBeConnected = connectedEntitiesIds.includes(entity[connectedEntitiesSchema.idField])
-      const entityIsConnected = entity[connectedEntitiesIdsField].includes(entityId)
-
-      if (entityIsConnected === entityShouldBeConnected) return
-
-      if (entityShouldBeConnected) {
-        patches[entity[connectedEntitiesSchema.idField]] = {
-          [connectedEntitiesIdsField]: [...entity[connectedEntitiesIdsField], entityId],
-        } as any
-      } else {
-        patches[entity[connectedEntitiesSchema.idField]] = {
-          [connectedEntitiesIdsField]: without(entity[connectedEntitiesIdsField], entityId),
-        } as any
-      }
-    })
-
-    await Promise.all(
-      Object.entries(patches).map(([entityId, patch]) => {
-        return flowResult(connectedEntitiesSchema.store.patch(entityId, patch))
-      })
-    )
-  }
-
   public async updateSourcesConnectionsToDestination(destinationId: string, connectedSourcesIds: string[]) {
     await this.updateEntitiesConnections(destinationId, connectedSourcesIds, {
       store: this.sourcesStore,
@@ -80,72 +49,36 @@ class ConnectionsHelper {
     this.updateSourcesConnectionsToDestination(destinationId, [])
   }
 
-  /**
-   *
-   * Below is an older boilerplate implementation. It is here just for the future reference.
-   * Feel free to remove this code if you find it otside the feature/785-migrate-ui-to-objects-api branch
-   *
-   */
+  private async updateEntitiesConnections<T extends EntityData>(
+    entityId: string,
+    connectedEntitiesIds: string[],
+    connectedEntitiesSchema: { store: EntitiesStore<T>; idField: string; connectedEntitiesIdsField: string }
+  ): Promise<void> {
+    const patches: { [id: string]: Partial<T> } = {}
+    const { connectedEntitiesIdsField } = connectedEntitiesSchema
+    connectedEntitiesSchema.store.list.forEach(entity => {
+      const entityShouldBeConnected = connectedEntitiesIds.includes(entity[connectedEntitiesSchema.idField])
+      const entityIsConnected = entity[connectedEntitiesIdsField].includes(entityId)
 
-  // public async updateSourcesConnectionsToDestination(destinationId: string, connectedSourcesIds: string[]) {
-  //   const sourcesPatches: { [sourceId: string]: Partial<SourceData> } = {}
-  //   this.sourcesStore.listIncludeHidden.forEach(source => {
-  //     const sourceShouldBeConnected = connectedSourcesIds.includes(source.sourceId)
-  //     const sourceIsConnected = source.destinations.includes(destinationId)
+      if (entityIsConnected === entityShouldBeConnected) return
 
-  //     if (sourceIsConnected === sourceShouldBeConnected) return
+      if (entityShouldBeConnected) {
+        patches[entity[connectedEntitiesSchema.idField]] = {
+          [connectedEntitiesIdsField]: [...entity[connectedEntitiesIdsField], entityId],
+        } as any
+      } else {
+        patches[entity[connectedEntitiesSchema.idField]] = {
+          [connectedEntitiesIdsField]: without(entity[connectedEntitiesIdsField], entityId),
+        } as any
+      }
+    })
 
-  //     if (sourceShouldBeConnected) {
-  //       sourcesPatches[source.sourceId] = {
-  //         destinations: [...source.destinations, destinationId],
-  //       }
-  //     } else {
-  //       sourcesPatches[source.sourceId] = {
-  //         destinations: without(source.destinations, destinationId),
-  //       }
-  //     }
-  //   })
-  // }
-
-  // public async updateDestinationsConnectionsToSource(sourceId: string, connectedDestinationsIds: string[]) {
-  //   const destinationsPatches: { [sourceId: string]: Partial<DestinationData> } = {}
-  //   this.destinationsStore.listIncludeHidden.forEach(destination => {
-  //     const destinationShouldBeConnected = connectedDestinationsIds.includes(destination._uid)
-  //     const destinationIsConnected = destination._sources.includes(sourceId)
-
-  //     if (destinationIsConnected === destinationShouldBeConnected) return
-
-  //     if (destinationShouldBeConnected) {
-  //       destinationsPatches[destination._uid] = {
-  //         _sources: [...destination._sources, sourceId],
-  //       }
-  //     } else {
-  //       destinationsPatches[destination._uid] = {
-  //         _sources: without(destination._sources, sourceId),
-  //       }
-  //     }
-  //   })
-  // }
-
-  // public async updateDestinationsConnectionsToApiKey(apiKeyId: string, connectedDestinationsIds: string[]) {
-  //   const destinationsPatches: { [apiKeyId: string]: Partial<DestinationData> } = {}
-  //   this.destinationsStore.listIncludeHidden.forEach(destination => {
-  //     const destinationShouldBeConnected = connectedDestinationsIds.includes(destination._uid)
-  //     const destinationIsConnected = destination._onlyKeys.includes(apiKeyId)
-
-  //     if (destinationIsConnected === destinationShouldBeConnected) return
-
-  //     if (destinationShouldBeConnected) {
-  //       destinationsPatches[destination._uid] = {
-  //         _onlyKeys: [...destination._onlyKeys, apiKeyId],
-  //       }
-  //     } else {
-  //       destinationsPatches[destination._uid] = {
-  //         _onlyKeys: without(destination._onlyKeys, apiKeyId),
-  //       }
-  //     }
-  //   })
-  // }
+    await Promise.all(
+      Object.entries(patches).map(([entityId, patch]) => {
+        return flowResult(connectedEntitiesSchema.store.patch(entityId, patch))
+      })
+    )
+  }
 }
 
 export const connectionsHelper = new ConnectionsHelper({ sources: sourcesStore, destinations: destinationsStore })
