95afe6cb2c89a5ab4b7b0ec80046d9812d8735e8
Art Sk <artem@jitsu.com>
2022-03-01 15:50:19 +0300

    #812 - documentation and fixes for sso auth feature (#844)
    * added sso auth feature documentation
    fixed bug with redirect after authorization
    
    * prettier fix
    
    * moved config param sso.configurator_root_path to ui.base_url
    added default values for ui.base_url
    updated sso doc
    
    * doc fix
    
    * viper ui base url key name fix

--- configurator/backend/handlers/sso_auth.go
+++ configurator/backend/handlers/sso_auth.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jitsucom/jitsu/configurator/authorization"
+	"github.com/spf13/viper"
 	"net/http"
 )
 
 const (
 	errorTmpl = `<script>
 	  window.localStorage.setItem("sso_error", "SSO Auth error! %v")
-	  window.location.href = "/"
+	  window.location.href = "%s"
 	</script>`
 	successTmpl = `<script>
 	  window.localStorage.setItem("en_access", "%s")
 	  window.localStorage.setItem("en_refresh", "%s")
-	  window.location.href = "/"
+	  window.location.href = "%s"
 	</script>`
 )
 
@@ -39,13 +40,15 @@ func (oh *SSOAuthHandler) Handler(c *gin.Context) {
 		return
 	}
 
+	uiBaseUrl := viper.GetString("ui.base_url")
+
 	td, err := oh.authService.SSOAuthenticate(code)
 	if err != nil {
-		c.String(http.StatusOK, errorTmpl, err)
+		c.String(http.StatusOK, errorTmpl, err, uiBaseUrl)
 		return
 	}
 
-	c.String(http.StatusOK, successTmpl, td.AccessTokenEntity.AccessToken, td.RefreshTokenEntity.RefreshToken)
+	c.String(http.StatusOK, successTmpl, td.AccessTokenEntity.AccessToken, td.RefreshTokenEntity.RefreshToken, uiBaseUrl)
 
 	return
 }
