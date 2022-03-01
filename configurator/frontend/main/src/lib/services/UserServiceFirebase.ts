9f1573031ca24bd99de08282e97203dd23ae310c
Art Sk <kirsan007@gmail.com>
2022-03-01 13:43:22 +0300

    prettier fix

--- configurator/frontend/main/src/lib/services/UserServiceFirebase.ts
+++ configurator/frontend/main/src/lib/services/UserServiceFirebase.ts
@@ -293,7 +293,7 @@ export class FirebaseUserService implements UserService {
   }
 
   getSSOAuthLink(): string {
-    return ''
+    return ""
   }
 
   sendLoginLink(email: string): Promise<void> {
