a98d9315b7ac8a98e4773c0615486baf2bf4d332
Art Sk <kirsan007@gmail.com>
2022-03-10 10:52:37 +0300

    new live events page

---
+++ configurator/frontend/main/src/lib/components/EventsStream/shared.ts
@@ -0,0 +1,29 @@
+import { Moment } from "moment"
+
+export enum EventType {
+  Destination = "destination",
+  Token = "token",
+}
+
+export enum EventStatus {
+  Success = "success",
+  Error = "error",
+  Pending = "pending",
+  Skip = "skip",
+}
+
+export type Event = {
+  type: EventType
+  timestamp: Moment
+  eventId: string
+  rawJson: any
+  id: string
+  status: EventStatus
+  resultJson: any
+}
+
+export type FilterOption = {
+  value: string
+  label: any
+  icon?: any
+}
