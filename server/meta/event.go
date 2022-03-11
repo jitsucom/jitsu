e73d420a4da8ac3bde870130a7f72cd59f1312ca
Art Sk <kirsan007@gmail.com>
2022-03-11 10:25:13 +0300

    added timestamp and uuid for events

--- server/meta/event.go
+++ server/meta/event.go
@@ -6,6 +6,8 @@ type Event struct {
 	Success       string `json:"success,omitempty" redis:"success"`
 	Error         string `json:"error,omitempty" redis:"error"`
 	Skip          string `json:"skip,omitempty" redis:"skip"`
+	Timestamp     string `json:"timestamp,omitempty" redis:"timestamp"`
+	UID           string `json:"uid,omitempty" redis:"uid"`
 	DestinationID string `json:"destination_id,omitempty" redis:"destination_id"`
 	TokenID       string `json:"token_id,omitempty" redis:"token_id"`
 }
