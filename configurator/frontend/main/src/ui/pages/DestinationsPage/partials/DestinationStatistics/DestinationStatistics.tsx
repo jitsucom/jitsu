ab5a1deeff9e580f81e62841be6f6aba104f5451
Art Sk <kirsan007@gmail.com>
2022-03-25 16:40:01 +0300

    destination statistics page fix

--- configurator/frontend/main/src/ui/pages/DestinationsPage/partials/DestinationStatistics/DestinationStatistics.tsx
+++ configurator/frontend/main/src/ui/pages/DestinationsPage/partials/DestinationStatistics/DestinationStatistics.tsx
@@ -70,7 +70,7 @@ export const DestinationStatistics: React.FC<CommonDestinationPageProps> = () =>
   const params = useParams<StatisticsPageParams>()
   const destination = destinationsStore.list.find(d => d._id === params.id)
   const destinationUid = destination?._uid
-  const destinationReference = destinationsStore.getDestinationReferenceById(params.id)
+  const destinationReference = destinationsStore.getDestinationReferenceById(destinationUid)
   const statisticsService = useMemo<IStatisticsService>(
     () => new StatisticsService(services.backendApiClient, services.activeProject.id, true),
     []
