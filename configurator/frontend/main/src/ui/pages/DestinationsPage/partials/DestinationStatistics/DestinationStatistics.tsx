285e8cd910dbac2a1110076ab2114a173fcff50a
Kirill Taletski <kirill@taletski.com>
2022-03-25 15:46:40 +0300

    Hotfixes UI (#870)
    * fixed occasional `[MobX] Dynamic observable objects cannot be frozen` error
    
    * fixes some nav links on connections and source editor pages
    
    * fixed prominent inconsistency of ids of connected entities
    
    On adding a new entity, the UI generates a temporary UID for it.
    The API then returns the added entity with a (possibly) different UID.
    If the entity to be connected to other entities, the UI in some cases
    used UI-generated UIDs instead of API-generated ones. So far, those,
    UIDs were the same but this might change in the future leading to storing
    non-existent (ui-generated) UIDs to the arrays of connected entities.
    
    The fix changed the behaviour of UI so that it always uses API-generated
    UIDs.
    
    * adds healConnections() that helps keep connections consistent
    
    healConnections() finds and unconnects non-existent entities that
    may exist due to connections management errors in UI
    
    * enables UI to remember the last used project
    
    UI restores the last successfully initialized project id from either
    the session storage or the local storage - this makes it more stable
    in cross-tabs user workflows.
    
    * adds project re-initialization on project id change in URL
    
    * fixes the DestinationStatistics page
    
    * makes healConnections() a bit more safe

--- configurator/frontend/main/src/ui/pages/DestinationsPage/partials/DestinationStatistics/DestinationStatistics.tsx
+++ configurator/frontend/main/src/ui/pages/DestinationsPage/partials/DestinationStatistics/DestinationStatistics.tsx
@@ -68,7 +68,7 @@ export const DestinationStatistics: React.FC<CommonDestinationPageProps> = () =>
   const history = useHistory()
   const services = useServices()
   const params = useParams<StatisticsPageParams>()
-  const destination = destinationsStore.get(params.id)
+  const destination = destinationsStore.list.find(d => d._id === params.id)
   const destinationUid = destination?._uid
   const destinationReference = destinationsStore.getDestinationReferenceById(params.id)
   const statisticsService = useMemo<IStatisticsService>(
