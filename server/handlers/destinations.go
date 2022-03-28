6e513cb111aabf8f4d0757dcafca54a3ae2c34f4
Ildar Nurislamov <absorbb@gmail.com>
2022-03-28 12:24:18 +0300

    #875 Fixed: Test Connection not working properly for Snowflake destination in batch mode with Google Cloud Storage

--- server/handlers/destinations.go
+++ server/handlers/destinations.go
@@ -569,10 +569,7 @@ func testSnowflake(config *config.DestinationConfig, eventContext *adapters.Even
 	if err != nil {
 		return err
 	}
-	s3config, ok := s3c.(*adapters.S3Config)
-	if !ok {
-		s3config = &adapters.S3Config{}
-	}
+	s3config, _ = s3c.(*adapters.S3Config)
 	var googleConfig *adapters.GoogleConfig
 	gc, err := config.GetConfig(snowflakeConfig.Google, config.Google, &adapters.GoogleConfig{})
 	if err != nil {
