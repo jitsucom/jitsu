5534ce16144a9780b5428833ab94d97284f7a2b4
taletski <kirill@taletski.com>
2022-04-02 14:31:09 +0300

    refactors StatusPage to include extra controls

--- configurator/frontend/main/src/lib/components/ApiKeys/ApiKeyStatistics.tsx
+++ configurator/frontend/main/src/lib/components/ApiKeys/ApiKeyStatistics.tsx
@@ -6,6 +6,7 @@ import { apiKeysStore } from "stores/apiKeys"
 import { StatusPage } from "lib/components/StatusPage/StatusPage"
 import { EntityNotFound } from "ui/components/EntityNotFound/EntityNotFound"
 import { apiKeysRoutes } from "./ApiKeysRouter"
+import { projectRoute } from "../ProjectLink/ProjectLink"
 
 type StatisticsPageParams = {
   id: string
@@ -16,7 +17,13 @@ export const ApiKeyStatistics: React.FC = () => {
   const apiKey = apiKeysStore.list.find(k => k.uid === params.id)
 
   return apiKey ? (
-    <StatusPage apiKeyId={apiKey.uid} />
+    // <StatusPage apiKeyId={apiKey.uid} />
+    <StatusPage
+      entityId={apiKey.uid}
+      entityType="api_key"
+      entitiesListRoute={projectRoute(apiKeysRoutes.listExact)}
+      editEntityRoute={projectRoute(apiKeysRoutes.editExact, { id: apiKey.uid })}
+    />
   ) : (
     <EntityNotFound entityDisplayType="API Key" entityId={params.id} entitiesListRoute={apiKeysRoutes.listExact} />
   )
