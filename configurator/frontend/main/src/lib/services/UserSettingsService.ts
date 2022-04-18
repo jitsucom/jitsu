5171d951e3805dbd6816831a775580367cbf2b83
Art Sk <kirsan007@gmail.com>
2022-04-18 12:57:16 +0300

    active project now saves to local storage

---
+++ configurator/frontend/main/src/lib/services/UserSettingsService.ts
@@ -0,0 +1,50 @@
+import { UserService } from "./UserService";
+import { merge } from "lodash";
+
+export interface UserSettingsService {
+  set(data: {[key: string]: any}): void
+
+  get(settingName?: string): any
+}
+
+export class UserSettingsLocalService implements UserSettingsService {
+  private _keyNamePrefix = "userSettings"
+  private _userService: UserService
+
+  constructor(userService: UserService) {
+    this._userService = userService
+  }
+
+  private get keyName() {
+    return `${this._keyNamePrefix}_${this._userService?.getUser()?.id}`
+  }
+
+  set(data: {[key: string]: any}): void {
+    let settings = this.get()
+
+    if (settings !== null) {
+      settings = merge(settings, data)
+    }
+
+    localStorage.setItem(this.keyName, JSON.stringify(settings))
+  }
+
+  get(settingName?: string) {
+    let json: string = localStorage.getItem(this.keyName)
+    let data: any;
+
+    if (json !== null) {
+      try {
+        data = JSON.parse(json)
+      } catch {
+        return null;
+      }
+    }
+
+    if (settingName) {
+      data = data[settingName] ?? null;
+    }
+
+    return data;
+  }
+}
