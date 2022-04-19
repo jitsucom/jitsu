95773764c46fe46d23b243df67e26bc6272da969
Igor Kulkov <igor@jitsu.com>
2022-04-19 14:32:13 +0300

    remove directories on close

--- server/script/node/script.go
+++ server/script/node/script.go
@@ -7,6 +7,7 @@ import (
 	"encoding/json"
 	"errors"
 	"io"
+	"os"
 	"time"
 
 	"github.com/jitsucom/jitsu/server/logging"
@@ -75,7 +76,7 @@ func (s *Script) Close() {
 		logging.Warnf("wait process failed: %v", err)
 	}
 
-	//_ = os.RemoveAll(s.Dir)
+	_ = os.RemoveAll(s.Dir)
 }
 
 func (s *Script) exchange(command string, payload, result interface{}) error {
