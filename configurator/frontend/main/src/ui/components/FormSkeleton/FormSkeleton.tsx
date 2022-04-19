0a0143ac4a9b29c34a61efbe9e7a8889bb74eb8b
Kirill Taletski <kirill@taletski.com>
2022-04-19 12:34:38 +0300

    Fix/not found routing (#898)
    * adds PageNotFound handler to api keys, sources and destinations routers
    
    * fixes the form loader skeleton width

--- configurator/frontend/main/src/ui/components/FormSkeleton/FormSkeleton.tsx
+++ configurator/frontend/main/src/ui/components/FormSkeleton/FormSkeleton.tsx
@@ -6,7 +6,7 @@ export const FormSkeleton: React.FC<Props> = () => {
   return (
     <Row className="w-full h-full">
       <Col span={4} />
-      <Col span={20}>
+      <Col span={20} className={"max-w-3xl"}>
         <Skeleton active paragraph={{ rows: 2 }} />
         <Skeleton active title={false} paragraph={{ rows: 3 }} className={`mt-5`} />
       </Col>
