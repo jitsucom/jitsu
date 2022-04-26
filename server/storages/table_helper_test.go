ff6fb0bdee81e3be4be02ffede7b6ce8276434d7
Igor Kulkov <igor@jitsu.com>
2022-04-26 13:52:36 +0300

    fix tests

--- server/storages/table_helper_test.go
+++ server/storages/table_helper_test.go
@@ -67,7 +67,7 @@ func TestMapTableSchema(t *testing.T) {
 func TestProcessTransformWithTypesOverride(t *testing.T) {
 	viper.Set("server.log.path", "")
 	viper.Set("sql_debug_log.ddl.enabled", false)
-	nodeFactory, err := node.NewFactory()
+	nodeFactory, err := node.NewFactory(1)
 	if err != nil {
 		t.Fatal(err)
 	}
