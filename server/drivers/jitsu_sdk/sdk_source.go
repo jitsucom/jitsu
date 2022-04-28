a2c05013f8af6b80bbfb70d30c09951858087390
Ildar Nurislamov <absorbb@gmail.com>
2022-04-28 19:32:15 +0300

    it is premature to replace redis with sdk implementation

--- server/drivers/jitsu_sdk/sdk_source.go
+++ server/drivers/jitsu_sdk/sdk_source.go
@@ -38,14 +38,14 @@ type SdkSource struct {
 func init() {
 	base.RegisterDriver(base.SdkSourceType, NewSdkSource)
 	base.RegisterTestConnectionFunc(base.SdkSourceType, TestSdkSource)
-	base.RegisterDriver(base.RedisType, func(ctx context.Context, sourceConfig *base.SourceConfig, collection *base.Collection) (base.Driver, error) {
-		sourceConfig.Config["package_name"] = "jitsu-redis-source"
-		return NewSdkSource(ctx, sourceConfig, collection)
-	})
-	base.RegisterTestConnectionFunc(base.RedisType, func(sourceConfig *base.SourceConfig) error {
-		sourceConfig.Config["package_name"] = "jitsu-redis-source"
-		return TestSdkSource(sourceConfig)
-	})
+	//base.RegisterDriver(base.RedisType, func(ctx context.Context, sourceConfig *base.SourceConfig, collection *base.Collection) (base.Driver, error) {
+	//	sourceConfig.Config["package_name"] = "jitsu-redis-source"
+	//	return NewSdkSource(ctx, sourceConfig, collection)
+	//})
+	//base.RegisterTestConnectionFunc(base.RedisType, func(sourceConfig *base.SourceConfig) error {
+	//	sourceConfig.Config["package_name"] = "jitsu-redis-source"
+	//	return TestSdkSource(sourceConfig)
+	//})
 }
 
 //NewSdkSource returns SdkSource driver and
