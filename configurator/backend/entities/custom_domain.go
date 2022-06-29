91fcdc41e08a1fa312a04e8e868e14208b149c29
Ildar Nurislamov <absorbb@gmail.com>
2022-06-29 17:20:35 +0400

    Store certificates data in redis in custom_domains entity

--- configurator/backend/entities/custom_domain.go
+++ configurator/backend/entities/custom_domain.go
@@ -7,5 +7,7 @@ type CustomDomain struct {
 
 type CustomDomains struct {
 	CertificateExpirationDate string          `firestore:"_certificateExpiration" json:"_certificateExpiration"`
+	Certificate               string          `firestore:"certificate" json:"certificate"`
+	PrivateKey                string          `firestore:"privateKey" json:"privateKey"`
 	Domains                   []*CustomDomain `firestore:"domains" json:"domains"`
 }
