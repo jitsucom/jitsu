7d0e9a4d506baa554d1d046a904ad5e3a12e6bc8
Ildar Nurislamov <absorbb@gmail.com>
2023-06-07 15:47:59 +0400

    added vercel.sh script to ee-api

---
+++ webapps/ee-api/vercel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script defines if vercel should deploy the commit,
+# see https://vercel.com/support/articles/how-do-i-use-the-ignored-build-step-field-on-vercel
+
+if [[ "$VERCEL_GIT_COMMIT_REF" != *"newjitsu"*  ]]; then
+    echo "❌ Not a newjitsu branch, skipping deploy"
+    exit 0
+fi
+
+npx turbo-ignore
