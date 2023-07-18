4a18c9ce2019f85704e45cb10dbdd982b012dd8f
Vladimir Klimontovich <vladimir@jitsu.com>
2023-07-18 16:12:26 -0400

    feat: disable iframe embedding

--- webapps/console/middleware.ts
+++ webapps/console/middleware.ts
@@ -2,11 +2,16 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+  let response;
   if (request.nextUrl.pathname.startsWith("/p.js")) {
-    return NextResponse.rewrite(new URL("/api/s/javascript-library", request.url));
+    response = NextResponse.rewrite(new URL("/api/s/javascript-library", request.url));
+  } else {
+    response = NextResponse.next();
   }
+  response.headers.set("X-Frame-Options", "DENY");
+  return response;
 }
 
 export const config = {
-  matcher: "/p.js",
+  matcher: "",
 };
