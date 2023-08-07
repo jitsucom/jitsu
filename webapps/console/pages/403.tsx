cac4242f08f01beea67aef71d0766ba18d77bd3f
Ildar Nurislamov <absorbb@gmail.com>
2023-08-07 15:23:33 +0400

    simple error pages for 403,404,500

---
+++ webapps/console/pages/403.tsx
@@ -0,0 +1,3 @@
+export default function Custom403() {
+  return <h1>403 - Not authorized</h1>;
+}
---
+++ webapps/console/pages/500.tsx
@@ -0,0 +1,3 @@
+export default function Custom500() {
+  return <h1>500 - Internal server error</h1>;
+}
