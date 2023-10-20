1876574fa2d81657d0f584ed5db919982bd74235
Ildar Nurislamov <absorbb@gmail.com>
2023-10-20 17:28:53 +0400

    small cleanups

--- libs/core-functions/src/functions/udf_wrapper.ts
+++ libs/core-functions/src/functions/udf_wrapper.ts
@@ -1,7 +1,6 @@
 import { getLog } from "juava";
 import { Isolate, ExternalCopy, Callback, Reference } from "isolated-vm";
 import { EventContext, EventsStore, FetchOpts, FuncReturn, JitsuFunction, Store } from "@jitsu/protocols/functions";
-import { DropRetryErrorName, RetryErrorName } from "@jitsu/functions-lib";
 import { AnalyticsServerEvent } from "@jitsu/protocols/analytics";
 import { createFullContext } from "../context";
 import { isDropResult } from "../index";
@@ -22,81 +21,6 @@ export type UDFWrapperResult = {
   meta: any;
   close: () => void;
 };
-
-const wrapperJs = `
-import * as udf from "udf";
-
-const userFunction = udf.default;
-const meta = udf.config || {};
-
-if (typeof RetryError === "undefined") {
-  global.RetryError = class extends Error {
-    constructor(message, options) {
-      super(message);
-      this.name = options?.drop ? "${DropRetryErrorName}" : "${RetryErrorName}";
-    }
-  }
-}
-
-const wrappedUserFunction = async function (eventcopy, ctxcopy) {
-  const c = ctxcopy.copy();
-  const ctx = {
-    ...c,
-    store: {
-      ...c.store,
-      get: async key => {
-        const res = await c.store.get.apply(undefined, [key], { arguments: { copy: true }, result: { promise: true } });
-        return res ? JSON.parse(res) : undefined;
-      },
-    },
-    fetch: async (url, opts) => {
-      const res = await c.fetch.apply(undefined, [url, opts], { arguments: { copy: true }, result: { promise: true } });
-      const r = JSON.parse(res);
-
-      return {
-        ...r,
-        json: async () => {
-          return JSON.parse(r.body);
-        },
-        text: async () => {
-          return r.body;
-        },
-        arrayBuffer: async () => {
-            throw new Error("Method 'arrayBuffer' is not implemented");
-        },
-        blob: async () => {
-            throw new Error("Method 'blob' is not implemented");
-        },
-        formData: async () => {
-            throw new Error("Method 'formData' is not implemented");
-        },
-        clone: async () => {
-            throw new Error("Method 'clone' is not implemented");
-        }
-      };
-    },
-  };
-  const event = eventcopy.copy();
-  if (!userFunction || typeof userFunction !== "function") {
-    throw new Error("Function not found. Please export default function.");
-  }
-  console = {
-    ...console,
-    log: ctx.log.info,
-    error: ctx.log.error,
-    warn: ctx.log.warn,
-    debug: ctx.log.debug,
-    info: ctx.log.info,
-    assert: (asrt, ...args) => {
-      if (!asrt) {
-        ctx.log.error("Assertion failed", ...args);
-      }
-    },
-  };
-  return userFunction(event, ctx);
-};
-export { meta, wrappedUserFunction };
-`;
 
 export const UDFWrapper = (functionId, name, functionCode: string): UDFWrapperResult => {
   log.atInfo().log(`[${functionId}] Compiling UDF function '${name}'`);
