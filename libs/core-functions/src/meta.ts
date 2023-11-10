c8f25ab9f99bb9d7f5fcaf4f191edeb186c4dc63
Vladimir Klimontovich <vladimir@jitsu.com>
2023-11-10 14:41:57 -0500

    fix: build fix

--- libs/core-functions/src/meta.ts
+++ libs/core-functions/src/meta.ts
@@ -8,7 +8,7 @@ export const FacebookConversionApiCredentials = z.object({
   pixelId: z.string().describe("Facebook Pixel ID"),
   accessToken: z.string().describe("Facebook Access Token"),
   events: z.string().optional().default("").describe(eventsParamDescription),
-  actionSource: z.string().optional().default(undefined).describe("Action Source"),
+  actionSource: z.string().optional().describe("Action Source"),
 });
 
 export const FacebookConversionApiCredentialsUi = {
