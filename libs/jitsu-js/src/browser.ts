bf8e902f1aec81d6d21e9b0f3678f63ddfde1c9d
Vladimir Klimontovich <vladimir@jitsu.com>
2023-11-13 19:59:07 -0500

    feat: support of jitsu event queue for browsers

--- libs/jitsu-js/src/browser.ts
+++ libs/jitsu-js/src/browser.ts
@@ -53,14 +53,35 @@ function getScriptAttributes(scriptElement: HTMLScriptElement) {
 
   const options = readJitsuOptions();
   const JITSU_V2_ID: string = options.namespace || "jitsu";
-
+  const queue = [];
   if (window[JITSU_V2_ID]) {
-    console.log("Jitsu is already initialized");
+    if (Array.isArray(window[JITSU_V2_ID])) {
+      //processing queue of events
+      if (options.debug) {
+        console.log(
+          `Initializing Jitsu with prior events queue size of ${window[JITSU_V2_ID].length}`,
+          window[JITSU_V2_ID]
+        );
+      }
+      queue.push(...window[JITSU_V2_ID]);
+    } else {
+      console.warn("Attempted to initialize Jitsu twice. Returning the existing instance");
+    }
   }
   if (options.debug) {
     console.log(`Jitsu options: `, JSON.stringify(options));
   }
   const jitsu = jitsuAnalytics(options);
+  for (const [method, args] of queue) {
+    if (options.debug) {
+      console.log(`Processing event ${method} from Jitsu queue on`, args);
+    }
+    try {
+      jitsu[method](...args);
+    } catch (e: any) {
+      console.warn(`Error processing event ${method} from Jitsu queue on`, args, e);
+    }
+  }
 
   if (options.onload) {
     const onloadFunction = window[options.onload] as any;
@@ -75,6 +96,18 @@ function getScriptAttributes(scriptElement: HTMLScriptElement) {
   }
   window[JITSU_V2_ID] = jitsu;
 
+  //new callback based queue
+  const callbackQueue = window[JITSU_V2_ID + "Q"] || [];
+  if (options.debug) {
+    console.log(`Jitsu callback queue size: ${callbackQueue.length}`, callbackQueue);
+  }
+  callbackQueue.forEach((callback: any) => {
+    try {
+      callback(jitsu);
+    } catch (e: any) {
+      console.warn(`Error processing callback from Jitsu queue`, e);
+    }
+  })
   if (!options.initOnly) {
     jitsu.page();
   }
