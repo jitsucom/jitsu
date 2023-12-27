2929b7f4545479c6d5061b0358ac26f53195475b
Ildar Nurislamov <absorbb@gmail.com>
2023-12-27 14:50:01 +0400

    fix test

--- libs/core-functions/__tests__/lib/mem-store.ts
+++ libs/core-functions/__tests__/lib/mem-store.ts
@@ -1,12 +1,15 @@
-import { AnonymousEventsStore, SetOpts, Store } from "@jitsu/protocols/functions";
+import { AnonymousEventsStore, SetOpts, Store, TTLStore } from "@jitsu/protocols/functions";
 import { AnalyticsServerEvent } from "@jitsu/protocols/analytics";
 
-export function createStore(): Store {
+export function createStore(): TTLStore {
   return {
     del(key: string): Promise<void> {
       throw new Error("Method not implemented.");
     },
     get(key: string): Promise<any> {
+      throw new Error("Method not implemented.");
+    },
+    getWithTTL(key: string): Promise<{ value: any; ttl: number } | undefined> {
       throw new Error("Method not implemented.");
     },
     set(key: string, value: any, opts?: SetOpts): Promise<void> {
