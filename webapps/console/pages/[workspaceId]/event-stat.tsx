b21968718281ebc9f3f13bda5f052435d373d3f2
Vladimir Klimontovich <vladimir@jitsu.com>
2024-01-24 10:58:56 -0500

    WIP - stat plot

---
+++ webapps/console/pages/[workspaceId]/event-stat.tsx
@@ -0,0 +1,8 @@
+import { EventStatPage } from "../../components/EventStat/EventStatPage";
+import { WorkspacePageLayout } from "../../components/PageLayout/WorkspacePageLayout";
+
+export default function EventStatPage0 ()  {
+  return <WorkspacePageLayout>
+    <EventStatPage />
+  </WorkspacePageLayout>
+} ;
