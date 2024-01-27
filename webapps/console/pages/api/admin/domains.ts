d761adeeacaf9176bcea64943add83174e8c4040
Ildar Nurislamov <absorbb@gmail.com>
2024-01-28 00:44:14 +0400

    caddy domain checker - skip IP addresses

--- webapps/console/pages/api/admin/domains.ts
+++ webapps/console/pages/api/admin/domains.ts
@@ -16,7 +16,7 @@ export default createRoute()
       token: z.string(),
     }),
   })
-  .handler(async ({ query: { token, domain } }) => {
+  .handler(async ({ query: { token, domain }, res }) => {
     if (!dataDomains) {
       throw new ApiError(`Domain ${domain} not found. Data domains configuration is absent`, {}, { status: 404 });
     }
@@ -24,7 +24,8 @@ export default createRoute()
       throw new ApiError("Unauthorized", {}, { status: 401 });
     }
     if (domain.match(/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)) {
-      throw new ApiError(`${domain} is an IP address`, {}, { status: 404 });
+      res.status(404).end(`${domain} is an IP address`);
+      return;
     }
     log.atInfo().log(`Validating domain ${domain}`);
 
