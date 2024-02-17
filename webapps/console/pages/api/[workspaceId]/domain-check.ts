d26fc7b4d874fe7bd016cc6bd2f8dbbd3d5ac857
Ildar Nurislamov <absorbb@gmail.com>
2024-02-17 19:47:30 +0400

    console: domain-check: use ingmgr for all domains

--- webapps/console/pages/api/[workspaceId]/domain-check.ts
+++ webapps/console/pages/api/[workspaceId]/domain-check.ts
@@ -42,29 +42,25 @@ export default createRoute()
       log.atWarn().log(`Domain ${domain} is not valid`);
       return { ok: false, reason: "requires_cname_configuration", cnameValue: customDomainCnames[0] };
     }
-    //TODO: remove condition after migration complete
-    if (cname === "cname2.jitsu.com") {
-      try {
-        const ingressStatus = await checkOrAddToIngress(domain);
-        log.atInfo().log(`Ingress status for ${domain}: ${JSON.stringify(ingressStatus)}`);
-        if (!ingressStatus) {
-          log.atWarn().log(`Incorrect ingress status ${domain} is not valid`);
-          return { ok: false, reason: "internal_error" };
-        }
-        if (ingressStatus.status === "ok") {
-          return { ok: true };
-        } else if (ingressStatus.status === "pending_ssl") {
-          return { ok: false, reason: "pending_ssl" };
-        } else if (ingressStatus.status === "dns_error") {
-          return { ok: false, reason: "requires_cname_configuration", cnameValue: customDomainCnames[0] };
-        } else {
-          return { ok: false, reason: "internal_error" };
-        }
-      } catch (e) {
-        log.atError().withCause(e).log(`Error checking ingress status for ${domain}`);
+    try {
+      const ingressStatus = await checkOrAddToIngress(domain);
+      log.atInfo().log(`Ingress status for ${domain}: ${JSON.stringify(ingressStatus)}`);
+      if (!ingressStatus) {
+        log.atWarn().log(`Incorrect ingress status ${domain} is not valid`);
         return { ok: false, reason: "internal_error" };
       }
+      if (ingressStatus.status === "ok") {
+        return { ok: true };
+      } else if (ingressStatus.status === "pending_ssl") {
+        return { ok: false, reason: "pending_ssl" };
+      } else if (ingressStatus.status === "dns_error") {
+        return { ok: false, reason: "requires_cname_configuration", cnameValue: customDomainCnames[0] };
+      } else {
+        return { ok: false, reason: "internal_error" };
+      }
+    } catch (e) {
+      log.atError().withCause(e).log(`Error checking ingress status for ${domain}`);
+      return { ok: false, reason: "internal_error" };
     }
-    return { ok: true };
   })
   .toNextApiHandler();
