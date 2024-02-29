4132a0c2e679262ce29d9e44b37c2c38be033c8d
Ildar Nurislamov <absorbb@gmail.com>
2024-02-29 14:36:01 +0400

    events-log-trim API endpoint to trim Events Log

---
+++ webapps/console/pages/api/admin/events-log-trim.ts
@@ -0,0 +1,115 @@
+import { createRoute } from "../../../lib/api";
+import { db } from "../../../lib/server/db";
+import { assertDefined, assertTrue, getLog, stopwatch } from "juava";
+import { clickhouse } from "../../../lib/server/clickhouse";
+import dayjs from "dayjs";
+
+export const log = getLog("events-log-trim");
+
+export default createRoute()
+  .GET({
+    auth: true,
+    streaming: true,
+  })
+  .handler(async ({ res, user }) => {
+    const userProfile = await db.prisma().userProfile.findFirst({ where: { id: user.internalId } });
+    assertDefined(userProfile, "User profile not found");
+    assertTrue(userProfile.admin, "Not enough permissions");
+    log.atInfo().log(`Trimming events log`);
+    const metricsSchema = process.env.CLICKHOUSE_METRICS_SCHEMA || "newjitsu_metrics";
+    const clickhouseCluster = process.env.CLICKHOUSE_CLUSTER || "jitsu_cluster";
+    const eventsLogSize = process.env.EVENTS_LOG_SIZE ? parseInt(process.env.EVENTS_LOG_SIZE) : 200000;
+    const statQuery: string = `select actorId, type, timestamp
+                           from (select actorId,
+                                        type,
+                                        timestamp,
+                                        row_number() OVER (PARTITION BY actorId, type ORDER BY timestamp desc) rn
+                                 from ${metricsSchema}.events_log
+                                 where xor(level = 'error', {withoutErrors:UInt8})) rows
+                           where rn = {eventsLogSize:UInt32}`;
+    const deleteQuery: string = `delete
+                             from ${metricsSchema}.events_log
+                             where actorId = {actorId:String}
+                               and
+                                 type = {type :String}
+                               and xor(level = 'error', {withoutErrors:UInt8})
+                               and
+                                 timestamp
+                                 < {timestamp :DateTime64};`;
+    const dropPartitionQuery: string = `alter table ${metricsSchema}.events_log on cluster ${clickhouseCluster} drop partition {partition:String}`;
+    const result: any[] = [];
+    for (const trimErrors of [false, true]) {
+      const sw = stopwatch();
+      const chResult = (await (
+        await clickhouse.query({
+          query: statQuery,
+          query_params: {
+            eventsLogSize: eventsLogSize,
+            withoutErrors: !trimErrors,
+          },
+          clickhouse_settings: {
+            wait_end_of_query: 1,
+          },
+        })
+      ).json()) as any;
+      log
+        .atInfo()
+        .log(
+          `Trimming ${trimErrors ? "error level" : "non-error levels"} for ${
+            chResult.data.length
+          } actors. (Loaded in ${sw.elapsedPretty()})`
+        );
+      for (const row of chResult.data) {
+        const actorId = row.actorId;
+        const type = row.type;
+        const timestamp = row.timestamp;
+        try {
+          const cr = await clickhouse.command({
+            query: deleteQuery,
+            query_params: {
+              actorId: actorId,
+              type: type,
+              withoutErrors: !trimErrors,
+              timestamp: timestamp,
+            },
+            clickhouse_settings: {
+              enable_lightweight_delete: 1,
+            },
+          });
+          result.push({ ...row, trimErrors });
+          log
+            .atInfo()
+            .log(`Trimmed ${trimErrors ? "error level" : "non-error levels"} for ${actorId} ${type} ${timestamp}`);
+        } catch (e: any) {
+          result.push({ ...row, errors: trimErrors, error: e.message });
+          log
+            .atError()
+            .withCause(e)
+            .log(
+              `Failed to trim ${trimErrors ? "error level" : "non-error levels"} for ${actorId} ${type} ${timestamp}`
+            );
+        }
+      }
+    }
+    const oldPartition = dayjs().subtract(2, "month").format("YYYYMM");
+    try {
+      await clickhouse.command({
+        query: dropPartitionQuery,
+        query_params: {
+          partition: oldPartition,
+        },
+      });
+      log.atInfo().log(`Deleted partition ${oldPartition}`);
+    } catch (e: any) {
+      log.atError().withCause(e).log(`Failed to delete partition ${oldPartition}`);
+    }
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    res.write(JSON.stringify(result, null, 2));
+    res.end();
+  })
+  .toNextApiHandler();
+export const config = {
+  maxDuration: 300, //5 mins
+};
