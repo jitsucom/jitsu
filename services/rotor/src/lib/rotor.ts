cd0057a4987ea997351603c2866035f39c4e5828
Ildar Nurislamov <absorbb@gmail.com>
2024-04-15 14:34:59 +0400

    rotor: reduce kafka sessionTimeout to 10s

--- services/rotor/src/lib/rotor.ts
+++ services/rotor/src/lib/rotor.ts
@@ -60,7 +60,7 @@ export function kafkaRotor(cfg: KafkaRotorConfig): KafkaRotor {
       consumer = kafka.consumer({
         groupId: consumerGroupId,
         allowAutoTopicCreation: false,
-        sessionTimeout: 30000,
+        sessionTimeout: 10000,
       });
       await consumer.connect();
       log.atInfo().log("Subscribing to kafka topics: ", kafkaTopics);
