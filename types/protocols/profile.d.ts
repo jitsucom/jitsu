59a4b13c3f0bf48a25ffef8a94483d7eeb50afde
Ildar Nurislamov <absorbb@gmail.com>
2024-10-02 12:51:26 +0400

    console: streams-with-destinations repo: never throttle active workspaces

--- types/protocols/profile.d.ts
+++ types/protocols/profile.d.ts
@@ -2,15 +2,15 @@ import { FunctionContext } from "./functions";
 import { AnalyticsServerEvent } from "./analytics";
 
 export type ProfileResult = {
-  properties: Record<string, any>
-}
+  properties: Record<string, any>;
+};
 
 export type ProfileFunction = (params: {
   context: FunctionContext;
   events: Iterable<AnalyticsServerEvent>;
-  user:{
+  user: {
     id?: string;
     anonymousId?: string;
     traits: Record<string, any>;
-  }
-}) => Promise<ProfileResult>
+  };
+}) => Promise<ProfileResult>;
