ebda1868867666e3dd34c02b066313ddeeaac6c5
Vladimir Klimontovich <vladimir@jitsu.com>
2026-07-28 09:08:34 -0400

    JITSU-135 feat(console): use official Microsoft Clarity logo
    Replaces the hand-drawn placeholder with Clarity's official mark, sourced from
    Clarity's own static assets (claritystatic.azureedge.net/images/logo.svg).

--- webapps/console/lib/schema/icons/clarity.tsx
+++ webapps/console/lib/schema/icons/clarity.tsx
@@ -1,13 +1,47 @@
 export default (
-  <svg width="100%" height="100%" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="100%" height="100%" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10.0004 -1.00888L14.4827 6.67518L3.72505 9.7488L10.0004 -1.00888Z" fill="#41A5EE" />
+    <path
+      d="M10.0004 -1.00888L14.4827 6.67518L3.72505 9.7488L10.0004 -1.00888Z"
+      fill="url(#clarity_paint0)"
+      fillOpacity="0.2"
+    />
+    <path d="M3.72505 9.7488L20.758 17.4329L14.4827 6.67518L3.72505 9.7488Z" fill="url(#clarity_paint1)" />
+    <path d="M20.758 17.4329H-0.757812L3.72505 9.7488L20.758 17.4329Z" fill="url(#clarity_paint2)" />
     <defs>
-      <linearGradient id="clarityGrad" x1="6" y1="6" x2="42" y2="42" gradientUnits="userSpaceOnUse">
-        <stop stopColor="#22C3E6" />
-        <stop offset="1" stopColor="#3B6FE0" />
+      <linearGradient
+        id="clarity_paint0"
+        x1="11.7936"
+        y1="1.62565"
+        x2="11.7936"
+        y2="12.1638"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="white" />
+        <stop offset="1" stopColor="white" stopOpacity="0" />
+      </linearGradient>
+      <linearGradient
+        id="clarity_paint1"
+        x1="10.0001"
+        y1="8.98039"
+        x2="19.221"
+        y2="15.8963"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#2B7CD3" />
+        <stop offset="1" stopColor="#185ABD" />
+      </linearGradient>
+      <linearGradient
+        id="clarity_paint2"
+        x1="2.64598"
+        y1="8.21064"
+        x2="20.8016"
+        y2="10.1506"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#185ABD" />
+        <stop offset="1" stopColor="#103F91" />
       </linearGradient>
     </defs>
-    <rect x="7" y="7" width="20" height="20" rx="5" fill="url(#clarityGrad)" fillOpacity="0.55" />
-    <rect x="21" y="21" width="20" height="20" rx="5" fill="url(#clarityGrad)" fillOpacity="0.55" />
-    <rect x="14" y="14" width="20" height="20" rx="5" fill="url(#clarityGrad)" fillOpacity="0.9" />
   </svg>
 );
